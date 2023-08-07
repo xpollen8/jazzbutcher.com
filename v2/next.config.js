986e2aba5295a817f1c1ca55d593844823c7522b
xpollen8 <github@xpollen8.com>
2023-08-06 20:06:47 -0700

    dw: /api redirects via config - neat

--- v2/next.config.js
+++ v2/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async redirects() {
+			return [
+				{
+					source: '/api/:path*',
+					destination: `${process.env.JBC_DATA_SERVER}/api/:path*`,
+					permanent: false,
+				},
+			]
+	},
 	images: {
 			remotePatterns: [
 				{

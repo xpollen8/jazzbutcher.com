acd4f4066373a0f1e7b1805e25136c41109afa7f
xpollen8 <github@xpollen8.com>
2023-08-08 00:13:47 -0700

    dw: initial checkin

---
+++ v2/src/app/memoriam/page.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import './page.css'
+import Header from '../../components/Header';
+import { linkInternal } from '../../lib/macros';
+import { Credit } from '../../components/GenericWeb';
+
+const Memoriam = () =>
+<>
+	<Header title="In Memoriam" />
+	<div className="w-full flex">
+		<div className="text-center w-1/2">
+			<br/> <br/> <br/>
+			<h2>Patrick Guy Sibley Huntrods</h2>
+			<h3>AKA: Pat Fish - The Jazz Butcher</h3>
+			<b> 1957-12-20 .. 2021-10-05</b>
+			<br/> <br/> <br/>
+			<b>{linkInternal("/eulogy", "Alan Moore's eulogy is here..")}</b>
+			<br/> <br/> <br/> <br/>
+			{linkInternal("/help", "Help enrich the JBC archives!")}
+		</div>
+		<div className="w-1/2">
+			<Link href="https://jazzbutcher.com/images/20211008_joolesjoyce_tribute.jpg">
+				<Image src="https://jazzbutcher.com/images/20211008_joolesjoyce_tribute_500.jpg" width={500} height={500} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} />
+			</Link>
+			<Credit g="Jooles Joyce" />
+		</div>
+	</div>
+</>
+
+export default Memoriam;

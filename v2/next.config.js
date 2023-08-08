2a617ca482e98fea76a3bd708ce9121b87928cb0
xpollen8 <github@xpollen8.com>
2023-08-07 19:12:19 -0700

    dw: redirect audio and images

--- v2/next.config.js
+++ v2/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
 	async redirects() {
 			return [
 				{
+					source: '/audio/:path*',
+					destination: `https://jazzbutcher.com/audio/:path*`,
+					permanent: false,
+				},
+				{
+					source: '/images/:path*',
+					destination: `https://jazzbutcher.com/images/:path*`,
+					permanent: false,
+				},
+				{
 					source: '/api/:path*',
 					destination: `${process.env.JBC_DATA_SERVER}/api/:path*`,
 					permanent: false,

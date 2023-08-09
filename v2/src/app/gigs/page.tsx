2f535d15e48a939dadac34d86104180cec9d60e7
xpollen8 <github@xpollen8.com>
2023-08-08 17:33:40 -0700

    dw: handle search errors

--- v2/src/app/gigs/page.tsx
+++ v2/src/app/gigs/page.tsx
@@ -8,11 +8,13 @@ import SearchResults from '../../components/SearchResults';
 
 const Gigs = () => {
 	const [results, setResults] = useState({});
+	const [error, setError] = useState();
 
 	return (<>
 		<Header section={`Gigs`} navType={`Gig`} navPrev={{ datetime: '2020-10-11' }} navNext={{ datetime: '2020-10-13' }}>
-			<GigHeader section='Gigs' f='venue' q='100' setResults={setResults} />
+			<GigHeader section='Gigs' f='venue' q='' setResults={setResults} setError={setError}/>
 		</Header>
+		{(error) && <h1 style={{ color: 'red' }}>{error}</h1>}
 		<SearchResults results={results} />
 	</>)
 }

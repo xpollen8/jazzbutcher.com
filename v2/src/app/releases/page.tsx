0f6190b5ad98b39013038b53e48571b1bc2e5376
xpollen8 <github@xpollen8.com>
2023-08-13 17:50:16 -0700

    dw: initial checkin

---
+++ v2/src/app/releases/page.tsx
@@ -0,0 +1,17 @@
+import moment from 'moment';
+import { releaseByLookup } from '../../lib/macros';
+import apiData from '../../lib/apiData';
+import Header from '../../components/Header';
+import MakeAlbumBlurb from '../../components/MakeAlbumBlurb';
+import { ReleaseType } from '../../components/Release';
+
+
+const Releases = async () => {
+	const releases = await apiData('releases');
+	return <>
+		<Header section="albums" />
+		{releases?.albums?.sort((a: ReleaseType, b: ReleaseType) => moment(b.year).diff(moment(a.year))).map(MakeAlbumBlurb)}
+	</>
+}
+
+export default Releases;

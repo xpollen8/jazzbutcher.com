a7715289624dfd7f9c9470ebcd58534fd67c11c7
xpollen8 <github@xpollen8.com>
2023-08-13 17:51:09 -0700

    dw: added ReleaseType

--- v2/src/components/Release.tsx
+++ v2/src/components/Release.tsx
@@ -1,6 +1,29 @@
 import { releaseByLookup } from '../lib/macros';
 import MakeAlbumBlurb from './MakeAlbumBlurb';
 
+export type ReleaseType =  {
+	type?: string
+	href?: string
+	title?: string
+	thumb?: string
+	blurb?: string
+	year?: string
+	buy?: string
+	project?: string
+	lookup?: string
+	label?: string
+	catalog?: string
+	media?: string
+	country?: string
+	credit_g?: string
+	credit_u?: string
+	credit_d?: string
+	contribution?: string
+}
+
+export type ReleaseTypeWithChildren = ReleaseType & { children?: string | React.ReactElement }
+
+
 const Release = async ({ lookup }: { lookup: string }) => <MakeAlbumBlurb {...(await releaseByLookup(lookup))} />
 
 export default Release;

d486881923aec2008ccf3cde87c383a7226dd33b
xpollen8 <github@xpollen8.com>
2023-08-14 14:32:23 -0700

    dw: nicer CSS

--- v2/src/components/GigGraph.tsx
+++ v2/src/components/GigGraph.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
 import { RecordType} from '../lib/macros';
-import Tag from './Tag';
 
 const DoGraph = ({ year, gigs=[], section='main', inpage=false, skip0=false, href }:
 {
@@ -15,16 +13,44 @@ const DoGraph = ({ year, gigs=[], section='main', inpage=false, skip0=false, hre
 	if (!(gigs.length || !skip0)) return <></>;	// no data
 
 	const scaling = 110;	// max # of gigs in a year
-	const typeLookup: RecordType = {
-		eg: '	Black Eg',
-		wilson: 'Wilson',
-		sumo: 'Sumosonic',
-		nopat: 'Presents',
-		tribute: 'Tribute',
-		solo: 'Pat Solo',
-		other: 'JBC',
+	const types: RecordType = {
+		eg: {
+			title: 'Black Eg',
+			color: '#fff',
+			background: '#666',
+		},
+		wilson: {
+			title: 'Wilson',
+			color: '#fff',
+			background: '#e00',
+		},
+		sumo: {
+			title: 'Sumosonic',
+			color: '#fff',
+			background: '#00e',
+		},
+		nopat: {
+			title: 'Presents',
+			color: '#fff',
+			background: '#555',
+		},
+		tribute: {
+			title: 'Tribute',
+			color: '#fff',
+			background: '#000',
+		},
+		solo: {
+			title: 'Pat Solo',
+			color: '#333',
+			background: '#0d0',
+		},
+		other: {
+			title: 'JBC',
+			color: '#333',
+			background: '#ee0',
+		},
 	}
-	const extras: string[] = [ 'eg', 'wilson', 'sumo', 'nopat', 'tribute', 'solo', 'other' ];
+	const extras: string[] = Object.keys(types);
 	const sums: RecordType = {};
 	extras.forEach((e: string) => { sums[e] = 0 });
 
@@ -35,8 +61,7 @@ const DoGraph = ({ year, gigs=[], section='main', inpage=false, skip0=false, hre
 			sums[e] += g?.extra?.includes(e) || 0;
 		});
 	});
-	const JBC = gigs.length - attributed;
-	sums['other'] = JBC;
+	sums['other'] = gigs.length - attributed;	// JBC gigs are default
 	let maxGigs = 0;
 	extras.forEach((e) => {
 		if (sums[e] > maxGigs) maxGigs = sums[e];
@@ -45,61 +70,47 @@ const DoGraph = ({ year, gigs=[], section='main', inpage=false, skip0=false, hre
 	const link = (inpage) ? `#${year}_${section}` : ((href) ?  href : `/gigs/${year}`);
 	const calcWidth = (num: number, max: number) => `${(1 + (100 * num / (scaling * 1.2)))}%`;
 
+	const Bar = ({ color, background, width, num }: { color: string, background: string, width: string, num: number }) => <div style={{ borderRadius: '10px', background, height: '15px', marginRight: '5px', width, color, textAlign: 'right', paddingRight: '3px', paddingTop: '1px' }} >{num}</div>
+
 	return (<>
 		<table style={{
-			listStyle: 'none',
-			paddingTop: '10px',
 			width: '100%',
-			marginBottom: '10px',
-			border: '1px',
+			backgroundColor: 'rgba(245,245,245,0.7)',
 		}}>
 			<tr>
-			<td>
+			<td style={{ padding: '3px' }}>
 				<a href={link}>{year}</a>
 			</td>
 
 			{(gigs.length) &&
 				<td style={{
-						backgroundColor: 'rgba(245,245,245,0.7)',
 						width: '100%',
-						height: '15px',
-						color: 'black',
 						lineHeight: '15px',
-						padding: '0px 5px',
-						marginBottom: '1px',
 					}} >
 
 					{(() => {
-						const color = (maxGigs) ? 'gray' : 'blue';
 						return (<>
 							{(maxGigs !== gigs.length) &&
 								<div className="flex m-1">
-									<Image
-										alt="gig count"
-										style={{ borderRadius: '10px', background: '#dedede', height: '15px', marginRight: '5px', width: calcWidth(gigs.length, maxGigs) }}
-										src="https://jazzbutcher.com/images/spacer.gif"
-										height="15"
-										width="500" />
-									<b>{gigs.length} gigs</b>
+									<Bar color="#000" background='#dedede' width={calcWidth(gigs.length, maxGigs)} num={gigs.length} />
+									<b style={{ color: 'blue' }}>Total</b>
 								</div>
 							}
 							{extras.map((type: string, key: number) => {
 								const num = sums[type];
 								if (num > 0) {
 									return <div key={key} className="flex m-1">
-										<Image
-											alt="segment count"
-											style={{ borderRadius: '10px', height: '15px', marginRight: '5px', width: calcWidth(num, maxGigs) }}
-											src={`https://jazzbutcher.com/images/gig_spacer_${type}.gif`}
-											height="15"
-											width="500"
-										/>
-										<span className="smalltext">
-											<span style={{ color }}>
-												{num}
-											</span>
-											{' '}({typeLookup[type]})
-										</span>
+										<Bar color={types[type].color} background={types[type].background} width={calcWidth(num, maxGigs)} num={num} />
+										{(num === gigs.length)
+											?  <b>
+													<span style={{ color: 'blue' }}>
+													{types[type].title}
+													</span>
+												</b>
+											: <span className="smalltext">
+													{types[type].title}
+												</span>
+										}
 									</div>
 								}
 							})}
@@ -110,20 +121,10 @@ const DoGraph = ({ year, gigs=[], section='main', inpage=false, skip0=false, hre
 
 			{!(gigs.length) &&
 				<td style={{
-						backgroundColor: 'rgba(245,245,245,0.7)',
 						width: '100%',
-						height: '15px',
-						color: 'black',
 						lineHeight: '15px',
-						padding: '0px 5px',
-						marginBottom: '1px',
 					}} >
-						<Image
-							alt="gig count"
-							style={{ background: '#dedede', height: '15px', width: calcWidth(0, maxGigs) }}
-							src="https://jazzbutcher.com/images/spacer.gif"
-							height="15"
-							width="500" />
+						<Bar color="#000" background='#dedede' width={calcWidth(0, maxGigs)} num={0} />
 					0
 				</td>
 			}

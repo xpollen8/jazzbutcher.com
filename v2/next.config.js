158bf80aa2dd1924603da0991ac5a304ea57c33e
xpollen8 <github@xpollen8.com>
2023-08-13 17:23:52 -0700

    dw: /albums->/releases

--- v2/next.config.js
+++ v2/next.config.js
@@ -1,19 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async rewrites() {
+			return [
+			]
+	},
+
 	async redirects() {
 			return [
 				{
+					source: '/albums/:path*',
+					destination: '/releases/:path*',
+					permanent: false,
+				},
+				{
+					source: '/albums',
+					destination: '/releases',
+					permanent: false,
+				},
+				{
 					source: '/audio/:path*',
-					destination: `https://jazzbutcher.com/audio/:path*`,
+					destination: 'https://jazzbutcher.com/audio/:path*',
 					permanent: false,
 				},
 				{
 					source: '/images/:path*',
-					destination: `https://jazzbutcher.com/images/:path*`,
+					destination: 'https://jazzbutcher.com/images/:path*',
 					permanent: false,
 				},
 				{
-					source: '/api/:path*',
+					source: '/api/v2/:path*',
 					destination: `${process.env.JBC_DATA_SERVER}/api/:path*`,
 					permanent: false,
 				},

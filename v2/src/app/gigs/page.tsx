b147b66fe4ddb6eff12b62b83c29d19170210a1e
xpollen8 <github@xpollen8.com>
2023-08-13 23:19:33 -0700

    dw: default search when entering gigs page

--- v2/src/app/gigs/page.tsx
+++ v2/src/app/gigs/page.tsx
@@ -5,17 +5,29 @@ import { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import GigHeader from '../../components/GigHeader';
 import SearchResults from '../../components/SearchResults';
+import apiData from '../../lib/apiData';
 
 const Gigs = () => {
-	const [results, setResults] = useState({});
+	//const [gigs, setGigs] = useState();
+	const [results, setResults] = useState();
 	const [error, setError] = useState();
+
+	useEffect(() => {
+		if (!results) {
+			(async () => {
+				// set initial results
+				setResults({ type: "archive", noun: "archive", ...(await apiData('gigs')) });
+			})();
+		}
+		return () => {}
+	}, []);
 
 	return (<>
 		<Header section={`Gigs`} navType={`Gig`} navPrev={{ datetime: '2020-10-11' }} navNext={{ datetime: '2020-10-13' }}>
 			<GigHeader section='Gigs' f='venue' q='' setResults={setResults} setError={setError}/>
 		</Header>
 		{(error) && <h1 style={{ color: 'red' }}>{error}</h1>}
-		<SearchResults results={results} />
+		<SearchResults results={results || {}} />
 	</>)
 }
 

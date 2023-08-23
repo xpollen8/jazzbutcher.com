7d5d7c2935f88feb9f4004d9e06f12a63d09b9c3
xpollen8 <github@xpollen8.com>
2023-08-23 08:11:22 -0700

    dw: added extra icons, results navigator

--- v2/src/app/gigs/page.tsx
+++ v2/src/app/gigs/page.tsx
@@ -1,15 +1,57 @@
 "use client"
 
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation'
-import { pat, max, owen, eg, at } from '../../lib/defines';
+import { pat, max, owen, eg, at } from '@/lib/defines';
 
-import Header from '../../components/Header';
-import GigHeader from '../../components/GigHeader';
-import SearchResults from '../../components/SearchResults';
-import apiData from '../../lib/apiData';
-import { Source } from '../../components/GenericWeb';
-import { doSearch, searchOptions } from '../../lib/macros';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import SearchDialog from '@/components/GigHeader';
+import SearchResults from '@/components/SearchResults';
+import apiData from '@/lib/apiData';
+import { Source } from '@/components/GenericWeb';
+import { parseYear, ts2URI, doSearch, searchOptions } from '@/lib/macros';
+
+type GigResults = {
+	[key: string]: any;
+	datetime?: string;
+}
+
+type Props_MakeHeader = {
+	project?: string
+	section?: string
+	title?: string
+	passthru?: string
+	navType?: 'Gig' | 'Year'
+	navPrev?: GigResults
+	navNext?: GigResults
+	children?: React.ReactNode
+}
+
+const ResultNavigator = (props: Props_MakeHeader): React.ReactNode => {
+	if (!props?.navType) return <></>;
+	const setGUI = (type: string, chr: React.ReactNode, cls: string, datetime?: string) => {
+		if (!datetime) return;
+		const uri = (type === 'Gig') ? ts2URI(datetime) : parseYear(datetime);
+		if (!uri) return;
+		return (
+			<Link href={`/gigs/${uri}`}>
+				<div className={cls}>
+					{chr}
+				</div>
+			</Link>
+		)
+	}
+	const prev = setGUI(props.navType, <>&lt;</>, 'left-arrow', props?.navPrev?.datetime);
+	const next = setGUI(props.navType, <>&gt;</>, 'right-arrow', props?.navNext?.datetime);
+	return (
+		<span className="smalltext" style={{ padding: '40px' }}>
+				{prev}
+				{next}
+		</span>
+	)
+}
 
 const GigBlurb = () =>
 <>
@@ -66,19 +108,28 @@ const Gigs = () => {
 			if (f && q) {
 				doSearch(f, q, setResults, setError);
 			} else {
-				setResults({ type: "archive", noun: "archive", ...(await apiData('gigs')) });
+				//const pix = (await apiData('gigmedias'))?.results.filter(r => r.type === 'pix');
+				const gigs = await apiData('gigs');
+				/*
+				gigs.results?.forEach((g, idx) => {
+					gigs.results[idx].pix = pix?.filter(p => p.datetime == g.datetime);
+					console.log("PIX", gigs.results[idx].pix);
+				});
+				*/
+				setResults({ type: "archive", noun: "archive", ...gigs });
 			}
 		})();
 		return () => {}
 	}, [ f, q ]);
 
+	const extraNav = <ResultNavigator navType='Gig' navPrev={{ datetime: '2020-10-11' }} navNext={{ datetime: '2020-10-13' }} />;
 	return (<>
-		<Header section={`Gigs`} navType={`Gig`} navPrev={{ datetime: '2020-10-11' }} navNext={{ datetime: '2020-10-13' }}>
-			<GigHeader section='Gigs' f={f} q={q} setResults={setResults} setError={setError}/>
-		</Header>
+		<Header section='gigs' extraNav={extraNav} />
+		<SearchDialog f={f} q={q} setResults={setResults} setError={setError} />
 		{(error) && <h1 style={{ color: 'red' }}>{error}</h1>}
 		{(!(f && q) || !(results)) && <GigBlurb />}
 		{(results) && <SearchResults results={results || {}} />}
+		<Footer />
 	</>)
 }
 

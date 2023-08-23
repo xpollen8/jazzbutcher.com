1f6c449bed7e3e94361fdc612d056271ec5823c7
xpollen8 <github@xpollen8.com>
2023-08-22 23:14:41 -0700

    dw: initial checki

---
+++ v2/src/app/mailinglist/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+const Missing = () => 
+<>
+	<Header />
+	<h1>This section is not yet implemented</h1>
+	<Footer />
+</>
+
+export default Missing;

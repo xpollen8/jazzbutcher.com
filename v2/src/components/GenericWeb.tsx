1c4039961ae09199729676292227c2a474434bb7
xpollen8 <github@xpollen8.com>
2023-08-28 18:48:16 -0700

    dw: parseDate to normalize date inputs

--- v2/src/components/GenericWeb.tsx
+++ v2/src/components/GenericWeb.tsx
@@ -1,4 +1,4 @@
-import { dateDiff, parseDomain, linkInternal, linkExternal } from '@/lib/macros';
+import { dateDiff, parseDate, parseDomain, linkInternal, linkExternal } from '@/lib/macros';
 
 const	genericWeb = ({ x, g, u, t, s, d }: {
 	x?: string
@@ -8,7 +8,7 @@ const	genericWeb = ({ x, g, u, t, s, d }: {
 	s?: string
 	d?: string
 }) => (
-	(x || g || u || d) && <div className={s}>
+	(x || g || u || parseDate(d)) && <div className={s}>
 		{(t) && <><b>{t}</b>:{' '}</>}
 		{(x) && <>x</>}
 		{(() => {
@@ -30,7 +30,7 @@ const	genericWeb = ({ x, g, u, t, s, d }: {
 				}
 			}
 		})()}
-		{(d) && <>{' '}{dateDiff(d)}</>}
+		{(parseDate(d)) && <>{' '}{dateDiff(d)}</>}
 	</div>
 )
 

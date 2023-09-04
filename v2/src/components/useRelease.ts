92bd9a9426bb6973b9568731a4fb35b2887cfa38
xpollen8 <github@xpollen8.com>
2023-09-03 22:58:49 -0700

    dw: use our own api

--- v2/src/components/useRelease.ts
+++ v2/src/components/useRelease.ts
@@ -2,7 +2,7 @@ import useSWR from 'swr';
 
 const useRelease = (lookup: string) => {
 	const fetcher = (url: string) => fetch(url).then((res) => res.json());
-	const { data, isLoading, error } = useSWR(`https://data.jazzbutcher.com/api/songs_by_release/${lookup}`, fetcher);
+	const { data, isLoading, error } = useSWR(`/api/songs_by_release/${lookup}`, fetcher);
 	return {
 		data,
 		isLoading,

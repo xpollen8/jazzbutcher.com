39fbc7158f812ac5ba59a10301629b6495f4b408
xpollen8 <github@xpollen8.com>
2023-09-05 13:11:38 -0700

    dw: map /letters to new form

--- v2/src/lib/fetchFeedback.tsx
+++ v2/src/lib/fetchFeedback.tsx
@@ -2,11 +2,34 @@
 
 import apiData from '@/lib/apiData';
 
+const mapLetterURLIFeedbackLookup = (uri: string) => {
+let ret = uri;
+	[
+		[ '19900214', '90Feb14' ],
+		[ '19910201', '91Feb01' ],
+		[ '19930830', '93Aug30' ],
+		[ '19931019', '93Oct19' ],
+		[ '19940413', '94Apr13' ],
+		[ '19940624', '94Jun24' ],
+		[ '19940913', '94Sep13' ],
+	].forEach(([ current, old ]: string[]) => {
+		ret = ret.replace(current, old);
+	});
+	if (!ret.match(/.html/)) {
+		if (ret[ret.length - 1].match(/[0-9]/)) {
+			return `${ret}/index.html`;
+		}
+		return `${ret}.html`;
+	}
+	return ret;
+}
+
 const pathname2feedbackURI = (pathname: string) => {
 	const fullpath = (uri: string) => {
 		if (uri === '/') return '/htdb/index.html';
 		if (uri === '/memoriam') return '/site/memoriam.html';
 		if (uri === '/letters') return '/letters/index.html';
+		if (uri.startsWith('/letters')) return mapLetterURLIFeedbackLookup(uri);
 		if (uri === '/eulogy') return '/site/eulogy.html';
 		//if (uri === '/mad') return '/mad/index.html';
 		//if (uri === '/gigs') return '/gigs/index.html';

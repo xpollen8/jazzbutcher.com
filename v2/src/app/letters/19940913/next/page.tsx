214d8963f6bc831ace81d13acbf81bb530337095
xpollen8 <github@xpollen8.com>
2023-09-06 13:14:27 -0700

    dw: LetterHeader, etc CSS

--- v2/src/app/letters/19940913/next/page.tsx
+++ v2/src/app/letters/19940913/next/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import LetterHeader from '@/components/LetterHeader';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
 import {
 } from '@/lib/defines';
 
@@ -8,11 +10,11 @@ const Letter = () =>  {
 	const title = <><Link href='/letters/19940913'>13Sep94</Link> | Upcoming Gigs</>
 	return ( <>
 		<Header section='letters' title={title} />
-		<div className="letter">
-		  <b>Upcoming Gigs</b> 
-			<br /><Link href="/gigs/1994/Oct7.html"><b>7 Oct 94</b></Link> - The Boat Race, Cambridge, England
-			<br /><Link href="/gigs/1994/Oct12.html"><b>12 Oct 94</b></Link> - Slurp&apos;s Wino Bar, Northampton, England
-			<br /><Link href="/gigs/1994/Dec21.html"><b>21 Dec 94</b></Link> - Slurp&apos;s Wino Bar, Northampton, England
+		<div className="etx">
+		  <LetterHeader title="Upcoming Gigs" />
+			<MakeSimpleURI uri="/gigs/1994/Oct7.html" text="7 Oct 94" aux="The Boat Race, Cambridge, England" />
+			<MakeSimpleURI uri="/gigs/1994/Oct12.html" text="12 Oct 94" aux="Slurp's Wino Bar, Northampton, England" />
+			<MakeSimpleURI uri="/gigs/1994/Dec21.html" text="21 Dec 94" aux="Slurp's Wino Bar, Northampton, England" />
 		</div>
 		<Footer />
 	</>)

f6f6bbba2fcf3cf3b3f5c6714b8563260e195691
xpollen8 <github@xpollen8.com>
2023-09-06 11:46:17 -0700

    dw: use a map and etc CSS

--- v2/src/app/letters/19940624/page.tsx
+++ v2/src/app/letters/19940624/page.tsx
@@ -1,18 +1,28 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
+
+const topics = [
+	{ uri: "/letters/19940624/intro.html", text: "Introduction" },
+	{ uri: "/letters/19940624/all_the_gigs.html", text: "All the gigs, ever", aux: "(c. 1994-06-04)" },
+	{ uri: "/letters/19940624/top_20.html", text: "Top 20 Gigs", aux: "(c. 1994-06-04)" },
+];
 
 const Letter = () =>  {
 	return ( <>
 		<Header section='letters' title='6Jun94' />
-		<div className="letter">
-			<center>
-				<br /><a href="/letters/19940624/intro.html"> Introduction</a>
-				<br /><a href="/letters/19940624/all_the_gigs.html"> All the gigs, ever</a> (circa)
-				<br /><a href="/letters/19940624/top_20.html"> Top 20 Gigs</a> (circa)
-				<p /><Image width={270} height={122} alt="[signature]" src="https://jazzbutcher.com/images/19940624/94Jun24_sig.gif" />
-			</center>
+		<div className="etc">
+			<div className="flex flex-wrap justify-around">
+				<div className="min-w-[50%]">
+					{topics.map(MakeSimpleURI)}
+				</div>
+				<div className="m-1">
+					<center>
+						<p /><Image width={270} height={122} alt="[signature]" src="https://jazzbutcher.com/images/19940624/94Jun24_sig.gif" />
+					</center>
+				</div>
+			</div>
 		</div>
 		<Footer />
 	</>)

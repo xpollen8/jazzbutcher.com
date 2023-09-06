0ee4ff1c420bddc480615f10fb234bef7a64df67
xpollen8 <github@xpollen8.com>
2023-09-06 11:41:54 -0700

    dw: use a map

--- v2/src/app/letters/19931019/page.tsx
+++ v2/src/app/letters/19931019/page.tsx
@@ -1,35 +1,35 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
+
+const topics = [
+	{ uri: "/letters/19931019/intro", text: "Introduction" },
+	{ uri: "/letters/19931019/eider", text: "Where Is Eider Man?" },
+	{ uri: "/letters/19931019/starpoint", text: "Starpoint Studio" },
+	{ uri: "/letters/19931019/college", text: "College" },
+	{ uri: "/letters/19931019/fall", text: "JBC -vs- The Fall" },
+	{ uri: "/letters/19931019/post_eider", text: "More Maximiliana" },
+	{ uri: "/letters/19931019/rockuptibles", text: "Les Rockuptibles" },
+	{ uri: "/letters/19931019/chang", text: "President Chang" },
+	{ uri: "/letters/19931019/news", text: "News" },
+];
 
 const Letter = () =>
 <>
 	<Header section='letters' title='19Oct93'/>
-	<div className="letter">
-		<center>
-		<b>DUCK EVERYBODY!  THE OLD FOOL&apos;s COMING IN LINE AGAIN...</b>
-		<p />
-			<Link href="/letters/19931019/intro"> Introduction</Link>
-			<br />
-			<Link href="/letters/19931019/eider"> Where Is Eider Man?</Link>
-			<br />
-			<Link href="/letters/19931019/starpoint">Starpoint Studio</Link>
-			<br />
-			<Link href="/letters/19931019/college"> College</Link>
-			<br />
-			<Link href="/letters/19931019/fall"> JBC -vs- The Fall</Link>
-			<br />
-			<Link href="/letters/19931019/post_eider"> More Maximiliana</Link>
-			<br />
-			<Link href="/letters/19931019/rockuptibles"> Les Rockuptibles</Link>
-			<br />
-			<Link href="/letters/19931019/chang"> President Chang</Link>
-			<br />
-			<Link href="/letters/19931019/news"> News</Link>
-			<p />
-			<Image width={351} height={163} alt="[signature]" src="https://jazzbutcher.com/images/19931019/93Oct19_sig.gif" />
+	<div className="etc">
+		<center className="p-5">
+		<b className="text-2xl">DUCK EVERYBODY!  THE OLD FOOL&apos;s COMING IN LINE AGAIN...</b>
 		</center>
+		<div className="flex flex-wrap justify-around">
+			<div className="min-w-[50%]">
+				{topics.map(MakeSimpleURI)}
+			</div>
+			<div className="mt-10">
+				<Image width={351} height={163} alt="[signature]" src="https://jazzbutcher.com/images/19931019/93Oct19_sig.gif" />
+			</div>
+		</div>
 	</div>
 	<Footer />
 </>

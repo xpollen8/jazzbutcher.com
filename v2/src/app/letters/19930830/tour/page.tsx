d227df4b795d1045e2db2592283c61d0a228a695
xpollen8 <github@xpollen8.com>
2023-09-05 17:23:40 -0700

    dw: clean up on isle

--- v2/src/app/letters/19930830/tour/page.tsx
+++ v2/src/app/letters/19930830/tour/page.tsx
@@ -10,59 +10,60 @@ const Letter = () =>  {
 		<Header section='letters' title={title} />
 		<div className="letter">
 			<center>
-				<b>1993 EUROPEAN TOUR REPORT</b>
+				<b>1993 EUROPEAN TOUR REPORT</b>{' '}
 			</center>
-			<div className="p-5"/>
+			<b>15.2</b>{' '}
 			<blockquote>
-			<ul>
-			<li><b>15.2</b>{' '}
 				JBC gets on the bus at 5am {pete} is roaring drunk.  {tony} considers bashing him.  Drive and float to Paris.
 				French tour promoter is young and practically monolingual.
 				Tony considers belting him.  JBC play four live songs
-				and executes
-				<Link href="/gigs/1993/Feb15.html">lengthy interview
+				and executes <Link href="/gigs/1993/Feb15.html">lengthy interview
 				all live on Bernard Lenoir, national radio</Link>.
 				Dooj falls asleep.  JBC buy beers and
 				rehearse in a garage until two or three in the morning,
 				with friends who skin up.  Tony considers marrying them.
 				A long day.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb16.html"><b>16.2</b>
+			</blockquote>
+			<Link href="/gigs/1993/Feb16.html"><b>16.2</b>{' '}
 				Paris - <b>Theatre L&apos;European</b></Link>
+			<blockquote>
 				Seated; 300 people, some drunks.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb17.html"><b>17.2</b>
-				Bordeaux - <b>Le Jimmy</b></Link> 650km.  drive to Bordeaux.
+			</blockquote>
+			<Link href="/gigs/1993/Feb17.html"><b>17.2</b>{' '}
+				Bordeaux - <b>Le Jimmy</b></Link>
+			<blockquote>
+				650km.  drive to Bordeaux.
 				Wake up at the back, you&apos;re on tour.  {joe} gets
 				in massive ruck with everyone in the restaurant at dinner.
 				Tony considers judging him.  <b>Le Jimmy</b> (yes!)
 				is a punky club with about 200 souls.  We rock.  Many
 				drunks.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb18.html"><b>18.2</b>
-				Nates - <b>Floride</b></Link> Tiny club, fragile heads.
+			</blockquote>
+			<Link href="/gigs/1993/Feb18.html"><b>18.2</b>{' '}
+				Nates - <b>Floride</b></Link>
+			<blockquote>
+				Tiny club, fragile heads.
 				Lead singer buys cakes for all.  What a swell guy.  Pete
 				makes the first of many doomed sexual overtures, later
 				asks promoter for girl&apos;s address.  Pat, Kathie, Joe
 				and Dooj stay at the Holiday Inn.  Tony, Nick and Pete
 				stay in small concentration camp above the club, which
 				was severely overloaded at 150 people.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb19.html"><b>19.2</b>
-				Rennes - <b>Ubu</b></Link> Short drive today, which is
+			</blockquote>
+			<Link href="/gigs/1993/Feb19.html"><b>19.2</b>{' '}
+				Rennes - <b>Ubu</b></Link>
+			<blockquote>
+				Short drive today, which is
 				fortunate as Tony succeeds in running out of petrol
 				today.  Tony considers bashing everybody.  T.V. Smith
 				of the Adverts opens, solo and acoustic.  He is great.
 				Some 150 customers, the show a little dull, but a great
 				party afterwards.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb20.html"><b>20.2</b>
-				Lorient - <b>Le Manege Enchante</b></Link> (that&apos;s the
+			</blockquote>
+			<Link href="/gigs/1993/Feb20.html"><b>20.2</b>{' '}
+				Lorient - <b>Le Manege Enchante</b></Link>
+			<blockquote>
+				(that&apos;s the
 				&quot;Magic Roundabout&quot;, you Brits).  Beer for breakfast.
 				Beer for lunch.  This is more like it.  Joe fails to
 				get delays moving at soundcheck.  Pat considers having
@@ -74,17 +75,19 @@ const Letter = () =>  {
 				support group.  JBC deliver first really hot show of
 				the tour. Only partially morose.  250 in the house.
 				Some drunks, chief among them Tony and Pat.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb21.html"><b>21.2</b>
-				Morlaix - <b>Le Coatelan</b></Link> Onstage at 8:00 pm
+			</blockquote>
+			<Link href="/gigs/1993/Feb21.html"><b>21.2</b>{' '}
+				Morlaix - <b>Le Coatelan</b></Link>
+			<blockquote>
+				Onstage at 8:00 pm
 				on a sunday in an ART SHED near a village in French
 				Wales.  50 punters.  Brilliant. Dinner of the tour follows.
-			</li>
-			<div className="p-5"/>
-			<li><b>22.2</b>	Drive back to Paris.  See Pascal.  Get paid.  </li>
-			<div className="p-5"/>
-			<li className="flex flex wrap">
+			</blockquote>
+			<b>22.2</b>{' '}
+			<blockquote>
+				Drive back to Paris.  See Pascal.  Get paid.
+			</blockquote>
+			<blockquote className="flex flex wrap">
 			<div className="w-1/2">
 			<b>23.2</b>	Drive out of France very fast, as well
 				have all had enough.  Got to Breda in Holland.  Tony
@@ -96,10 +99,11 @@ const Letter = () =>  {
 				<Link href="https://jazzbutcher.com/images/19930223/euro93.jpg">
 					<Image style={{ float: 'right' }} width={500} height={500} alt="a pic" src="https://jazzbutcher.com/images/19930223/euro93_250.jpg" /></Link>
 			</div>
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb24.html"><b>24.2</b>
-				Breda - <b>Zanetti</b></Link> Nick has a snare drum and
+			</blockquote>
+			<Link href="/gigs/1993/Feb24.html"><b>24.2</b>{' '}
+				Breda - <b>Zanetti</b></Link>
+			<blockquote>
+				Nick has a snare drum and
 				some brushes.  The other three have guitars.  Everyone
 				sings as we play in a small stone bar with psychadelic
 				lights for about 25 people.  We&apos;re supposed to be in
@@ -107,100 +111,111 @@ const Letter = () =>  {
 				anyway.  We play <i>King Of Joy</i> and <i>Bad Moon
 				Rising</i>, but Kathie prevents us from rendering the
 				classic Red River Dave song the California Hippy Murders.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb25.html"><b>25.2</b>
-				Amsterdam - <b>Kremlin</b></Link> A hipster hangout in
+			</blockquote>
+			<Link href="/gigs/1993/Feb25.html"><b>25.2</b>{' '}
+				Amsterdam - <b>Kremlin</b></Link>
+			<blockquote>
+				A hipster hangout in
 				a scary and squalid part of town.  Small and full (100?150?)
 				and quite cheerful.  French drunks.  Drive back to Breda
 				cursing the gas stations that only work on credit cards.
 				Tony gets breath tested by The Man and comes up negative.
 				We all applaud as we drive away from the bewildered cops.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb26_2000.html"><b>26.2</b>
+			</blockquote>
+			<Link href="/gigs/1993/Feb26_2000.html"><b>26.2</b>{' '}
 				Breda - <b>Para</b></Link> and <Link href="/gigs/1993/Feb26_2300.html">
-				Breda - <b>Zanetti</b></Link> A double header.  We were
+				Breda - <b>Zanetti</b></Link>
+			<blockquote>
+				A double header.  We were
 				booked for Para (100 people.  Loud and vicious) but
 				we went on to <b>Zanetti</b> again to play in the basement,
 				a fabulous drugs cavern where we played not-really-acoustic
 				at-all for extra money and laffs.  Many drunks.  Deep joy.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb27.html"><b>27.2</b>
-				Hamburg - <b>Logo</b></Link> The first of four German dates
+			</blockquote>
+			<Link href="/gigs/1993/Feb27.html"><b>27.2</b>{' '}
+				Hamburg - <b>Logo</b></Link>
+			<blockquote>
+				The first of four German dates
 				supporting {belly}.
 				Nobody in Hamburg seems to know who they are.  350 people
 				in a smallish club.  We and they are great.  Quite forget
 				about Old Belly, who come on later and are not very
 				interested in a typically {_4AD} way.
 				Many drunks.  Some narcotics abuse.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Feb28.html"><b>28.2</b>
-				Berlin - <b>Loft</b></Link> Great journey to Berlin, wherein
-				Tony Pat and Kathie form <b>The New Government Of Russia</b>
+			</blockquote>
+			<Link href="/gigs/1993/Feb28.html"><b>28.2</b>{' '}
+				Berlin - <b>Loft</b></Link>
+			<blockquote>
+				Great journey to Berlin, wherein
+				Tony Pat and Kathie form <b>The New Government Of Russia</b>{' '}
 				(policies on application - $5,799.99).  Shoddy show
 				from the JBC but {belly} are more interesting tonight.  We like Gail
 				the bass player.  If anyone&apos;s worried about the two
 				singers they&apos;re being very adult about it.  400 people
 				in the house.  East German drunks.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar1.html"><b>1.3</b>
-				Munich - <b>Charterhalle</b></Link> Bus refuses to start.
+			</blockquote>
+			<Link href="/gigs/1993/Mar1.html"><b>1.3</b>{' '}
+				Munich - <b>Charterhalle</b></Link>
+			<blockquote>
+				Bus refuses to start.
 				Pete and Pat abandon rest of party in Berlin, get in
-				German tour manager&apos;s car and play Munich <b>Charterhalle</b>
+				German tour manager&apos;s car and play Munich <b>Charterhalle</b>{' '}
 				as a duo.  The &quot;club&quot; is the hall of Munich&apos;s only-just-disused
 				airport.  Intimate it isn&apos;t especially with only 200
 				people in this huge barn.  We all have fun, though,
 				and loot {belly}&apos;s booze n stuff ruthlessly.
 				Meanwhile, in Berlin, Joe fails to find the way to Cologne
 				for four hours.  Everyone, I&apos;m told, considers stabbing him.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar2.html"><b>2.3</b>
-				Cologne - <b>Luxor</b></Link> We arrive to find that {belly}&apos;s
+			</blockquote>
+			<Link href="/gigs/1993/Mar2.html"><b>2.3</b>{' '}
+				Cologne - <b>Luxor</b></Link>
+			<blockquote>
+				We arrive to find that {belly}&apos;s
 				van has broken down.  We laugh and <Link
 				href="/press/94takeover.html"> send Tony in The Love
-				Bus to rescue them</Link>.  400 people, a really good show.
+				Bus to rescue them</Link>
+				.  400 people, a really good show.
 				Many drunks, some violent.
-			</li>
-			<div className="p-5"/>
-			<li><b>3.3</b>
+			</blockquote>
+			<b>3.3</b>{' '}
+			<blockquote>
 				We drive to Freiburg.  On the way, entirely by chance,
 				we pull in at what must be the only vegetarian motorway
 				restaurant in Europe.  Deep joy, snow, vodka, Freiburg,
 				pizza, bliss.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar4.html"><b>4.3</b>
-				Valenza Po - <b>Circolo Palomar</b></Link> Long scary drive
+			</blockquote>
+			<Link href="/gigs/1993/Mar4.html"><b>4.3</b>{' '}
+				Valenza Po - <b>Circolo Palomar</b></Link>
+			<blockquote>
+				Long scary drive
 				over the Alps to Italy.  50 people watch us play for
 				over two hours at a wonderful club.  The chap on stage
 				doing the handstands and shit is the promoter.  The
 				hotel has llamas.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar5.html"><b>5.3</b>
-				Florence - <b>Auditorium Flog</b></Link> (No, honest!).
+			</blockquote>
+			<Link href="/gigs/1993/Mar5.html"><b>5.3</b>{' '}
+				Florence - <b>Auditorium Flog</b></Link>
+			<blockquote>
+				(No, honest!).
 				800 in a huge discotheque and a rider that would embarrass
 				the support group at the Rock Garden.  No wonder the
 				singer punched out the toilet doors in the dressing
 				room.  This offers the cue for the release of general
 				mayhem, not helped by the Hotel Putana where we have
 				been put.  Great concert, insulting promoters, bloody night.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar6.html"><b>6.3</b>
-				Forli - <b>Ex Macchina</b></Link> Small, communist-run
+			</blockquote>
+			<Link href="/gigs/1993/Mar6.html"><b>6.3</b>{' '}
+				Forli - <b>Ex Macchina</b></Link>
+			<blockquote>
+				Small, communist-run
 				club, the opposite of last night, and all the more pleasing.
 				150 people, many drunks, and a support group something
 				like {haus}.  More drunks.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar7.html"><b>7.3</b>
-				Salzburg - <b>Nonntal</b></Link> Pete, acting on an internal
+			</blockquote>
+			<Link href="/gigs/1993/Mar7.html"><b>7.3</b>{' '}
+				Salzburg - <b>Nonntal</b></Link>
+			<blockquote>
+				Pete, acting on an internal
 				agenda that has led him to drink solidly for 72 hours,
 				disappears in the middle of the night to devote his
 				time to serving drinks on British Airways.  &quot;A little
@@ -217,37 +232,40 @@ const Letter = () =>  {
 				says the promoter.  Smiling, I make another phone call.
 				Promoter buys us all an elaborate dinner.  Tony considers
 				sloshing him.
-			</li>
-			<div className="p-5"/>
-			<li><b>8.3</b>
+			</blockquote>
+			<b>8.3</b>{' '}
+			<blockquote>
 				We have a day off in Salzburg, which is all it takes.
 				I stay in bed till five, have a meal in the Styrian
 				bar, pick up our secret weapon and go back to the Styrian
 				bar, where I meet the group and introduce them to {lix}.
 				Jagermeister.  Drunks.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar9.html"><b>9.3</b>
-				Lienz - <b>Glocklturm</b></Link> This is where it gets
+			</blockquote>
+			<Link href="/gigs/1993/Mar9.html"><b>9.3</b>{' '}
+				Lienz - <b>Glocklturm</b></Link>
+			<blockquote>
+				This is where it gets
 				Really Weird.  The Love Bus finally clocks out at lunchtime,
 				and we travel the last 80km to the show on the back
 				of a tow-truck.  In the dark.  Through Alps.  The show
 				is somebody&apos;s 40th birthday surprise.  He has one leg.
 				Whoooa.  Lix makes his live JBC debut.  Very loud.  Very
 				exciting.  All drunk.  The whole town.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar10.html"><b>10.3</b>
-				Innsbruck - <b>Utopia</b></Link> 200 people.  Back in the
+			</blockquote>
+			<Link href="/gigs/1993/Mar10.html"><b>10.3</b>{' '}
+				Innsbruck - <b>Utopia</b></Link>
+			<blockquote>
+				200 people.  Back in the
 				hotel where we were previously narrowly prevented from
 				stealing the ducks.  No sign of any ducks this time.
 				Dinner in the rudest restaurant in the world.  All think
 				about biffing the propriator, but don&apos;t because he&apos;s
 				Turkish and it wouldn&apos;t do to be misunderstood.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar11.html"><b>11.3</b>
-				Vienna - <b>Szene Wien</b></Link> 300 people (more?)  Opening
+			</blockquote>
+			<Link href="/gigs/1993/Mar11.html"><b>11.3</b>{' '}
+				Vienna - <b>Szene Wien</b></Link>
+			<blockquote>
+				300 people (more?)  Opening
 				act is one &quot;Jac&quot;, a lone 19 year old Austrian who is
 				brilliant.  JBC follow him on and open with {girls}, which
 				Lix has just remembered a few minutes ago.  It, and
@@ -255,10 +273,11 @@ const Letter = () =>  {
 				and by the end of the night I am fairly sure that this
 				was the best JBC show ever.  Many drunks, nightclubs.
 				Kathie, Lix and I end up in the hotel manager&apos;s apartment.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar12.html"><b>12.3</b>
-				Ebensee - <b>Kino</b></Link> Ebensee is reached down a
+			</blockquote>
+			<Link href="/gigs/1993/Mar12.html"><b>12.3</b>{' '}
+				Ebensee - <b>Kino</b></Link>
+			<blockquote>
+				Ebensee is reached down a
 				lakeside road with bizarre gothic tunnels cut through
 				forbidding rock faces, all decorated with ancient coats
 				of arms.  &quot;Castle?  There is no castle HERE...&quot;  Dooj
@@ -266,35 +285,36 @@ const Letter = () =>  {
 				Gerard Langley&apos;s silly mic-stand, which he left here.
 				We still have it and taunt him with it at regular intervals.
 				Thousands of drunks.
-			</li>
-			<div className="p-5"/>
-			<li><Link href="/gigs/1993/Mar13.html"><b>13.3</b>
-				Dornbirn - <b>Spielboden</b></Link> We close the tour in
+			</blockquote>
+			<Link href="/gigs/1993/Mar13.html"><b>13.3</b>{' '}
+				Dornbirn - <b>Spielboden</b></Link>
+			<blockquote>
+				We close the tour in
 				one of our favourite venues.  Two guitars and one amplifier
 				loaded on the Love Bus in urgent need of some professional
 				maintenance.  Notable for acapella rendition of <i>Winter
 				Wonderland</i> enlivened by guitar roadie&apos;s fetching
 				alto harmony.  She is not, however, yet expected to
 				appear onstage with a lot of keyboards.  Drunks.
-			</li>
-			<div className="p-5"/>
-			<li><b>14.3</b>	GREAT ROAD JOURNEYS OF HISTORY: We leave
+			</blockquote>
+			<b>14.3</b>{' '}
+			<blockquote>
+				GREAT ROAD JOURNEYS OF HISTORY: We leave
 				Dornbirn, on the Austro/Swiss border after a lengthly
 				lunchtime beer session, during which it is agreed the
 				We Still Love The Old World.  We arrive in Breda on
-				the Dutch/Belgian border at midnight and go to <b>Zanetti&apos;s</b>
+				the Dutch/Belgian border at midnight and go to <b>Zanetti&apos;s</b>{' '}
 				to get irredeemably twisted.  We even stop for a proper
 				dinner on the way.  This is not possible, but we did
 				it anyway.
-			</li>
-			<div className="p-5"/>
-			<li><b>15.3</b>	Go to Belgium, but a lot of cheap beer,
+			</blockquote>
+			<b>15.3</b>{' '}
+			<blockquote>
+				Go to Belgium, but a lot of cheap beer,
 				miss two boats, go to the pub, get on the boat, get
 				drunk, get paid (I got &#163;27!), go home.
-			</li>
-			</ul>
 			</blockquote>
-			<div className="p-5"/>
+			<hr />
 			<blockquote>
 			VERDICT: Best European tour since <Link href="/gigs/1988/index.html">1988.</Link>
 				Not as violent
@@ -302,7 +322,6 @@ const Letter = () =>  {
 				we really did a lot to remind people how good this thing
 				can be, and the dates with Lix were Something Else.
 				Funny how things turn out.
-			<div className="p-5"/>
 			<br/><b>{tony}:</b> First Driver, Justice and Security
 			<br/><b>{kathie}:</b> Relief driver, guitars and backline tech
 			<br/><b>{joe}:</b> Sound tech

ef5df84996e7a45b291ec249040890ec86c72e01
xpollen8 <github@xpollen8.com>
2023-09-06 12:01:18 -0700

    dw: annotation and etc CSS

--- v2/src/app/letters/19931019/starpoint/page.tsx
+++ v2/src/app/letters/19931019/starpoint/page.tsx
@@ -7,7 +7,7 @@ const Letter = () =>  {
 	const title = <><Link href='/letters/19931019'>19Oct93</Link> | PRESIDENT CHANG</>
 	return ( <>
 		<Header section='letters' title={title} />
-		<div className="letter">
+		<div className="etc">
 			<b>&quot;STARPOINT STUDIO&quot;</b>
 			<p />
 			<samp>
@@ -18,21 +18,23 @@ const Letter = () =>  {
 				&gt; is the same as &quot;ReMaximum&quot; studios there?
 			</samp>
 			<p />
-			In fact, the place was called STARFORCE Studio, and it
-			was on St.  John&apos;s Hill just by Clapham Junction Station
-			(&quot;Welcome to Britain&apos;s busiest railway station!&quot;), more
-			or less next door to the venue now called The Grand.
-			It was very very cheap and slightly dodgy, an 8-track.
-			It was run by a gentleman called Martin K. Daley, the Prime
-			Minister Of Funk.  Other records made there in the early
-			eighties include many of the TV
-			Personalities&apos; early discs and a solo l.p. by a chap called 
-			Jedrech Dmokowski, who - oddly enough - is the brother
-			of the sweaty dancer chap in the {aero}.  Jed does a mean Marc Bolan
-			impression, and can be heard singing the chorus
-			on the Aeroplanes song <i>Tolerance</i>.  I really don&apos;t
-			know if Starforce was at any time known as &quot;ReMaximum&quot;,
-			but i sort of doubt it.
+			<blockquote classsName="annotation">
+				In fact, the place was called STARFORCE Studio, and it
+				was on St.  John&apos;s Hill just by Clapham Junction Station
+				(&quot;Welcome to Britain&apos;s busiest railway station!&quot;), more
+				or less next door to the venue now called The Grand.
+				It was very very cheap and slightly dodgy, an 8-track.
+				It was run by a gentleman called Martin K. Daley, the Prime
+				Minister Of Funk.  Other records made there in the early
+				eighties include many of the TV
+				Personalities&apos; early discs and a solo l.p. by a chap called 
+				Jedrech Dmokowski, who - oddly enough - is the brother
+				of the sweaty dancer chap in the {aero}.  Jed does a mean Marc Bolan
+				impression, and can be heard singing the chorus
+				on the Aeroplanes song <i>Tolerance</i>.  I really don&apos;t
+				know if Starforce was at any time known as &quot;ReMaximum&quot;,
+				but i sort of doubt it.
+			</blockquote>
 		</div>
 		<Footer />
 	</>)

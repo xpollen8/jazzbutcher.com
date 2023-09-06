0b5d5aebb5291d5a5cfec4bd04b70cb4639afeb6
xpollen8 <github@xpollen8.com>
2023-09-06 11:20:12 -0700

    dw: 'etc' class mods

--- v2/src/app/letters/artwork/page.tsx
+++ v2/src/app/letters/artwork/page.tsx
@@ -1,47 +1,46 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import Tag from '@/components/Tag';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
 
 const Artwork = () => 
 <>
 	<Header section='letters' title='Pat Fish Sketches' />
-	<div className="letter">
-		<h2>Scanned from:</h2>
-		<p>
-			<Link href="https://jazzbutcher.com/images//advert.gif">
-				<Image width={195} height={223} alt="[advert]" src="https://jazzbutcher.com/images//sadvert.gif" />
-			</Link>
-		</p>
-		<p>
-				<Link href="https://jazzbutcher.com/images//notes.gif"> Happy Notes</Link> and
-				<Link href="https://jazzbutcher.com/images//HappyNotes.gif"> processed</Link>
-				<Link href="https://jazzbutcher.com/images//sky.gif"> Happy Clouds</Link>
-				<br/><Link href="https://jazzbutcher.com/images//them.gif"> Happy Cassettes</Link>
-				<br/><Link href="https://jazzbutcher.com/images//fish.gif"> Somber Fish</Link>
-		</p>
-	</div>
-	<div className="letter">
-		<h2>Scanned from:</h2>
-		<p>
-			<Link href="https://adjective.com/images/eg/pmurphy.jpg">
-				<Image width={201} height={223} alt="[image]" src="https://adjective.com/images/eg/spmurphy.gif" />
-			</Link>
-		</p>
-		
-		<p>
-				<Link href="https://adjective.com/images/eg/eg.gif"> Happy Eg(g)</Link>
-				<br/>
-				<Link href="https://adjective.com/images/eg/flag_bw.gif"> Tutto Possibile Flag</Link>
-				and
-				<Link href="https://adjective.com/images/eg/flag.gif">In Color</Link>
-				<br/>
-				<Link href="https://adjective.com/images/eg/blackeg.gif">&quot;The Black Eg&quot;</Link>
-				and
-				<Link href="https://adjective.com/images/eg/bigblackeg.gif">bigger</Link>
-				<br/><Link href="https://adjective.com/images/eg/pmurphy.gif">Color Flag &amp; Eg</Link>
-		</p>
+	<div className="etc">
+		<div className="flex flex-wrap justify-around border pt-5">
+			<div className="m-1">
+				<Link href="https://jazzbutcher.com/images/advert.gif">
+					<Image width={400} height={400} alt="[advert]" src="https://jazzbutcher.com/images//advert.gif" />
+				</Link>
+			</div>
+			<div className="min-w-[50%]">
+				<b className="text-2xl">Extracted Images:</b>
+				<MakeSimpleURI uri="https://jazzbutcher.com/images//notes.gif" text="Happy Notes" />
+				<MakeSimpleURI uri="https://jazzbutcher.com/images//HappyNotes.gif" text="Happy Notes (processed)" />
+				<MakeSimpleURI uri="https://jazzbutcher.com/images//sky.gif" text="Happy Clouds" />
+				<MakeSimpleURI uri="https://jazzbutcher.com/images//them.gif" text="Happy Cassettes" />
+				<MakeSimpleURI uri="https://jazzbutcher.com/images//fish.gif" text="Somber Fish" />
+			</div>
+		</div>
+		<hr />
+		<p />
+		<div className="flex flex-wrap justify-around border" pt-5>
+			<div className="m-1">
+				<Link href="https://adjective.com/images/eg/pmurphy.jpg">
+					<Image width={400} height={400} alt="[image]" src="https://adjective.com/images/eg/pmurphy.jpg" />
+				</Link>
+			</div>
+			<div className="min-w-[50%]">
+				<b className="text-2xl">Extracted Images:</b>
+				<MakeSimpleURI uri="https://adjective.com/images/eg/eg.gif" text="Happy Eg(g)" />
+				<MakeSimpleURI uri="https://adjective.com/images/eg/flag_bw.gif" text="Tutto Possibile Flag" />
+				<MakeSimpleURI uri="https://adjective.com/images/eg/flag.gif" text="Tutto Possibile Flag (color)" />
+				<MakeSimpleURI uri="https://adjective.com/images/eg/blackeg.gif" text="The Black Eg" />
+				<MakeSimpleURI uri="https://adjective.com/images/eg/bigblackeg.gif" text="The Black Eg (bigger)" />
+				<MakeSimpleURI uri="https://adjective.com/images/eg/pmurphy.gif" text="Color Flag &amp; Eg" />
+			</div>
+		</div>
 	</div>
 	<Footer />
 </>

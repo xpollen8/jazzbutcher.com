b714ac414a439dda920a22181578304b8b9647cf
xpollen8 <github@xpollen8.com>
2023-09-06 11:47:34 -0700

    dw: use a map and etc CSS

--- v2/src/app/letters/19940913/page.tsx
+++ v2/src/app/letters/19940913/page.tsx
@@ -1,26 +1,34 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
+
+const topics = [
+	{ uri: "/letters/94Sep13/intro.html", text: "Introduction" },
+	{ uri: "/letters/94Sep13/gigs.html", text: "Recent Gig Reviews" },
+	{ uri: "/letters/94Sep13/lynch.html", text: "David Lynch -v- JBC" },
+	{ uri: "/letters/94Sep13/albums.html", text: "Re-Issues" },
+	{ uri: "/letters/94Sep13/harlan.html", text: "Harlan" },
+	{ uri: "/letters/94Sep13/lorre.html", text: "Peter Lorre" },
+	{ uri: "/letters/94Sep13/politics.html", text: "Politics" },
+	{ uri: "/letters/94Sep13/sis.html", text: "Sister Death" },
+	{ uri: "/letters/94Sep13/misc.html", text: "Odds-n-Ends" },
+	{ uri: "/letters/94Sep13/news.html", text: "News" },
+	{ uri: "/letters/94Sep13/next.html", text: "Up-coming gigs" },
+];
 
 const Letter = () =>  {
 	return ( <>
 		<Header section='letters' title='13Sep94' />
-		<div className="letter">
-			<center>
-				<Link href="/letters/94Sep13/intro.html"> Introduction</Link>
-				<br /><Link href="/letters/94Sep13/gigs.html"> Recent Gig Reviews</Link>
-				<br /><Link href="/letters/94Sep13/lynch.html"> David Lynch -v- JBC</Link>
-				<br /><Link href="/letters/94Sep13/albums.html"> Re-Issues</Link>
-				<br /><Link href="/letters/94Sep13/harlan.html"> Harlan</Link>
-				<br /><Link href="/letters/94Sep13/lorre.html"> Peter Lorre</Link>
-				<br /><Link href="/letters/94Sep13/politics.html"> Politics</Link>
-				<br /><Link href="/letters/94Sep13/sis.html"> Sister Death</Link>
-				<br /><Link href="/letters/94Sep13/misc.html"> Odds-n-Ends</Link>
-				<br /><Link href="/letters/94Sep13/news.html"> News</Link>
-				<br /><Link href="/letters/94Sep13/next.html"> Up-coming gigs</Link>
-				<p /><Image width={176} height={176} alt="[signature]" src="https://jazzbutcher.com/images/19940913/94Sep13_sig.gif" />
-			</center>
+		<div className="etc">
+			<div className="flex flex-wrap justify-around">
+				<div className="min-w-[50%]">
+					{topics.map(MakeSimpleURI)}
+				</div>
+				<div className="m-1">
+					<Image width={176} height={176} alt="[signature]" src="https://jazzbutcher.com/images/19940913/94Sep13_sig.gif" />
+				</div>
+			</div>
 		</div>
 		<Footer />
 	</>)

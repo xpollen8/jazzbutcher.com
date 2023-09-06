79805df44c243047ec143e4a8db1ba4fa1ea1efc
xpollen8 <github@xpollen8.com>
2023-09-06 11:52:42 -0700

    dw: annotation and etc CSS

--- v2/src/app/letters/19931019/fall/page.tsx
+++ v2/src/app/letters/19931019/fall/page.tsx
@@ -7,7 +7,7 @@ const Letter = () =>  {
 	const title = <><Link href='/letters/19931019'>19Oct93</Link> | THE FALL</>
 	return ( <>
 		<Header section='letters' title={title} />
-		<div className="letter">
+		<div className="etc">
 			<b>JBC -vs- The Fall</b>
 			<p />
 			<samp>
@@ -29,40 +29,42 @@ const Letter = () =>  {
 				&gt; Smith just havin&apos; fun? There&apos;s something here.  I know it.
 			</samp>
 			<p />
-			We put out the single {smith} in the autumn of 1983.  The
-			Fall&apos;s <b>C.R.E.E.P.</b>, with <i>Pat, Trip Dispenser</i>
-			on the b-side, came out about the same time.  Therefore,
-			I feel that it must have been recorded some time BEFORE
-			the Fall could have heard our single.  Our bassist at
-			the time (and on the single) was none other than {rolo}
-			({tops}).  Accordingly, the combination of the Fall title
-			and Smith&apos;s strangled cries of &quot;McGinty!&quot; on the record
-			did make for rather spooky listening, but I believe it&apos;s
-			just a co-incidence.  A little later we received a letter
-			from The Fall, from which we gathered that they dug our
-			record, and later we did quite a few dates supporting
-			them.  No one ever had sex with Brix (honest!), but she
-			did punch Mr Jones in the head once.  Coming off stage
-			at a show in <Link href="/gigs/1985/Mar7.html"><b>Hammersmith
-			Town Hall</b> in March &apos;85</Link>, {owen} was a touch peeved
-			about the fact that he&apos;d just had to play for forty five
-			minutes with no monitor mix worthy of the name, and,
-			in disgust, KICKED open the door to what he thought was
-			OUR dressing room.  Of course, it wasn&apos;t, and THAT is
-			how Owen got a smack in the mush from an astonished and
-			alarmed Brixie.  After she realised what had happened,
-			though, she chilled right out and gave him a BIG KISS.
-			<p />
-			{loony} has always been a Fall fan (well, you DO, don&apos;t
-			you?) and was very happy to start working with them a
-			couple of years ago, but I don&apos;t actually know how this
-			came to pass.  We&apos;re not really in touch with The Fall right now.
-			<p />
-			Incidentally, the last Fall l.p. not only has cover by
-			Pascal, it was also produced by Rex Sergent, who has
-			worked with the JBC as a live sound engineer on tours
-			in the UK and Spain.  He&apos;s ALSO the house sound engineer
-			for the Love Kittens.  But they&apos;ve split up now.
+			<blockquote className="annotation">
+				We put out the single {smith} in the autumn of 1983.  The
+				Fall&apos;s <b>C.R.E.E.P.</b>, with <i>Pat, Trip Dispenser</i>
+				on the b-side, came out about the same time.  Therefore,
+				I feel that it must have been recorded some time BEFORE
+				the Fall could have heard our single.  Our bassist at
+				the time (and on the single) was none other than {rolo}
+				({tops}).  Accordingly, the combination of the Fall title
+				and Smith&apos;s strangled cries of &quot;McGinty!&quot; on the record
+				did make for rather spooky listening, but I believe it&apos;s
+				just a co-incidence.  A little later we received a letter
+				from The Fall, from which we gathered that they dug our
+				record, and later we did quite a few dates supporting
+				them.  No one ever had sex with Brix (honest!), but she
+				did punch Mr Jones in the head once.  Coming off stage
+				at a show in <Link href="/gigs/1985/Mar7.html"><b>Hammersmith
+				Town Hall</b> in March &apos;85</Link>, {owen} was a touch peeved
+				about the fact that he&apos;d just had to play for forty five
+				minutes with no monitor mix worthy of the name, and,
+				in disgust, KICKED open the door to what he thought was
+				OUR dressing room.  Of course, it wasn&apos;t, and THAT is
+				how Owen got a smack in the mush from an astonished and
+				alarmed Brixie.  After she realised what had happened,
+				though, she chilled right out and gave him a BIG KISS.
+				<p />
+				{loony} has always been a Fall fan (well, you DO, don&apos;t
+				you?) and was very happy to start working with them a
+				couple of years ago, but I don&apos;t actually know how this
+				came to pass.  We&apos;re not really in touch with The Fall right now.
+				<p />
+				Incidentally, the last Fall l.p. not only has cover by
+				Pascal, it was also produced by Rex Sergent, who has
+				worked with the JBC as a live sound engineer on tours
+				in the UK and Spain.  He&apos;s ALSO the house sound engineer
+				for the Love Kittens.  But they&apos;ve split up now.
+			</blockquote>
 		</div>
 		<Footer />
 	</>)

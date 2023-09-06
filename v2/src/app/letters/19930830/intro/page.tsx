3fc2b308ab8b912936e2028573cdae81435e407f
xpollen8 <github@xpollen8.com>
2023-09-06 11:22:31 -0700

    dw: 'etc' class mods

--- v2/src/app/letters/19930830/intro/page.tsx
+++ v2/src/app/letters/19930830/intro/page.tsx
@@ -4,37 +4,40 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Nobr } from '@/lib/macros';
 import { AU, FIDDLER, } from '@/lib/defines';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
 
 const Letter = () =>  {
 	const title = <><Link href='/letters/19930830'>30Aug93</Link> | Introduction</>
 	return ( <>
 		<Header section='letters' title={title} />
-		<div className="letter">
-			<Link href="https://jazzbutcher.com/images/advert.gif">
-				<Image src="https://jazzbutcher.com/images/advert.gif" width={250} height={250} alt="[advert]"
-					style={{ float: "right", margin: "5px" }} />
-			</Link>
-			<p>
+		<div className="etc">
+			<blockquote className="annotation">
 				I have <Link href="/letters/19940624/all_the_gigs.html">
 				tapes of all manner of JBC live</Link>, tapes and recordings
 				of radio sessions, all line-ups, all periods, all qualities
 				of sound and performance.  I&apos;ll glady trade what you want,
 				but I ONLY WANT the following recordings:
-			</p>
-			<Nobr>
-			<ul>
-				<li><Link href="/gigs/1993/Mar11.html"><b>Szene Wien</b>, Vienna, {AU} - March 1993</Link></li>
-				<li><Link href="/gigs/1992/Jun5.html"><b>Roxy Theatre</b>, Hollywood, California - June 1992</Link></li>
-				<li><Link href="/gigs/1992/Jun13.html"><b>Liberty Lunch</b>, Austin, Texas - June 1992</Link></li>
-				<li><Link href="/gigs/1993/May15.html">{FIDDLER} Acoustic Room, London - May 1993</Link></li>
-				<li><Link href="/gigs/1992/Jun26.html"><b>Black Eg, Chicago Radio Session</b> - June 1992</Link></li>
-				<li><Link href="/gigs/1992/Jun3.html"><b>Sweetwater Saloon</b>, Los Osos, California - June 1992</Link></li>
-			</ul>
-			</Nobr>
-			<p>
-				If you can help, let me know what you want.
-			</p>
-			<div className="clear-both" />
+			</blockquote>
+			<div className="flex flex-wrap justify-around border pt-5">
+				<div className="min-w-[50%]">
+					<Nobr>
+						<MakeSimpleURI uri="/gigs/1993/Mar11.html" text={`Szene Wien, Vienna, ${AU} - March 1993`} />
+						<MakeSimpleURI uri="/gigs/1992/Jun5.html" text="Roxy Theatre, Hollywood, California - June 1992" />
+						<MakeSimpleURI uri="/gigs/1992/Jun13.html" text="Liberty Lunch, Austin, Texas - June 1992" />
+						<MakeSimpleURI uri="/gigs/1993/May15.html" text={`${FIDDLER} Acoustic Room, London - May 1993`} />
+						<MakeSimpleURI uri="/gigs/1992/Jun26.html" text="Black Eg, Chicago Radio Session - June 1992" />
+						<MakeSimpleURI uri="/gigs/1992/Jun3.html" text="Sweetwater Saloon, Los Osos, California - June 1992" />
+					</Nobr>
+					<p />
+					If you can help, let me know what you want.
+					<p />
+				</div>
+				<div className="m-1">
+					<Link href="https://jazzbutcher.com/images/advert.gif">
+						<Image src="https://jazzbutcher.com/images/advert.gif" width={400} height={400} alt="[advert]" />
+					</Link>
+				</div>
+			</div>
 		</div>
 		<Footer />
 	</>)

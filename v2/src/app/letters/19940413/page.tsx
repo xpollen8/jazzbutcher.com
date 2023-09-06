ac42d478ffcea4148297bf86fd0dc1e89683053b
xpollen8 <github@xpollen8.com>
2023-09-06 11:44:27 -0700

    dw: use a map and etc CSS

--- v2/src/app/letters/19940413/page.tsx
+++ v2/src/app/letters/19940413/page.tsx
@@ -4,29 +4,40 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { ParsedCaption } from '@/components/GenericWeb';
 import { FIDDLER, nick, curt, pat, dooj } from '@/lib/defines';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
+
+const topics = [
+	{ uri: "/letters/94Apr13/intro.html", text: "Introduction" },
+	{ uri: "/letters/94Apr13/tour.html", text: "The Spring 94 European Tour" },
+	{ uri: "/letters/94Apr13/garage.html", text: "The 10Mar94 Garage gig, London" },
+	{ uri: "/letters/94Apr13/fiddler.html", text: `The 8Apr94 ${FIDDLER} gig, London` },
+	{ uri: "/letters/94Apr13/gigs.html", text: "JBC Dates You Have Missed" },
+	{ uri: "/letters/94Apr13/news.html", text: "Other News" },
+];
 
 const Letter = () =>  {
 	return ( <>
 		<Header section='letters' title='13Apr94' />
-			<div className="letter">
-				<center>
-				<Link href="https://jazzbutcher.com/images/paris.jpg">
-					<Image width={400} height={400} src="https://jazzbutcher.com/images/paris.jpg" alt="paris group"/>
-				</Link>
-				<ParsedCaption
-					image_caption=<>{nick}, {curt}, {pat}, {dooj} (Paris)</>
-					credit_date="1994-02-17"
-					credit="Lucien Borderline"
-				/>
-				<p />
-					<Link href="/letters/94Apr13/intro.html"> Introduction</Link>
-					<p /><Link href="/letters/94Apr13/tour.html"> The Spring 94 European Tour</Link>
-					<p /><Link href="/letters/94Apr13/garage.html"> The 10Mar94 Garage gig, London</Link>
-					<p /><Link href="/letters/94Apr13/fiddler.html"> The 8Apr94 {FIDDLER} gig, London</Link>
-					<p /><Link href="/letters/94Apr13/gigs.html"> JBC Dates You Have Missed</Link>
-					<p /><Link href="/letters/94Apr13/news.html"> Other News</Link>
-					<p /><Image width={353} height={105} alt="[signature]" src="https://jazzbutcher.com/images/19940413/94Apr13_sig.gif" />
+		<div className="etc">
+			<div className="flex flex-wrap justify-around">
+				<div className="min-w-[50%]">
+					{topics.map(MakeSimpleURI)}
+				</div>
+				<div className="m-1">
+					<center>
+					<Link href="https://jazzbutcher.com/images/paris.jpg">
+						<Image width={400} height={400} src="https://jazzbutcher.com/images/paris.jpg" alt="paris group"/>
+					</Link>
+					<ParsedCaption
+						image_caption=<>{nick}, {curt}, {pat}, {dooj} (Paris)</>
+						credit_date="1994-02-17"
+						credit="Lucien Borderline"
+					/>
+					<p />
+					<Image width={353} height={105} alt="[signature]" src="https://jazzbutcher.com/images/19940413/94Apr13_sig.gif" />
 				</center>
+			</div>
+			</div>
 		</div>
 		<Footer />
 	</>)

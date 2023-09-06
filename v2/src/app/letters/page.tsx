d3fbba1756c9f7c0529b766a1efc96e3e0f6be93
xpollen8 <github@xpollen8.com>
2023-09-06 12:20:44 -0700

    dw: fix html quotes

--- v2/src/app/letters/page.tsx
+++ v2/src/app/letters/page.tsx
@@ -7,12 +7,12 @@ import MakeSimpleURI from '@/components/MakeSimpleURI';
 
 const letters = [
 	{ uri: "/letters/90Feb14", text: "1990-02-14", aux: "Cult Of The Basement" },
-	{ uri: "/letters/91Feb01", text: "1991-02-01", aux: "the &quot;Hunter S. Thompson&quot; letter" },
+	{ uri: "/letters/91Feb01", text: "1991-02-01", aux: 'the "Hunter S. Thompson" letter' },
 	{ uri: "/letters/93Aug30", text: "1993-08-30", aux: "The Albums, The Bands, LoveBus, 93 Euro Tour" },
 	{ uri: "/letters/93Oct19", text: "1993-10-19", aux: "Eider, The Fall, News" },
 	{ uri: "/letters/94Apr13", text: "1994-04-13", aux: "94 Euro Tour, Two Gigs, News, 93/94 Tours" },
 	{ uri: "/press/94stop_press.html", text: "1994-05-20", aux: "Press Release" },
-	{ uri: "/letters/94Jun24", text: "1994-06-24", aux: "All the gigs, Pat&apos;s Top 20 Gigs (circa)" },
+	{ uri: "/letters/94Jun24", text: "1994-06-24", aux: "All the gigs, Pat's Top 20 Gigs (circa)" },
 	{ uri: "/letters/94Sep13", text: "1994-09-13", aux: "Gig reviews, jbc-list rebuttals, etc" },
 	{ uri: "/letters/artwork.html", text: "1994", aux: "Sketches culled from the above letters" },
 	{ uri: "/press/20020709_starindia.html", text: "2002-07", aux: "The Star Of India: Pat reviews his favorite Restaurant" },

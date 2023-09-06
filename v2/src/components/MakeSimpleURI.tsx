bd56b3bc5dab7c13d865c5edae2729cbacddf83e
xpollen8 <github@xpollen8.com>
2023-09-06 11:49:40 -0700

    dw: make distinct from NavItem, used in letters section

--- v2/src/components/MakeSimpleURI.tsx
+++ v2/src/components/MakeSimpleURI.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link';
 
-const MakeSimpleURI = ({ uri, text }: {
-	uri?: string,
-	text: string,
+const MakeSimpleURI = ({ uri='', text, aux, children }: {
+	uri?: string
+	text: string
+	aux?: string
+	children?: React.ReactNode
 }): React.ReactNode  => (
-	<div className="menuDiv hover:outline">
-		{uri ? <Link href={uri}>{text}</Link> : <div>{text}</div>}
+	<div style={{ marginBottom: '-20px', marginRight: '10px' }}>
+	<Link href={uri}>
+		<div className="clickDiv hover:outline bg-white">
+			{text}
+			{' '}<span className="date">{aux}</span>
+		</div>
+	</Link>
+	{(children) && <div className="ml-5 mr-5 mb-10 -mt-4">{children}</div>}
 	</div>
 )
 

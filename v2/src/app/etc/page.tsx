4563ce703ba9d15360e46297a6da3f7f2208ea51
xpollen8 <github@xpollen8.com>
2023-09-06 15:58:45 -0700

    dw: added c. 2003

--- v2/src/app/etc/page.tsx
+++ v2/src/app/etc/page.tsx
@@ -68,7 +68,7 @@ const features = [
 const ETC = () => <>
 	<Header section='Etc' />
 	<div className="etc">
-		<LetterHeader title="A repository of older Jazz Butcher website features" />
+		<LetterHeader title="A repository of older Jazz Butcher website features" subhead="(c. 2003)" />
 		The Jazz Butcher website first appeared online in February, 1994 - making it
 		among the first 1,000 websites <b>in the world</b>. Over the years,
 		an enormous amount of disparate information has accummulated here (some 4235 pages

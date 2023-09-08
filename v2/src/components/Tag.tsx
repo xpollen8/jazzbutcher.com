33b882ea4d1193e6acf619ebc6f2a8b4c85c4015
xpollen8 <github@xpollen8.com>
2023-09-07 19:47:03 -0700

    dw: CSS name change

--- v2/src/components/Tag.tsx
+++ v2/src/components/Tag.tsx
@@ -1,6 +1,6 @@
 import LetterHeader from '@/components/LetterHeader';
 
-const Tag = ({ className, children }: { className?: any, children: React.ReactNode }) => <div className={(className) ? className : 'headingItem'}>{children}</div>
+const Tag = ({ className, children }: { className?: any, children: React.ReactNode }) => <div className={(className) ? className : 'tag'}>{children}</div>
 
 //const Tag = ({ className, children }: { className?: any, children: React.ReactNode }) => <div className={className}><LetterHeader title={children} /></div>
 

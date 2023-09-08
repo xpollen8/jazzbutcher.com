e5023a796c1b53c21dcab36e3ad41ff14d1461e7
xpollen8 <github@xpollen8.com>
2023-09-08 00:24:16 -0700

    dw: use swiper, not awesome-slider

--- v2/src/app/fishy_mansions/page.tsx
+++ v2/src/app/fishy_mansions/page.tsx
@@ -1,11 +1,15 @@
 "use client"
-import React, { Suspense } from 'react';
+
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import FishySession from '@/components/FishySession';
 import LetterHeader from '@/components/LetterHeader';
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
+
+import { Navigation, Scrollbar } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 
 const sessions = [
 	{
@@ -60,38 +64,24 @@ const FishyMansions = () =>
 	<Header section='pat' title="Fishy Mansions" />
 	<div className="layout">
 		<LetterHeader title="The Fishy Mansions Sessions" subhead="2020-2021" />
-		<center>
-			<blockquote>
-				During the COVID era, Pat streamed 9 performances from his home in Northampton..
-				<p />
-				We were able to liberate these shows from, er, Meta.
-				<p />
-				Enjoy
-			</blockquote>
-			<AwesomeSlider organicArrows={false} style={{  height: '680px', marginBottom: '70px', border: '1px solid black' }}>
-				{sessions.map((session: any, key: number) => (
-					<div key={key} className="flex flex-wrap justify-center bg-slate-200 py-5" style={{ background: '#e2e8f0', display: 'inline-block', overflow: 'scroll' }}>
-						<video
-							width={350}
-							controls={true}
-							preload="none"
-							className="video"
-							poster={session.poster}
-						>
-							<source src={session.mp4} type="video/mp4" />
-						</video>
-						<div className="bg-slate-200 p-5">
-						<center>
-							<Suspense fallback={<>Loading...</>}>
-								{FishySession(session, key)}
-							</Suspense>
-						</center>
-						</div>
-					</div>
-				))}
-			</AwesomeSlider>
-		</center>
-
+    <Swiper
+				style={{  height: '720px', border: '1px solid black', overflow: 'scroll' }}
+			modules={[Navigation, Scrollbar, ]}
+			navigation
+			scrollbar={{ draggable: true }}
+			pagination={{ clickable: true }}
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+			{sessions.map(( session: any, ordinal: number ) =>
+				<SwiperSlide key={ordinal}>
+					<FishySession session={{...session, ordinal}} />
+				</SwiperSlide>
+			)}
+		</Swiper>
+		<p />
 		<Footer />
 	</div>
 </>

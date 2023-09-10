2fa5157f4b5f0924a2d48e92073a8a314b9a66d6
xpollen8 <github@xpollen8.com>
2023-09-10 00:05:16 -0700

    dw: initial checkins

---
+++ v2/src/app/notebooks/page.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+const Notebooks = () => 
+<>
+	<Header section='pat' title="Excerpts from Pat's Notebooks" />
+		TODO
+	<Header />
+</>
+
+export default Notebooks;
+
+/*
+some content found at
+
+	~Desktop/20220222_Pat_Notebooks
+ */
---
+++ v2/src/app/prejbc/institution/page.tsx
@@ -0,0 +1,152 @@
+import Image from 'next/image';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+const Institution = () => 
+<>
+	<Header section="prejbc" title="The Tonix"/>
+	<div className="gig_thetonix">
+	<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheInstitution/Institution_Shirt_1.jpg" width={500} height={500} alt="shirt 1" />
+	<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheInstitution/Institution_Shirt_2.jpg" width={500} height={500} alt="shirt 2" />
+	<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheInstitution/Institution_Shirt_3.jpg" width={500} height={500} alt="shirt 3" />
+	Images from Philip Snow.
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/TheTonix_front.jpg" width={500} height={500} alt="sleev front" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/TheTonix_back.jpg" width={500} height={500} alt="sleev back" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/TheTonix_vinyl.jpg" width={500} height={500} alt="vinyl" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/tonix_A.jpg" width={500} height={500} alt="single A" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/tonix_B.jpg" width={500} height={500} alt="single B" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/tonix_wonder_world_of_glass.jpg" width={500} height={500} alt="wonderful world of glass" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/7+Tonix+1+(1).jpg" width={500} height={500} alt="live 1" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/7+Tonix+1+(2).jpg" width={500} height={500} alt="live 2" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/7+Tonix+1+(3).jpg" width={500} height={500} alt="live 3" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/7+Tonix+1+(4).jpg" width={500} height={500} alt="live 4" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/7+Tonix+1+(5).jpg" width={500} height={500} alt="live 5" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/7+Tonix+1+(6).jpg" width={500} height={500} alt="live 6" />
+<code>
+<br /> Jonathan Stephenson - https://www.youtube.com/channel/UCKfznmFizRjptR0VAw5Edgw
+<br />
+<br />	https://www.discogs.com/release/5338218-The-Institution-Jane-And-Jon
+<br />	7"er:
+<br />	A. https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_JaneAndJon.mp3
+<br />	B. https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_Stephanie.mp3
+<br />	Label:	Industrial Accident – SGS-112
+<br />	Format:	
+<br />	Vinyl, 7", 45 RPM, White Label
+<br />	Country:	UK
+<br />	Released:	1979
+<br />	Genre:	Rock, Reggae
+<br />	Style:	Post-Punk, Dub
+<br />	
+<br />	Credits
+<br />	Bass – Rolo (3)
+<br />	Drums – Rob Wilford
+<br />	Guitar – Max Eider
+<br />	Vocals, Saxophone – Pat Fish
+<br />	
+<br />	Issued on a hand-stamped white label in a plain white sleeve.
+<br />	
+<br />	Other original members included Johnathan Stephenson on Synthesizers and John Duvall on Drums.
+<br />	Both left before the record was cut.
+<br />
+<br />	youtube:
+<br />	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_JonathanStephenson_InnocentsAndEnglishmen.mp3
+<br />	https://www.youtube.com/watch?v=PdvFEe55_i0
+<br />	Nov 4, 2021  Probably our masterpiece!  Words of course, Patrick, sax Patrick, and I think the music was a joint effort.
+<br />
+<br />	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_JonathanStephenson_JaneAndJon.mp3
+<br />	https://www.youtube.com/watch?v=omFadRK4O4Q
+<br />	Oct 14, 2021  The Institution.  About 1978. Pat Fish, Rolo McGinty, Pete Millson, Jonathan Stephenson, John Duvall. This track by Pete.
+<br />
+<br />	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_JonathanStephenson_JeremyAiroplane.mp3
+<br />	https://www.youtube.com/watch?v=xoDh_HYROWs
+<br />	Oct 14, 2021  The Institution. About 1978. Pat Fish, Rolo McGinty, Pete Millson, Jonathan Stephenson, John Duvall. This track by Rolo.
+<br />
+<br />	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_JonathanStephenson_LoveAndARecordContract.mp3
+<br />	https://www.youtube.com/watch?v=CHwt3C4MkDQ
+<br />	Oct 14, 2021  The Institution. About 1978. Pat Fish, Rolo McGinty, Pete Millson, Jonathan Stephenson, John Duvall.  This track by Rolo
+<br />
+<br />	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_JonathanStephenson_Stephanie.mp3
+<br />	https://www.youtube.com/watch?v=umKEN-VcAsI
+<br />	Oct 14, 2021  The Institution. About 1978. About 1978. Pat Fish, Rolo McGinty, Pete Millson, Jonathan Stephenson, John Duvall.   This track:  Pat
+<br />
+<br />	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_JonathanStephenson_Vampire.mp3
+<br />
+<br />	internet:
+<br />	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/Institution/Institution_Unknown_InnocentsAndEnglishman.mp3
+</code>
+	</div>
+	<Footer />
+</>
+
+export default Institution;
+
+/*
+export default Institution;
+	${mkSimpleURI(text=Sonic Tonix)}
+
+	On Mon, 4 Sep 2023, Tom Peterson wrote:
+
+	“More grainy images from the distant past courtesy of my carpet-trousered friend Mr Warphole. This time the Sonic Tonix at
+	the Oranges & Lemons in St Clements, Oxford, plus some unusual dancing and a musical trio under plastic. Oh, and a
+	contemporaneous photographical artwork from the man himself.”
+
+	files: ~/Desktop/Jazz Butcher (unsorted)/2023-09-04-TomPeterson_SonicTonix
+
+
+#	https://www.discogs.com/release/2078217-Sonic-Tonix-Dont-Go-Away-Telepathy
+#	Label:	Industrial Accident –  SGS-114
+#	Format:	
+#	Vinyl, 7", Single, Test Pressing
+#	Country:	UK
+#	Released:	1980
+#	Genre:	Rock
+#	Style:	Power Pop, New Wave
+#	A		Don't Go Away
+#	Vocals – John Silver (4)
+#	B		Telepathy
+#	Vocals – Dave Goldie
+#	Bass, Vocals – John Silver (4)
+#	Drums, Vocals – Owen Jones
+#	Guitar, Vocals – Dave Goldie, Simon Mawby
+#	Photography By – Crispin Plaskitt
+#	Producer – Fish*, Tonix*
+#	Recorded at DUFFY'S.
+#	This was an Oxford-based outfit (ft. former members of INSTITUTION) who's sole release comes with rubber-stamped white labels (500 copies made). Members resurfaced in THE JAZZ BUTCHER and THE WOODENTOPS.
+#
+#	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_DontGoAway.mp3
+#	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_Telepathy.mp3
+
+	${mkSimpleURI(text=The Tonix)}
+#	https://www.discogs.com/release/2270038-The-Tonix-Strangers
+#	Label:	109 Product – STEG 002
+#	Format:	
+#	Vinyl, 7"
+#	Country:	UK
+#	Released:	1981
+#	Genre:	Rock
+#	Style:	New Wave
+#	A		Strangers
+#	B		Talk To Me
+#	Recorded At – Foel Studio
+#	Phonographic Copyright ℗ – Cherry Red Music Ltd.
+#	Pressed By – SNA
+#	Cover [Design] – Crispin Plaskitt
+#	Guitar, Backing Vocals – Max Eider
+#	Lacquer Cut By – Porky (5)
+#	Lead Vocals, Saxophone – Pat Fish
+#	Producer – Dave Anderson (3)
+#	Written-By, Recorded By, Producer – The Tonix
+#	Matrix / Runout (Side A runout etched): STEG 002 A1 SNA A PORKY PRIME CUT.
+#	Matrix / Runout (Side B runout etched): STEG 002 B1 SNA WELSHPOOL ... WOW HELLO K.B.
+#
+#	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/TheTonix_TalkToMe.mp3
+
+
+BLACK EG
+	files: ~/Desktop/Jazz Butcher (unsorted)/2018-08-31 - JBC - Kevin Carroll
+		unique black eg tracks
+			JBCBE00_Song Of The Homeland.mp3
+			JBCBE00_Ostrava Girls.mp3
+			JBCBE00_Indianapolis 500.mp3
+			JBCBE00_Yokohama.mp3
+*/

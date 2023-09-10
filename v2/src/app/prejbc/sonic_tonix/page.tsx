8e81ead3a1e9d77927534b301e4b43a0248a5a3f
xpollen8 <github@xpollen8.com>
2023-09-10 00:24:13 -0700

    dw: roughing stuff in

---
+++ v2/src/app/prejbc/sonic_tonix/page.tsx
@@ -0,0 +1,117 @@
+import Image from 'next/image';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+const Nightshift = () => 
+<>
+	<Header section="prejbc" title="Nightshift"/>
+	<div className="gig_nightshift">
+<code>
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_IMG_0175.jpg" width={500} height={500} alt="live" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_IMG_0176.jpg" width={500} height={500} alt="live" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_IMG_0177.jpg" width={500} height={500} alt="live" />
+<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_IMG_0178.jpg" width={500} height={500} alt="live" />
+  On Mon, 4 Sep 2023, Tom Peterson wrote:
+  “More grainy images from the distant past courtesy of my carpet-trousered friend Mr Warphole. This time the Sonic Tonix at
+  the Oranges & Lemons in St Clements, Oxford, plus some unusual dancing and a musical trio under plastic. Oh, and a
+  contemporaneous photographical artwork from the man himself.”
+
+<br/> https://www.discogs.com/release/2078217-Sonic-Tonix-Dont-Go-Away-Telepathy
+<br/> Label:  Industrial Accident –  SGS-114
+<br/> Format:
+<br/> Vinyl, 7", Single, Test Pressing
+<br/> Country:  UK
+<br/> Released: 1980
+<br/> Genre:  Rock
+<br/> Style:  Power Pop, New Wave
+<br/> A   Don't Go Away
+<br/> Vocals – John Silver (4)
+<br/> B   Telepathy
+<br/> Vocals – Dave Goldie
+<br/> Bass, Vocals – John Silver (4)
+<br/> Drums, Vocals – Owen Jones
+<br/> Guitar, Vocals – Dave Goldie, Simon Mawby
+<br/> Photography By – Crispin Plaskitt
+<br/> Producer – Fish*, Tonix*
+<br/> Recorded at DUFFY'S.
+<br/> This was an Oxford-based outfit (ft. former members of INSTITUTION) who's sole release comes with rubber-stamped white labels (500 copies made). Members resurfaced in THE JAZZ BUTCHER and THE WOODENTOPS.
+<br/>
+<br/> https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_DontGoAway.mp3
+
+</code>
+	</div>
+	<Footer />
+</>
+
+export default Nightshift;
+
+/*
+export default Institution;
+	${mkSimpleURI(text=Sonic Tonix)}
+
+	On Mon, 4 Sep 2023, Tom Peterson wrote:
+
+	“More grainy images from the distant past courtesy of my carpet-trousered friend Mr Warphole. This time the Sonic Tonix at
+	the Oranges & Lemons in St Clements, Oxford, plus some unusual dancing and a musical trio under plastic. Oh, and a
+	contemporaneous photographical artwork from the man himself.”
+
+	files: ~/Desktop/Jazz Butcher (unsorted)/2023-09-04-TomPeterson_SonicTonix
+
+
+#	https://www.discogs.com/release/2078217-Sonic-Tonix-Dont-Go-Away-Telepathy
+#	Label:	Industrial Accident –  SGS-114
+#	Format:	
+#	Vinyl, 7", Single, Test Pressing
+#	Country:	UK
+#	Released:	1980
+#	Genre:	Rock
+#	Style:	Power Pop, New Wave
+#	A		Don't Go Away
+#	Vocals – John Silver (4)
+#	B		Telepathy
+#	Vocals – Dave Goldie
+#	Bass, Vocals – John Silver (4)
+#	Drums, Vocals – Owen Jones
+#	Guitar, Vocals – Dave Goldie, Simon Mawby
+#	Photography By – Crispin Plaskitt
+#	Producer – Fish*, Tonix*
+#	Recorded at DUFFY'S.
+#	This was an Oxford-based outfit (ft. former members of INSTITUTION) who's sole release comes with rubber-stamped white labels (500 copies made). Members resurfaced in THE JAZZ BUTCHER and THE WOODENTOPS.
+#
+#	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_DontGoAway.mp3
+#	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/SonicTonix/SonicTonix_Telepathy.mp3
+
+	${mkSimpleURI(text=The Tonix)}
+#	https://www.discogs.com/release/2270038-The-Tonix-Strangers
+#	Label:	109 Product – STEG 002
+#	Format:	
+#	Vinyl, 7"
+#	Country:	UK
+#	Released:	1981
+#	Genre:	Rock
+#	Style:	New Wave
+#	A		Strangers
+#	B		Talk To Me
+#	Recorded At – Foel Studio
+#	Phonographic Copyright ℗ – Cherry Red Music Ltd.
+#	Pressed By – SNA
+#	Cover [Design] – Crispin Plaskitt
+#	Guitar, Backing Vocals – Max Eider
+#	Lacquer Cut By – Porky (5)
+#	Lead Vocals, Saxophone – Pat Fish
+#	Producer – Dave Anderson (3)
+#	Written-By, Recorded By, Producer – The Tonix
+#	Matrix / Runout (Side A runout etched): STEG 002 A1 SNA A PORKY PRIME CUT.
+#	Matrix / Runout (Side B runout etched): STEG 002 B1 SNA WELSHPOOL ... WOW HELLO K.B.
+#
+#	https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheTonix/TheTonix_TalkToMe.mp3
+
+
+BLACK EG
+	files: ~/Desktop/Jazz Butcher (unsorted)/2018-08-31 - JBC - Kevin Carroll
+		unique black eg tracks
+			JBCBE00_Song Of The Homeland.mp3
+			JBCBE00_Ostrava Girls.mp3
+			JBCBE00_Indianapolis 500.mp3
+			JBCBE00_Yokohama.mp3
+*/
---
+++ v2/src/app/project/black_eg/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+const BlackEg = () => 
+<>
+	<Header section="prejbc" title="Nightshift"/>
+	<div className="gig_nightshift">
+<code>
+<br />unique black eg tracks
+<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Yokohama.mp3
+<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Song Of The Homeland.mp3
+<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Ostrava Girls.mp3
+<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Indianapolis 500.mp3
+<br />source: Kevin Carroll 2018-08-31
+</code>
+	</div>
+	<Footer />
+</>
+
+export default BlackEg;

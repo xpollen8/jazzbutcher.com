1991266bc40c2c120b0b2a4497123ff8bcf0e4bb
xpollen8 <github@xpollen8.com>
2023-09-10 19:33:22 -0700

    dw: roughing in subsections

--- v2/src/app/press/page.tsx
+++ v2/src/app/press/page.tsx
@@ -1,11 +1,28 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import MakeSimpleURI from '@/components/MakeSimpleURI';
 
-const Missing = () => 
+const sections = [
+	{ uri: '/press/interviews', text: 'Published Interviews' },
+	{ uri: '/press/promos', text: 'Promotional Material' },
+	{ uri: '/press/albums', text: 'Album Reviews' },
+	{ uri: '/press/albums/pat', text: "Pat's Album Reviews" },
+	{ uri: '/press/reviews/gig', text: 'Published Gig Reviews' },
+	{ uri: '/press/reviews/fan', text: 'Fan Gig Reviews' },
+	{ uri: '/press/reviews/self', text: "Pat's Gig Reviews" },
+	{ uri: '/press/reviews/pat', text: "Pat Reviews Other Things" },
+	{ uri: '/press/obits', text: 'Rememberences' },
+	{ uri: '/press/audio', text: 'Audio Interviews' },
+	{ uri: '/press/black_eg', text: 'Black Eg' },
+	{ uri: '/press/wilson', text: 'Wilson' },
+	{ uri: '/press/sumosonic', text: 'Sumosonic' },
+];
+
+const Press = () => 
 <>
 	<Header section="press" />
-		<h1>This section is not yet ported from old JBC site</h1>
+		{sections.map(MakeSimpleURI)}
 	<Footer />
 </>
 
-export default Missing;
+export default Press;

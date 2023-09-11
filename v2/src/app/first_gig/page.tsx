acd01c9f4890c76eca6e1f34160ce1246a457b30
xpollen8 <github@xpollen8.com>
2023-09-10 23:23:23 -0700

    de: initial checkin

---
+++ v2/src/app/first_gig/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+const FirstGig = () => 
+<>
+	<Header section="pat" title="First Gig"/>
+	<blockquote>
+	This was posted by a gentleman named Chris Zero on The Jazz Butcher Facebook Group.
+	<p />
+	&quot;Patrick&apos;s first ever performance, 1974 Uppingham,
+	age 17, with &apos;Chris Zero&apos; and &apos;others&apos; inc. &apos;Rolo&apos; on synth.
+	<p />
+	Duval: Sat Oct  8 17:21:44 PDT 2022
+	</blockquote>
+	<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/images/first_gig/first_gig_1.jpg" width={500} height={500} alt="sleev front" />
+	<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/images/first_gig/first_gig_2.jpg" width={500} height={500} alt="sleev front" />
+	<Image src="https://s3.amazonaws.com/assets.jazzbutcher.com/images/first_gig/first_gig_3.jpg" width={500} height={500} alt="sleev front" />
+	<Footer />
+</>
+
+export default FirstGig;
---
+++ v2/src/app/rememberences/david_jackson/page.tsx
@@ -0,0 +1,81 @@
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+const DavidJackson = () =>
+<>
+	<Header section='rememberences' title="David Jackson" />
+		&quot;It was an honour and privilege to play with someone with so much talent.&quot;
+		<p />
+		By David Jackson
+		<p />
+		Friends and musicians have paid tribute to Pat Fish who sadly died last week, describing him as “a true artist” and “great songwriter”.
+		<p />
+		One of Northampton’s most influential figures during the 1980s, Pat formed and was also best known as The Jazz Butcher.
+		<p />
+		Born in London, Patrick Huntrods moved to Northampton after studying in Oxford, forming The Jazz Butcher in the early 1980s with Max Eider.
+		<p />
+		While he would continue to play under that moniker, it was Pat’s willingness to write, record and perform with others which meant the band’s line up featured dozens of musicians over the years.
+		<p />
+		The Jazz Butcher released a series of albums on Glass Records in the first half of the 1980s before they signed with Alan McGee’s Creation Records.
+		<p />
+		In a recent tribute to Fish on Instagram, label boss McGee said: “(Pat was) one of the first people who believed in Creation.
+		<p />
+		“At that point, 1986/1987, when we signed Pat, no one would come on Creation except unknown bands and he was a big deal.
+		<p />
+		“Him and Lawrence Hayward (Felt) - we got these guys and we could do international licensing deals and although he didn’t go on and sell millions, and he should have, he was part of the reason that we actually did well.”
+		<p />
+		Sometimes also known as The Jazz Butcher Conspiracy or The Jazz Butcher And His Sikkorskis From Hell, the band’s sound was rooted in indie and alternative.
+		<p />
+		The Jazz Butcher’s first album on Creation was 1988’s Fishcotheque and they went onto release numerous albums on the label.
+		<p />
+		McGee added: “You need these artists to build labels, and there were these guys, especially Pat, who were never about the money it was just about wanting to put his records out.
+		<p />
+		“I’m gutted for Pat. Such a good human being, such a true artist, such a great songwriter and such a nice bloke. Rest in peace.”
+		<p />
+		Aside from The Jazz Butcher, Pat also featured in bands such as Wilson, Black Eg, Sumosonic, Spacemen 3 and more recently The Drones Club.
+		<p />
+		Alex Novak, who runs Spiral Archive record shop, has played in numerous bands in Northampton over the past 30 years, including the Venus Fly Trap, Religious Overdose and Tempest.
+		<p />
+		He said musicians like Pat played a crucial part in the “cross fertilisation,” of acts in Northampton, which at the time included Bauhaus.
+		<p />
+		He said: “It was when Pat moved to Northampton that the genesis of The Jazz Butcher properly began.
+		<p />
+		“We were on the same label, I was in Religious Overdose and then Tempest and that’s where the whole connection started
+		<p />
+		“There was also the links with David J and Kevin Haskins from Bauhaus, there was a lot of cross fertilisation in the Northampton music scene at the time with Glass Records in the middle of it.
+		<p />
+		“As Tempest, we did quite a few gigs with The Jazz Butcher and then later on Pat played saxophone on one of the Venus Fly Trap records and produced Pandoras Box which we recorded in France.”
+		<p />
+		Mr Novak explained while some bands and musicians don’t work with others, it was the opposite for Pat, adding, “He didn’t have any siblings or children and I think other musicians became part of his close family.
+		<p />
+		“I think people get complacent and think people will be around for ever, it’s important we appreciate people while they&apos;re around.”
+		<p />
+		While Pat would continue to play as The Jazz Butcher, one of his more recent projects was as bassist in The Drones Club.
+		<p />
+		The Drones Club, which has its roots in the folk scene and play ancestral and improvised music, came together in 2014 and were formed by Ian Clabburn and Russell Cooper - the latter who Pat had previously played with in both Wilson and Sumosonic. Pat replaced the band&apos;s first bassist and they were later joined by guitarist Steve Gordon on guitar.
+		<p />
+		Mr Clabburn said “Pat was very generous with his expertise.
+		<p />
+		“It’s a great loss. There was no pretention, he took you at face value and there was never a subtext. “You can&apos;t say that about many people.
+		<p />
+		“It was Pat who took a disparate group of musicians and with good humour moulded them into a cohesive band with its own sound.
+		<p />
+		“I learned so much and feel that I am a better musician, for which I will be forever grateful.”
+		<p />
+		The Drones Club continued to meet weekly until Pat’s recent death, to rehearse, with Mr Clabburn admitting it was “often more of a social get together, peppered with Pat&apos;s apparently endless supply of anecdotes”.
+		<p />
+		“It was an honour and privilege to play with someone with so much talent, so much drive and so much determination. We will miss him deeply,” he added.
+		<p />
+		Besides performing music, Pat also hosted indie music show Transmission which was broadcast on ITV at the end of the 1980s.
+		<p />
+		In recent years, Fire Records had started an extensive series of Jazz Butcher reissues from his time on Glass and Creation records.
+		<p />
+		Pat had also become a regular performer online, hosting numerous gigs from ‘Fishy Mansions’.
+		<p />
+		It’s understood that Pat had also been working on a new album which is expected to be released in the future.
+		<p />
+		<b>David Jackson</b> - 2021-10-11
+	<Footer />
+</>
+
+export default DavidJackson;

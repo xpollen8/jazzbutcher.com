831360856fc26e0799d747c2dfac7888d4a11d73
xpollen8 <github@xpollen8.com>
2023-09-10 18:17:23 -0700

    dw: initial checkin

--- v2/src/app/video/page.tsx
+++ v2/src/app/video/page.tsx
@@ -1,11 +1,14 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
-const Missing = () => 
+const Video = () => 
 <>
 	<Header />
 	<h1>This section is not yet ported from old JBC site</h1>
+	<pre>
+	https://s3.amazonaws.com/assets.jazzbutcher.com/video/JazzButcherQuartet_NeverGiveUp.mp4
+	</pre>
 	<Footer />
 </>
 
-export default Missing;
+export default Video;

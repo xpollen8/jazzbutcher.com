9e5f48edbb406362abe799a88eb3770750f0a5db
xpollen8 <github@xpollen8.com>
2023-09-12 14:21:54 -0700

    dw: use Tribute component

--- v2/src/app/tributes/page.tsx
+++ v2/src/app/tributes/page.tsx
@@ -1,17 +1,11 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import MakeSimpleURI from '@/components/MakeSimpleURI';
-
-const entries = [
-	{ uri: '/tributes/david_jackson', text: 'David Jackson' },
-	{ uri: '/tributes/rolo_mcginty', text: 'Rolo McGinty', aux: 'Another World' },
-	{ uri: '/tributes/peter_astor', text: 'Peter Astor', aux: 'Fine & Dandy' },
-];
+import Tributes from '@/components/Tributes';
 
 const Rememberences = () => 
 <>
 	<Header section="memoriam" title="Tributes" />
-		{entries.map(MakeSimpleURI)}
+		<Tributes />
 	<Footer />
 </>
 

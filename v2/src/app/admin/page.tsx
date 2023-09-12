294cea7283706018fad28eaa3240fd59ac623a22
xpollen8 <github@xpollen8.com>
2023-09-11 22:04:55 -0700

    dw: added notes

--- v2/src/app/admin/page.tsx
+++ v2/src/app/admin/page.tsx
@@ -1,22 +1,29 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Attribution } from '@/components/GenericWeb';
 import MakeSimpleURI from '@/components/MakeSimpleURI';
 
 const options = [
-	{ uri: '/admin/edit/press', text: 'Edit Press' },
-	{ uri: '/admin/edit/conspirator', text: 'Edit Conspirator' },
-	{ uri: '/admin/edit/gig', text: 'Edit Gig' },
-	{ uri: '/admin/edit/release', text: 'Edit Release' },
-	{ uri: '/admin/edit/performance', text: 'Edit Performance' },
-	{ uri: '/admin/edit/song', text: 'Edit Song' },
-	{ uri: '/admin/edit/defines', text: 'Edit Global Defines' },
+	{ uri: '/admin/press', text: 'Manage Press', aux: 'possible backed tool for edit form data: https://github.com/vantezzen/auto-form' },
+	{ uri: '/admin/entity', text: 'Manage Entities' },
+	{ uri: '/admin/conspirator', text: 'Manage Conspirators' },
+	{ uri: '/admin/gig', text: 'Manage Gigs' },
+	{ uri: '/admin/release', text: 'Manage Releases' },
+	{ uri: '/admin/performance', text: 'Manage Performances' },
+	{ uri: '/admin/song', text: 'Manage Songs' },
+	{ uri: '/admin/defines', text: 'Manage Global defines' },
+	{ uri: '/admin/s3', text: 'Manage AWS S3 file storage', aux: "(Need to incoprorate https://www.npmjs.com/package/react-aws-s3)"  },
 ]
 
 const Admin = () => {
 	return (
 		<>
 			<Header section='admin' />
-			(WIP)
+			<blockquote>
+			Nothing here yet works, but am including to show intent
+			<p />
+			<Attribution g="David Whittemore" d="2023-09-11" />
+			</blockquote>
 			{options.map(MakeSimpleURI)}
 			<Footer />
 		</>

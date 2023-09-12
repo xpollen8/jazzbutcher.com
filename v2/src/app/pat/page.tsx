2e4bb577869d0ee0b3ec875dfddec0412ab58e24
xpollen8 <github@xpollen8.com>
2023-09-12 11:36:44 -0700

    dw: tpyo

--- v2/src/app/pat/page.tsx
+++ v2/src/app/pat/page.tsx
@@ -5,7 +5,7 @@ import MakeSimpleURI from '@/components/MakeSimpleURI';
 const sections = [
 	{ uri: '/first_gig', text: "Pat's first gig - 1974" },
 	{ uri: '/project', text: "Side projects during JBC years" },
-	{ uri: '/fishy_masions', text: "The 2020-2021 Fishy Mansions livestreams" },
+	{ uri: '/fishy_mansions', text: "The 2020-2021 Fishy Mansions livestreams" },
 	{ uri: '/letters', text: '"Letters From Pat" - 1990-1994' },
 	{ uri: '/notebooks', text: "Excerpts from Pat's notebooks" },
 	{ uri: '/memoriam', text: "In Memoriam" },

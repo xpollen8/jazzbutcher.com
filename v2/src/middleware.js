c347e7e76b90cb549f2553b596f0ad7a798b87af
xpollen8 <github@xpollen8.com>
2023-09-11 21:39:25 -0700

    dw: conditional matcher has me stumped

--- v2/src/middleware.js
+++ v2/src/middleware.js
@@ -2,5 +2,5 @@ import middleware from "next-auth/middleware";
 export default middleware;
 
 export const config = {
-	matcher: [(process.env.LOVAL_DEV) ? "/XXXadmin" : "/admin"],
+	matcher: ["/admin"],
 };

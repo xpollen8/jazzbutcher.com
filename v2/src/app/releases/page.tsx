a7ad087f9faf6237bf751dca8ac7031a1195c9e7
xpollen8 <github@xpollen8.com>
2023-09-12 21:06:18 -0700

    dw: added year divider

--- v2/src/app/releases/page.tsx
+++ v2/src/app/releases/page.tsx
@@ -4,12 +4,27 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import MakeAlbumBlurb from '@/components/MakeAlbumBlurb';
 import { ReleaseType } from '@/components/Release';
+import { parseYear } from '@/lib/macros';
+import Tag from '@/components/Tag';
 
 const Releases = async () => {
 	const releases = await apiData('releases');
 	return <>
 		<Header section="releases" />
-		{releases?.albums?.sort((a: ReleaseType, b: ReleaseType) => moment.utc(b.year).diff(moment.utc(a.year))).map(MakeAlbumBlurb)}
+		{(() => {
+			let year = 999;
+			return releases?.albums?
+				.sort((a: ReleaseType, b: ReleaseType) => moment.utc(b.year).diff(moment.utc(a.year)))
+				.map((item: any, key: number) => {
+					const yr = parseYear(item.year);
+					let tag = <></>;
+					if (yr !== year) {
+						tag = <Tag>{yr}</Tag>
+						year = yr;
+					}
+					return <>{tag}{MakeAlbumBlurb(item, key)}</>;
+				});
+		})()}
 		<Footer />
 	</>
 }

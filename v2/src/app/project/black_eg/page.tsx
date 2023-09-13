4552d28ba1cd47769792c057a1716bfc6affec39
xpollen8 <github@xpollen8.com>
2023-09-12 22:57:53 -0700

    dw: AudioLink all mp3 files

--- v2/src/app/project/black_eg/page.tsx
+++ v2/src/app/project/black_eg/page.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import LinkAudio from '@/components/LinkAudio';
 
 const BlackEg = () => 
 <>
 	<Header section="project" title="The Black Eg"/>
 	<div className="gig_eg" />
-<code>
-<br />unique black eg tracks
-<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Yokohama.mp3
-<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Song Of The Homeland.mp3
-<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Ostrava Girls.mp3
-<br />https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll__Indianapolis 500.mp3
-<br />source: Kevin Carroll 2018-08-31
-</code>
+<b>Unique black eg tracks</b>
+<p />source: Kevin Carroll 2018-08-31
+<p />
+<LinkAudio mp3="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Yokohama.mp3" title="Yokohama" />
+<LinkAudio mp3="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Song Of The Homeland.mp3" title="Song Of The Homeland" />
+<LinkAudio mp3="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Ostrava Girls.mp3" title="Ostrava Girls" />
+<LinkAudio mp3="https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Indianapolis 500.mp3" title="Indianapolis 500" />
 	<Footer />
 </>
 

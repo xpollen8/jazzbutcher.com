26a0ddab8ea3bdde5f31df06548f5d83800eb0f6
xpollen8 <github@xpollen8.com>
2023-09-13 23:15:25 -0700

    dw: handle non-uri case

--- v2/src/components/MakeSimpleURI.tsx
+++ v2/src/components/MakeSimpleURI.tsx
@@ -7,12 +7,19 @@ const MakeSimpleURI = ({ uri='', text, aux, children }: {
 	children?: React.ReactNode
 }, key: number): React.ReactNode  => (
 	<div key={key} style={{ marginBottom: '-20px', marginRight: '10px' }}>
-	<Link href={uri}>
-		<div className="clickDiv hover:outline bg-white">
+	{(uri) ?
+		<Link href={uri}>
+			<div className="clickDiv hover:outline bg-white">
+				{text}
+				{' '}<span className="date">{aux}</span>
+			</div>
+		</Link>
+		:
+		<div className="clickDiv" style={{ marginBottom: '30px' }}>
 			{text}
 			{' '}<span className="date">{aux}</span>
 		</div>
-	</Link>
+	}
 	{(children) && <div className="ml-5 mr-5 mb-10 -mt-4">{children}</div>}
 	</div>
 )

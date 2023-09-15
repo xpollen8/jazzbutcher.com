828c690c3d4afaca40ee849236413089a951b104
xpollen8 <github@xpollen8.com>
2023-09-14 18:13:15 -0700

    dw: more CSS futzing.

--- v2/src/components/MakeSimpleURI.tsx
+++ v2/src/components/MakeSimpleURI.tsx
@@ -6,21 +6,21 @@ const MakeSimpleURI = ({ uri='', text, aux, children }: {
 	aux?: string
 	children?: React.ReactNode
 }, key: number): React.ReactNode  => (
-	<div key={key} style={{ marginBottom: '-20px', marginRight: '10px' }}>
+	<div key={key} className="mt-2 p-1">
 	{(uri) ?
-		<Link href={uri}>
+		<Link href={uri} style={{ display: 'inline-block', width: '100%' }}>
 			<div className="clickDiv hover:outline bg-white">
 				{text}
 				{' '}<span className="date">{aux}</span>
 			</div>
 		</Link>
 		:
-		<div className="clickDiv bg-white" style={{ marginBottom: '30px' }}>
+		<div className="clickDiv bg-white" style={{ marginBottom: '20px' }}>
 			{text}
 			{' '}<span className="date">{aux}</span>
 		</div>
 	}
-	{(children) && <div className="ml-5 mr-5 mb-10 -mt-4">{children}</div>}
+	{(children) && <div className="p-2">{children}</div>}
 	</div>
 )
 

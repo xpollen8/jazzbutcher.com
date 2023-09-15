c27e9b6990e2b5dae2bcf5ee30c8c9be1d725112
David Whittemore <git@xpollen8.com>
2023-09-15 04:44:24 +0000

    dw: schema update

--- v1/htdb/SQL/media.sql
+++ v1/htdb/SQL/media.sql
@@ -3,6 +3,8 @@ drop table if exists media;
 create table media (
 	media_id mediumint unsigned NOT NULL auto_increment,
 	type varchar(20) NOT NULL,
+	project varchar(20) NOT NULL,
+	collection varchar(200) NOT NULL,
 	name varchar(200) NOT NULL,
 	comment varchar(200) NOT NULL,
 	mp3 varchar(200) NOT NULL,
@@ -11,5 +13,6 @@ create table media (
 	dtcreated datetime not null default NOW(),
 	INDEX idx0(media_id),
 	INDEX idx1(type),
-	UNIQUE INDEX idx2(name, mp3, href)
+	INDEX idx2(href),
+	UNIQUE INDEX idx3(project, collection, mp3, href)
 );

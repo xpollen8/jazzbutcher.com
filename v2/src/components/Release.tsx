d6b60f94ebc3acfeb3d4cb533a18153f661bee0d
xpollen8 <github@xpollen8.com>
2023-09-15 23:46:04 -0700

    dw: bring in more sections and make pretty

--- v2/src/components/Release.tsx
+++ v2/src/components/Release.tsx
@@ -2,7 +2,8 @@
 
 import { Suspense } from 'react';
 
-import MakeAlbumBlurb from './MakeAlbumBlurb';
+import Tag from '@/components/Tag';
+import MakeAlbumBlurb from '@/components/MakeAlbumBlurb';
 import useRelease from '@/lib/useRelease';
 import { AutoLinkPlayer, AutoLinkSong } from '@/lib/defines';
 
@@ -16,6 +17,7 @@ export type ReleaseType =  {
 	dtrecorded?: string
 	studio?: string
 	liner?: string
+	thanks?: string
 	buy?: string
 	project?: string
 	lookup?: string
@@ -46,6 +48,93 @@ const PerformanceCredits = ({ who, album_credits, song_credits }: { who: string,
 	</>)
 }
 
+const ReleaseSongList = ({ songs }: { songs: any[] }) => {
+	if (!songs?.length) return;
+	return (<>
+		<Tag>Songs</Tag>
+		<blockquote>
+			{songs?.map((title: any, key: number) => <div key={key}><i>{AutoLinkSong(title)}</i></div>)}
+		</blockquote>
+	</>)
+}
+
+const ReleaseCredits = ({ credits }: { credits: Object }) => {
+	const creds = Object.keys(credits); 
+	if (!creds?.length) return;
+	return (<>
+		<Tag>Credits</Tag>
+		<blockquote>
+			{creds?.map((who: any, key: number) => {
+				const { album_credits={}, song_credits={} } = credits[who];
+				return (<div key={key}>
+					<PerformanceCredits who={who} album_credits={album_credits} song_credits={song_credits} />
+				</div>)
+			})}
+		</blockquote>
+	</>)
+}
+
+const ReleaseContribution = ({ release }: { release: ReleaseTypeWithChildren }) => {
+	if (release?.contribution) {
+		return (<>
+			<Tag>Contribution</Tag>
+			<blockquote>
+				{AutoLinkSong(release?.contribution)}
+			</blockquote>
+		</>)
+	}
+}
+
+const ReleaseLiner = ({ release }: { release: ReleaseTypeWithChildren }) => {
+	if (release?.liner) {
+		return (<>
+			<Tag>Liner Notes</Tag>
+			<blockquote>
+				{release?.liner}
+			</blockquote>
+		</>)
+	}
+}
+
+const ReleaseThanks = ({ release }: { release: ReleaseTypeWithChildren }) => {
+	if (release?.thanks) {
+		return (<>
+			<Tag>Thanks</Tag>
+			<blockquote>
+				{release?.thanks}
+			</blockquote>
+		</>)
+	}
+}
+
+const ReleaseDetails = ({ release }: { release: ReleaseTypeWithChildren }) => {
+
+	const labels = {
+		type: "Release Type",
+		title: "Release Title",
+		dtreleased: "Released",
+		label: "Label",
+		catalog: "Catalogue",
+		media: "Media",
+		country: "Country",
+		dtrecorded: "Recorded",
+		studio: "Studio",
+	};
+
+	return (<div className={`gig_${release?.project}`}>
+		<Tag>Details</Tag>
+		<blockquote>
+			{Object.keys(labels).map((label: string, key: number) => {
+				if (release[label]) {
+					return <div key={key}>
+						{labels[label]}: {release[label]}
+					</div>
+				}
+			})}
+		</blockquote>
+	</div>)
+}
+
 const Release = ({ release }: { release: ReleaseTypeWithChildren }, key: number) => {
 	const lookup = release?.lookup ?? '';
 	const { data, isLoading, error } = useRelease(lookup);
@@ -55,16 +144,11 @@ const Release = ({ release }: { release: ReleaseTypeWithChildren }, key: number)
 		<div key={key}><MakeAlbumBlurb {...release} /></div>
 		<Suspense fallback=<>Loading...</>>
 			{(!isLoading) && (<>
-				<blockquote>
-					{songs?.map((title: any, key: number) => <div key={key}><i>{AutoLinkSong(title)}</i></div>)}
-				</blockquote>
-				<hr />
-				{Object.keys(credits)?.map((who: any, key: number) => {
-					const { album_credits={}, song_credits={} } = credits[who];
-					return (<div key={key}>
-						<PerformanceCredits who={who} album_credits={album_credits} song_credits={song_credits} />
-					</div>)
-				})}
+				<ReleaseDetails release={release} />
+				{(release?.contribution) ? <ReleaseContribution release={release} /> : <ReleaseSongList songs={songs} />}
+				<ReleaseCredits credits={credits} />
+				<ReleaseLiner release={release} />
+				<ReleaseThanks release={release} />
 			</>)}
 		</Suspense>
 	</>)

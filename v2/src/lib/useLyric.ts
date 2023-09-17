cdd0a896a4a145ba9e4be330b808e97efe378e25
xpollen8 <github@xpollen8.com>
2023-09-17 00:15:16 -0700

    dw: go to local api

--- v2/src/lib/useLyric.ts
+++ v2/src/lib/useLyric.ts
@@ -2,8 +2,7 @@ import useSWR from 'swr';
 
 const useLyric = (href: string) => {
 	const fetcher = (url: string) => fetch(url).then((res) => res.json());
-	console.log("FETCH", href, `/api/v2/lyric_by_href/${href}`);
-	const { data, error, isLoading } = useSWR(`/api/v2/lyric_by_href/${href}`, fetcher);
+	const { data, error, isLoading } = useSWR(`/api/lyric_by_href/${href}`, fetcher);
 	return {
 		lyric: data,
 		isLoading,

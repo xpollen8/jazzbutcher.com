d3a6785d167b205f0b486a1370ec60fb0754b6cd
xpollen8 <github@xpollen8.com>
2023-09-17 00:35:22 -0700

    dw: return as data

--- v2/src/lib/useLyric.ts
+++ v2/src/lib/useLyric.ts
@@ -4,7 +4,7 @@ const useLyric = (href: string) => {
 	const fetcher = (url: string) => fetch(url).then((res) => res.json());
 	const { data, error, isLoading } = useSWR(`/api/lyric_by_href/${href}`, fetcher);
 	return {
-		lyric: data,
+		data,
 		isLoading,
 		error,
 	}

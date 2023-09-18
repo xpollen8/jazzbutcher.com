3ea3a8222422c7f611130d3127260590558b8f38
xpollen8 <github@xpollen8.com>
2023-09-17 23:25:38 -0700

    dw: useRelease

--- v2/src/app/releases/[id]/page.tsx
+++ v2/src/app/releases/[id]/page.tsx
@@ -1,17 +1,22 @@
-import { releaseByLookup } from '@/lib/macros';
+"use client"
+
+import { Suspense } from 'react';
+
 import Release from '@/components/Release';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import useRelease from '@/lib/useRelease';
 
-const Page = async ({ params }: { params?: any }) => {
-  const release = await releaseByLookup(params?.id);
-	if (!release) return <></>
-	return (
-	<>
-		<Header section="releases" title={release?.title} />
-		<Release release={release} />
-		<Footer />
-	</>
+const Page = ({ params }: { params?: any }) => {
+	const { data, isLoading, error } = useRelease(params?.id);
+	const release = data;
+	return (<Suspense fallback=<>Loading...</> >
+		{(!isLoading && release) && (<>
+			<Header section="releases" title={release?.title} />
+			<Release release={release} />
+			<Footer />
+		</>)}
+	</Suspense>
 	)
 }
 

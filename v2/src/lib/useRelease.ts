9290e0391910bf43c3c0b129acc031cc1127ce8f
xpollen8 <github@xpollen8.com>
2023-09-17 23:41:52 -0700

    dw; initial checkin

---
+++ v2/src/lib/useRelease.ts
@@ -0,0 +1,13 @@
+import useSWR from 'swr';
+
+const useRelease = (lookup: string) => {
+	const fetcher = (url: string) => fetch(url).then((res) => res.json());
+	const { data, isLoading, error } = useSWR(`/api/release_by_lookup/${lookup}`, fetcher);
+	return {
+		data,
+		isLoading,
+		error,
+	}
+}
+
+export default useRelease;

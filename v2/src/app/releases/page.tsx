ad9c384202b9933014c3f6f8b1afbf1996c95792
xpollen8 <github@xpollen8.com>
2023-09-17 23:17:21 -0700

    dw: useReleases

--- v2/src/app/releases/page.tsx
+++ v2/src/app/releases/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { Suspense } from 'react';
+
 import moment from 'moment';
-import apiData from '@/lib/apiData';
+import useReleases from '@/lib/useReleases';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import MakeAlbumBlurb from '@/components/MakeAlbumBlurb';
@@ -7,25 +11,28 @@ import { ReleaseType } from '@/components/Release';
 import { parseYear } from '@/lib/macros';
 import Tag from '@/components/Tag';
 
-const Releases = async () => {
-	const releases = await apiData('releases');
-	return <>
+const Releases = () => {
+	const { data, isLoading, error } = useReleases();
+	const releases = data?.albums;
+	return <Suspense fallback=<>Loading...</> >
 		<Header section="releases" />
-		{(() => {
-			let year = 999;
-			return releases?.albums?.sort((a: ReleaseType, b: ReleaseType) => moment.utc(b.dtreleased).diff(moment.utc(a.dtreleased)))
-				.map((item: any, key: number) => {
-					const yr = parseYear(item.dtreleased);
-					let tag = <></>;
-					if (yr !== year) {
-						tag = <Tag>{yr}</Tag>
-						year = yr;
-					}
-					return <>{tag}{MakeAlbumBlurb(item, key)}</>;
-				});
-		})()}
+		{(!isLoading && releases) && (<>
+			{(() => {
+				let year = 999;
+				return releases?.sort((a: ReleaseType, b: ReleaseType) => moment.utc(b.dtreleased).diff(moment.utc(a.dtreleased)))
+					.map((item: any, key: number) => {
+						const yr = parseYear(item.dtreleased);
+						let tag = <></>;
+						if (yr !== year) {
+							tag = <Tag>{yr}</Tag>
+							year = yr;
+						}
+						return <>{tag}{MakeAlbumBlurb(item, key)}</>;
+					});
+			})()}
+		</>)}
 		<Footer />
-	</>
+	</Suspense>
 }
 
 export default Releases;

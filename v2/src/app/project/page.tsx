98e64d524200346d02d787acd45315070e8ae0bb
xpollen8 <github@xpollen8.com>
2023-09-19 15:07:21 -0700

    dw; 'Prime Evils'

--- v2/src/app/project/page.tsx
+++ v2/src/app/project/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import MakeSimpleURI from '@/components/MakeSimpleURI';
@@ -19,6 +21,7 @@ const collaborations = [
 	{ uri: '/project/vaguely_familiar', text: 'Vaguely Familiar', aux: "w/Richard Formby + Sumishta Brahm"  },
 	{ uri: '/project/trampling_tokyo', text: "With Alan Moore", aux: "The 'Trampling Tokyo' single" },
 	{ uri: '/project/the_undertakers', text: "The Undertakers", aux: "Unrehearsed covers, live" },
+	{ uri: '/project/prime_evils', text: "Prime Evils", aux: <>Bass, Joe Woolley and Jonny Mattock (ref: <Link href='/gigs/2006/Jul27'>2006-07-27</Link>)</> },
 ];
 
 const other = [
@@ -33,7 +36,7 @@ const Project = () =>
 		{music_projects.map(MakeSimpleURI)}
 		<LetterHeader title="Music With Others" subhead="Pat in other groups" />
 		{with_others.map(MakeSimpleURI)}
-		<LetterHeader title="One-offs" subhead="Short-term music collaborations" />
+		<LetterHeader title="Short Collaborations" subhead="one-offs and project-based efforts" />
 		{collaborations.map(MakeSimpleURI)}
 		<LetterHeader title="Not necessarily music" />
 		{other.map(MakeSimpleURI)}

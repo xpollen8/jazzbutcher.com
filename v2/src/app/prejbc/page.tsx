201b81e6b7350dfd02ea9c723f6972825164f520
xpollen8 <github@xpollen8.com>
2023-09-21 01:10:39 -0700

    dw: added some inline text and added Wow Federation

--- v2/src/app/prejbc/page.tsx
+++ v2/src/app/prejbc/page.tsx
@@ -4,10 +4,30 @@ import MakeSimpleURI from '@/components/MakeSimpleURI';
 
 const projects = [
 	{ uri: '/prejbc/first_gig', text: "Pat's first gig", aux: "allegedly" },
-	{ uri: '/prejbc/institution', text: 'The Institution' },
+	{ uri: '/prejbc/institution', text: 'The Institution',
+		children: <>
+		Credited on the single:
+		<p />
+		Bass – Rolo<br />
+		Drums – Rob Wilford<br />
+		Guitar – Max Eider<br />
+		Vocals, Saxophone – Pat Fish<br />
+		<hr />
+		Left before the single:
+		<p />
+		Synthesizers - Johnathan Stephenson<br />
+		Drums- John Duvall<br />
+		</>},
 	{ uri: '/prejbc/nightshift', text: 'Nightshift' },
 	{ uri: '/prejbc/sonic_tonix', text: 'Sonic Tonix' },
 	{ uri: '/prejbc/the_tonix', text: 'The Tonix' },
+	{ text: 'Wow Federation', aux: 'w/Max',
+		children: <>
+		Max played guitar with Pat Fish in college in bands such as The Institution, Wow Federation and The Tonix.
+		<p />
+		Source: https://music.metason.net/artistinfo?name=Max%20Eider
+		</>
+		},
 ];
 
 const PreJBC = () => 

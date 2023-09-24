a4967981230c942ef86a0ed159f1ad3911fd0666
xpollen8 <github@xpollen8.com>
2023-09-23 18:07:30 -0700

    dw: addex aux

--- v2/src/components/LetterHeader.tsx
+++ v2/src/components/LetterHeader.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 
-const LetterHeader = (props: { title: string | React.ReactNode, subhead?: string | React.ReactNode }) =>
+const LetterHeader = (props: { title: string | React.ReactNode, subhead?: string | React.ReactNode, aux?: string }) =>
 <>
 	<p />
 	<center>
 		<b className="text-2xl">{props.title}</b>
 		{(props?.subhead) && <><br />{props.subhead}</>}
+		{(props?.aux) && <><br />{props.aux}</>}
 	</center>
 	<p />
 	<hr />

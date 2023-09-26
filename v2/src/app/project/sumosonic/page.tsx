db453b0daea99356cba83966225b39ba466cdda7
xpollen8 <github@xpollen8.com>
2023-09-25 21:48:03 -0700

    dw: added FilterPresses

--- v2/src/app/project/sumosonic/page.tsx
+++ v2/src/app/project/sumosonic/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import FilterReleases from '@/components/FilterReleases';
+import FilterPresses from '@/components/FilterPresses';
 
 const Sumosonic = () => 
 <>
 	<Header project="sumo" section="sumosonic" />
 	<main>
 		<FilterReleases project="sumo" />
+		<FilterPresses project="sumo" />
 	</main>
 	<Footer />
 </>

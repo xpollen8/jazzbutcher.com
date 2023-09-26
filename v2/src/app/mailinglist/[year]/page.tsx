a5b41dba025ae51ca564d5967063d7f1fde3b831
xpollen8 <github@xpollen8.com>
2023-09-25 21:39:49 -0700

    dw: CSS

--- v2/src/app/mailinglist/[year]/page.tsx
+++ v2/src/app/mailinglist/[year]/page.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link';
+//import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import LetterHeader from '@/components/LetterHeader';
-import MakeSimpleURI from '@/components/MakeSimpleURI';
 
 const Missing = ({ params }: any) => {
 	return (<>
 		<Header section="mailinglist" title={params.year} />
-		<div className="etc">
-			<LetterHeader title="JBC Mailing List Archives" subhead={`For ${params.year}`} />
-			<h1>This section is not yet ported from old JBC site</h1>
-		</div>
+		<main>
+			<div className="etc">
+				<LetterHeader title="JBC Mailing List Archives" subhead={`For ${params.year}`} />
+				<h1>This section is not yet ported from old JBC site</h1>
+			</div>
+		</main>
 		<Footer />
 	</>);
 }

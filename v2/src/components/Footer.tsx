2a8fa3ee06dd15e48c8b3fb71bf7d35f8541c07f
xpollen8 <github@xpollen8.com>
2023-09-25 21:57:40 -0700

    dw: null for now

--- v2/src/components/Footer.tsx
+++ v2/src/components/Footer.tsx
@@ -1,12 +1,14 @@
-import Link from 'next/link';
+//import Link from 'next/link';
 
 const Footer = (): React.ReactNode => {
 	return <></>;
+	/*
 	return (
 	<footer className="clear_float p-2 mt-5">
-			<Link href="/help">Get Involved!</Link> Let's do this, together.
+			<Link href="/help">Get Involved!</Link> Let&apos;s do this, together.
 	</footer>
 	)
+	*/
 }
 
 export default Footer;

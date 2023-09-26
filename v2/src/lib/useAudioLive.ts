491c96d9a5db6419ef6a1ec55e0bbb5095e5ab07
xpollen8 <github@xpollen8.com>
2023-09-25 21:52:20 -0700

    dw: fetch gig or project audio

--- v2/src/lib/useAudioLive.ts
+++ v2/src/lib/useAudioLive.ts
@@ -1,11 +1,10 @@
 import useSWR from 'swr';
 
-const useAudioLive = () => {
+const useAudioLive = (project: string ='') => {
 	const fetcher = async (url: any) => {
-		return fetch(url)
-			.then((res) => res.json())
-		}
-	const { data, error, isLoading } = useSWR(`/api/gigs_with_audio`, fetcher);
+		return fetch(url).then((res) => res.json())
+	}
+	const { data, error, isLoading } = useSWR((project) ? `/api/audio_by_project/${project}` : `/api/gigs_with_audio`, fetcher);
 
 	return {
 		data,

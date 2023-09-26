fe4fe23e83da77bcb38307c451e2a5d7de66fabe
xpollen8 <github@xpollen8.com>
2023-09-25 21:49:51 -0700

    dw: use SectionOptions

--- v2/src/app/writings/page.tsx
+++ v2/src/app/writings/page.tsx
@@ -1,29 +1,26 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import Tag from '@/components/Tag';
 import LetterHeader from '@/components/LetterHeader';
 import MakeSimpleURI from '@/components/MakeSimpleURI';
+import SectionOptions from '@/components/SectionOptions';
 
 const writings = [
-	{ uri: "/fiascos", text: "Top 10 JBC Fiascos", aux: "(2020-09)" },
 	{ uri: '/writings/albums', text: "Pat Reviews His Albums" },
 	{ uri: '/writings/gigs', text: "Pat Reviews His Gigs" },
-	{ uri: "/press/20020709_starindia.html", text: "The Star Of India - Pat reviews his favorite Restaurant", aux: "(2002-07)" },
-	{	uri: '/press/20040512_wilson_about.html', text: "Wilson Explainer", aux: '(2002)' },
-	{ uri: '/letters', text: "Letters From Pat", aux: "(1990-1994)" },
-	{ uri: '/fanclub', text: "Early Fan Club issues" },
-	{ uri: '/tomhall', text: "Pat's tribute to musician Tom Hall", aux: "(2003)" },
+	{ uri: "/press/20020709_starindia.html", text: "The Star Of India - Pat reviews his favorite Restaurant", aux: "(2002-07)", children: "Northampton's Star of India restaurant is dearly loved by the artist" },
+	{	uri: '/press/20040512_wilson_about.html', text: "Wilson Explainer", aux: '(2002)', children: "I started the Wilson project fondly imagining that it would be a solo thing." },
 ];
 
 const Writings = () => 
 <>
 	<Header section='writings' />
-	<div className="etc">
+	<main>
 		<LetterHeader title="Pat had a few things to say" />
+		<SectionOptions section='writings' />
+		<hr />
 		{writings.map(MakeSimpleURI)}
-	</div>
+	</main>
 	<Footer />
 </>
 

808e7b6322e7099b00cf899a254bc30e2a51bf43
xpollen8 <github@xpollen8.com>
2023-09-25 21:36:46 -0700

    dw: CSS

--- v2/src/app/etc/page.tsx
+++ v2/src/app/etc/page.tsx
@@ -1,19 +1,23 @@
+import Header from '@/components/Header';
 import LetterHeader from '@/components/LetterHeader';
 import SectionOptions from '@/components/SectionOptions';
 
 const ETC = (props) => <>
-	<div className="etc">
-		<LetterHeader title="A repository of older Jazz Butcher website features" subhead="(c. 2003)" />
-		The Jazz Butcher website first appeared online in February, 1994 - making it
-		among the first 1,000 websites <b>in the world</b>. Over the years,
-		an enormous amount of disparate information has accummulated here (some 4235 pages
-		and counting!)
-		<p />
-		But some material - no matter how lovely - has a shelf-life.
-		Links to this historical and ephemeral information will be preserved here.
-		<p />
-		<SectionOptions section="etc" />
-	</div>
+	<Header section="etc" />
+		<main>
+			<div className="etc">
+				<LetterHeader title="A repository of older Jazz Butcher website features" subhead="(c. 2003)" />
+				The Jazz Butcher website first appeared online in February, 1994 - making it
+				among the first 1,000 websites <b>in the world</b>. Over the years,
+				an enormous amount of disparate information has accummulated here (some 4235 pages
+				and counting!)
+				<p />
+				But some material - no matter how lovely - has a shelf-life.
+				Links to this historical and ephemeral information will be preserved here.
+				<p />
+				<SectionOptions section="etc" />
+			</div>
+	</main>
 </>
 
 export default ETC;

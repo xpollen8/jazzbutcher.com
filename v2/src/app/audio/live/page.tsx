b1dea6604121b2a3bcac3e124a9380712badd7de
xpollen8 <github@xpollen8.com>
2023-09-25 21:44:42 -0700

    dw: initial checkin

---
+++ v2/src/app/audio/live/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import AudioLive from '@/components/AudioLive';
+
+const Bootlegs = () =>
+<>
+	<Header section="live" />
+	<main>
+		<AudioLive />
+	</main>
+	<Footer />
+</>
+
+export default Bootlegs;

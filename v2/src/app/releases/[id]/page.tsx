0a45d91b1fbd61ec0a411c5c86b9d017fda0e949
xpollen8 <github@xpollen8.com>
2023-09-25 21:51:41 -0700

    dw: CSS

--- v2/src/app/releases/[id]/page.tsx
+++ v2/src/app/releases/[id]/page.tsx
@@ -12,8 +12,10 @@ const Page = ({ params }: { params?: any }) => {
 	const release = data && data?.results[0];
 	return (<Suspense fallback=<>Loading...</> >
 		{(!isLoading && release) && (<>
-			<Header section="releases" title={release?.title} />
-			<Release release={release} />
+			<Header project={release?.project} section="releases" title={release?.title} />
+			<main>
+				<Release release={release} />
+			</main>
 			<Footer />
 		</>)}
 	</Suspense>

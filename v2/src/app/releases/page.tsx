6531dc24a6e63867e4b798816bab07977e3bb099
xpollen8 <github@xpollen8.com>
2023-09-25 21:50:57 -0700

    dw: CSS, use FilterReleases

--- v2/src/app/releases/page.tsx
+++ v2/src/app/releases/page.tsx
@@ -1,38 +1,16 @@
-"use client";
-
-import { Suspense } from 'react';
-
-import moment from 'moment';
-import useReleases from '@/lib/useReleases';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import MakeAlbumBlurb from '@/components/MakeAlbumBlurb';
-import { ReleaseType } from '@/components/Release';
-import { parseYear } from '@/lib/macros';
-import Tag from '@/components/Tag';
+import FilterReleases from '@/components/FilterReleases';
 
 const Releases = () => {
-	const { data, isLoading, error } = useReleases();
-	const releases = data?.results;
-	return <Suspense fallback=<>Loading...</> >
+	const types = ['CDR','various','compilation','single','album','live'];
+	return (<>
 		<Header section="releases" />
-		{(!isLoading && releases) && (<>
-			{(() => {
-				let year = 999;
-				return releases?.sort((a: ReleaseType, b: ReleaseType) => parseYear(b.dtreleased) - parseYear(a.dtreleased))
-					.map((item: any, key: number) => {
-						const yr = parseYear(item.dtreleased);
-						let tag = <></>;
-						if (yr !== year) {
-							tag = <Tag>{yr}</Tag>
-							year = yr;
-						}
-						return <>{tag}{MakeAlbumBlurb(item, key)}</>;
-					});
-			})()}
-		</>)}
+			<main>
+				<FilterReleases types={types} /> 
+			</main>
 		<Footer />
-	</Suspense>
+	</>)
 }
 
 export default Releases;

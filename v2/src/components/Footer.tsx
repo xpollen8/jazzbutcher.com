f1d71842f86248dfba39f390ae69558e43f8311f
xpollen8 <github@xpollen8.com>
2023-09-25 21:52:40 -0700

    dw: still not settled

--- v2/src/components/Footer.tsx
+++ v2/src/components/Footer.tsx
@@ -1,6 +1,12 @@
-//import PageComments from './PageComments';
+import Link from 'next/link';
 
-//const Footer = (): React.ReactNode => <footer className="clear_float"><PageComments /></footer>
-const Footer = (): React.ReactNode => <footer className="clear_float"></footer>
+const Footer = (): React.ReactNode => {
+	return <></>;
+	return (
+	<footer className="clear_float p-2 mt-5">
+			<Link href="/help">Get Involved!</Link> Let's do this, together.
+	</footer>
+	)
+}
 
 export default Footer;

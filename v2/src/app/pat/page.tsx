300a7c15f46ab7acbb439161d228875dfa834521
xpollen8 <github@xpollen8.com>
2023-09-25 21:41:02 -0700

    dw: use SectionOptions

--- v2/src/app/pat/page.tsx
+++ v2/src/app/pat/page.tsx
@@ -2,24 +2,22 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import MakeSimpleURI from '@/components/MakeSimpleURI';
 import LetterHeader from '@/components/LetterHeader';
+import SectionOptions from '@/components/SectionOptions';
 
 const sections = [
-	{ uri: '/gallery', text: "Photo Gallery" },
-	{ uri: '/project', text: "Other Projects", aux: "During the JBC years" },
 	{ uri: '/facebook', text: "Facebook Activity", aux: "(IDEA: the best of posts from Facebook)" },
-	{ uri: '/fishy_mansions', text: "Fishy Mansions", aux: "2020-2021 livestreams" },
-	{ uri: '/prejbc', text: "Before 'The Jazz Butcher'" },
 	{ uri: '/interviews', text: "Interviews" },
-	{ uri: '/writings', text: "The Butcher Writes", aux: "reviews, comments" },
-	{ uri: '/memoriam', text: "In Memoriam" },
-	{ uri: '/notebooks', text: "Excerpts from Pat's notebooks" },
 ];
 
 const Pat = () => 
 <>
 	<Header section="pat" />
-		<LetterHeader title="Main Sections" />
-		{sections.map(MakeSimpleURI)}
+		<main>
+			<LetterHeader title="Main Sections" />
+			<SectionOptions section='pat' />
+			<hr />
+			{sections.map(MakeSimpleURI)}
+		</main>
 	<Footer />
 </>
 

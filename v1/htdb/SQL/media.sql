fcab4559377f44caf6fd9f9afd4662cb2b2230ce
David Whittemore <git@xpollen8.com>
2023-09-27 00:44:28 +0000

    dw: added lookup

--- v1/htdb/SQL/media.sql
+++ v1/htdb/SQL/media.sql
@@ -2,8 +2,10 @@ drop table if exists media;
 
 create table media (
 	media_id mediumint unsigned NOT NULL auto_increment,
+	lookup varchar(30) NOT NULL,
 	type varchar(20) NOT NULL,
 	subtype varchar(100),
+	ordinal varchar(50) NOT NULL,
 	project varchar(20) NOT NULL,
 	collection varchar(200) NOT NULL,
 	name varchar(200) NOT NULL,
@@ -13,11 +15,12 @@ create table media (
 	author varchar(200) NOT NULL,
 	credit varchar(200),
 	parent varchar(50),
-	images text,
 	`datetime` datetime not null,
+	images text,
 	dtcreated datetime not null default NOW(),
 	INDEX idx0(media_id),
 	INDEX idx1(type,subtype),
 	INDEX idx2(href),
+	INDEX idx4(lookup),
 	UNIQUE INDEX idx3(project, collection, mp3, href)
 );

d41cfaa95301e46b41fd15ca8c1859afc874f863
xpollen8 <github@xpollen8.com>
2023-09-30 12:47:44 -0700

    dw: SectionHeader

--- v2/src/app/press/page.tsx
+++ v2/src/app/press/page.tsx
@@ -1,33 +1,13 @@
-import Link from 'next/link';
-
 import Header from '@/components/Header';
 import SectionOptions from '@/components/SectionOptions';
-//import PressItems from '@/components/PressItems';
 import FilterPresses from '@/components/FilterPresses';
-
-const Lsections = [
-	{ uri: '/press/interviews', text: 'Published Interviews' },
-	{ uri: '/press/reviews/gig', text: 'Published Gig Reviews' },
-	{ uri: '/press/albums', text: 'Published Album Reviews' },
-	{ uri: '/press/obits', text: 'Published Rememberences' },
-	{ uri: '/press/promos', text: 'JBC Promotional Material and Bios' },
-	{ uri: '/press/reviews/fan', text: "'I Was There' Gig Reviews" },
-	{ uri: '/writings/albums', text: "Pat Reviews His Albums" },
-	{ uri: '/writings/gigs', text: "Pat Reviews His Gigs" },
-	{ uri: '/writings', text: "Pat Reviews Other Things" },
-	{ uri: '/press/audio', text: 'Audio Interviews', aux: "radio and online" },
-	{ uri: '/press/black_eg', text: 'Press for Black Eg' },
-	{ uri: '/press/wilson', text: 'Press for Wilson' },
-	{ uri: '/press/sumosonic', text: 'Press for Sumosonic' },
-];
 
 const Press = (props: any) => {
 	return (<>
 		<Header section='press' />
 		<main>
 			<SectionOptions section='press' />
-			<FilterPresses />
-			{/*<PressItems />*/}
+			<FilterPresses title="All Press" />
 		</main>
 	</>)
 }

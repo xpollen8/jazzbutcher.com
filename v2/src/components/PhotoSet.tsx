e0fe2b2ffc490fdfa0bb96cae9a4cee004bc8f81
xpollen8 <github@xpollen8.com>
2023-10-02 18:49:41 -0700

    dw: added description and credit

---
+++ v2/src/components/PhotoSet.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Tag from '@/components/Tag';
+
+import { Credit } from '@/components/GenericWeb';
+
+const PhotoSet = ({ title, photos, description, credit, credit_url, credit_date }: { title: string, photos: any[], description?: string | React.ReactNode, credit_url?: string, credit_date?: string }) => <>
+	<Tag> {title} </Tag>
+	{(description) && <blockquote>{description}</blockquote>}
+	{(credit) && <blockquote><Credit g={credit} u={credit_url} d={credit_date} /></blockquote>}
+	<div className="flex flex-wrap justify-center gap-3 listItem">
+		{photos.map((w: any, key: number) => {
+			const { src, alt, credit, credit_url, credit_date, body } = w;
+			const [ part, ext ] = src.split('.');
+			return <div className="p-1 drop-shadow-sm border border-slate-500 text-center w-64">
+				<Link href={src}><Image key={key} unoptimized src={`${part}_250.${ext}`} width={250} height={250} alt={alt} /></Link>
+				<br/>
+				{alt}
+				{(body) && <><p />&quot;<i>{body}</i>&quot;</>}
+				{(credit) && <><br /><Credit g={credit} u={credit_url} d={credit_date} /></>}
+			</div>
+		})}
+	</div>
+</>
+
+export default PhotoSet;

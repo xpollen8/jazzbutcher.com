dc5f3f1d2f0eaf5efb83312bba3d80a4031609db
xpollen8 <github@xpollen8.com>
2023-10-02 19:22:41 -0700

    dw: added JBC Family Tree

--- v2/src/app/prejbc/page.tsx
+++ v2/src/app/prejbc/page.tsx
@@ -1,11 +1,18 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import SectionOptions from '@/components/SectionOptions';
+import PhotoSet from '@/components/PhotoSet';
+
+const snow1 = [
+	{ src: "/assets/projects/JBC+Rock+Family+Tree+1.jpg", alt: "JBC Family Tree #1", credit: "Philip Snow" },
+	{ src: "/assets/projects/JBC+Rock+Family+Tree+2.jpg", alt: "JBC Family Tree #2", credit: "Philip Snow" },
+];
 
 const PreJBC = () => 
 <>
 	<Header section="prejbc" />
 		<main>
+			<PhotoSet title="JBC Family Tree" description="From Pat's notebooks, as captured for 'Miracles and Wonders'" photos={snow1} />
 			<SectionOptions section="prejbc" />
 		</main>
 	<Footer />

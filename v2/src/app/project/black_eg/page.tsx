70a00f91bfc5b8514e348d12adcfbbaf9bc6b124
xpollen8 <github@xpollen8.com>
2023-10-03 23:41:43 -0700

    dw: some cleanup

--- v2/src/app/project/black_eg/page.tsx
+++ v2/src/app/project/black_eg/page.tsx
@@ -1,7 +1,7 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import AudioLive from '@/components/AudioLive';
-import LetterHeader from '@/components/LetterHeader';
+import Tag from '@/components/Tag';
 import EmbedMedia from '@/components/EmbedMedia';
 import FilterReleases from '@/components/FilterReleases';
 import FilterPresses from '@/components/FilterPresses';
@@ -11,21 +11,16 @@ const EgAudio = () =>
 	<Header project='eg' section="black_eg" />
 	<main>
 		<FilterReleases project="eg" />
+		<p />
 		<FilterPresses title="Black Eg Press" project="eg" />
+		<p />
 		<AudioLive project='eg' />
-		<LetterHeader title="Unique black eg tracks" />
-		<div className="listItem">
+		<p />
+		<Tag>Demos tracks provided by Kevin Carroll</Tag>
 			<EmbedMedia data={{ mediaurl: "https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Yokohama.mp3", song: "Yokohama", mediacredit: "Kevin Carroll", mediacreditdate: "2018-08-31" }} />
-		</div>
-		<div className="listItem">
 			<EmbedMedia data={{ mediaurl: "https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Song Of The Homeland.mp3", song: "Song Of The Homeland", mediacredit: "Kevin Carroll", mediacreditdate: "2018-08-31" }} />
-		</div>
-		<div className="listItem">
 			<EmbedMedia data={{ mediaurl: "https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Ostrava Girls.mp3", song: "Ostrava Girls", mediacredit: "Kevin Carroll", mediacreditdate: "2018-08-31" }} />
-		</div>
-		<div className="listItem">
 			<EmbedMedia data={{ mediaurl: "https://s3.amazonaws.com/assets.jazzbutcher.com/projects/TheBlackEg/BlackEg_KevinCarroll_Indianapolis 500.mp3", song: "Indianapolis 500", mediacredit: "Kevin Carroll", mediacreditdate: "2018-08-31" }} />
-		</div>
 	</main>
 	<Footer />
 </>

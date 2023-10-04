335cc599a488f87950d8993c3302d736f95c7cc6
xpollen8 <github@xpollen8.com>
2023-10-04 12:30:04 -0700

    dw: initial checkin

---
+++ v2/src/components/AudioReleases.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { parseCredit } from '@/lib/macros';
+import useAudioReleases from '@/lib/useAudioReleases';
+import EmbedMedia from '@/components/EmbedMedia';
+import LetterHeader from '@/components/LetterHeader';
+
+const AudioSection = ({ title, data }: any) => {
+	return !!(data && data?.length) && (<>
+		<LetterHeader title={title} />
+		{data?.map((d: any, key: number) => {
+			const { performance_id, datetime, category, lookup, variant, type, performer, aka, title, setnum, ordinal, song, version, author, instruments, media } = d;
+			return <EmbedMedia key={key}
+				data={{
+					//datetime: datetime,
+					//venue: lookup,
+					//venue: d.venue,
+					//city: d.city,
+					mediaurl: media,
+					title: song,
+					// author: d.collection,
+					comment: lookup,
+					// mediacredit: credit,
+					// mediacrediturl: crediturl,
+					// mediacreditdate: creditdate,
+					//children: creditcaption
+				}} />;
+		})}
+	</>)
+}
+
+const AudioReleases = ({ project='jbc' }: { project?: string }) => {
+	const { data, isLoading, error } = useAudioReleases(project);
+	return <AudioSection title="Audio" data={data?.results} />
+}
+
+export default AudioReleases;

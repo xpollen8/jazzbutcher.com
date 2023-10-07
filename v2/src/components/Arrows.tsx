e86aead617db8aba9a03ec0e7140777f4df20f1d
xpollen8 <github@xpollen8.com>
2023-10-07 13:47:58 -0700

    dw: initial checkin

---
+++ v2/src/components/Arrows.tsx
@@ -0,0 +1,37 @@
+export const PrevArrow = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="si-glyph si-glyph-two-arrow-left"
+		width={20}
+		height={20}
+    viewBox="0 -0.5 17 17"
+    {...props}
+  >
+    <title>{"1134"}</title>
+    <g fill="#434343" fillRule="evenodd">
+      <path
+        d="M5.146 8.027 8.89 3.032H4.95L1.007 8.027l4.042 4.964h3.918l.01-.011-3.831-4.953ZM12.929 8.027l3.951-4.995h-3.939L8.976 8.027l4.064 4.964h3.918l.01-.011-4.039-4.953Z"
+        className="si-glyph-fill"
+      />
+    </g>
+  </svg>
+)
+
+export const NextArrow = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="si-glyph si-glyph-two-arrow-right"
+		width={20}
+		height={20}
+    viewBox="0 -0.5 17 17"
+    {...props}
+  >
+    <title>{"1133"}</title>
+    <g fill="#434343" fillRule="evenodd">
+      <path
+        d="M12.83 7.999 9.086 13h3.939l3.944-5.001-4.042-4.969H9.009l-.011.011 3.832 4.958ZM5.047 7.999 1.096 13h3.938L9 7.999 4.935 3.03H1.018l-.01.011 4.039 4.958Z"
+        className="si-glyph-fill"
+      />
+    </g>
+  </svg>
+)

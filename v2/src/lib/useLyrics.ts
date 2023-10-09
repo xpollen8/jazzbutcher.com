7f74cb8421f004d500378224dc0db6b952b63c6a
xpollen8 <github@xpollen8.com>
2023-10-08 23:51:00 -0700

    dw: simplify

--- v2/src/lib/useLyrics.ts
+++ v2/src/lib/useLyrics.ts
@@ -2,14 +2,7 @@ import useSWR from 'swr';
 
 const useLyrics = () => {
 	const fetcher = async (url: any) => fetch(url).then((res) => res.json());
-	const { data, error, isLoading } = useSWR(`/api/lyrics`, fetcher);
-
-	//console.log("RET", data);
-	return {
-		data,
-		isLoading,
-		error,
-	}
+	return useSWR(`/api/lyrics`, fetcher);
 }
 
 export default useLyrics;

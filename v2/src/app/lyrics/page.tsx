bf2b3bb23b86f49b322043ccfef45dbcc180f23a
xpollen8 <github@xpollen8.com>
2023-10-09 00:14:45 -0700

    dw: cleanup display, finally

--- v2/src/app/lyrics/page.tsx
+++ v2/src/app/lyrics/page.tsx
@@ -5,19 +5,27 @@ import Link from 'next/link';
 import { Suspense } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Tag from '@/components/Tag';
 import useLyrics from '@/lib/useLyrics';
 
 const Lyrics = () => {
 	const { data, isLoading, error } = useLyrics();
 
 	const lyrics = data?.results;
+	let chr: string;
 	return (<>
 		<Header section='lyrics' />
 		<Suspense fallback=<>Fallback...</> >
 		{(!isLoading) && (<>
 			{lyrics.map((lyric: any, key: number) => {
+				let banner;
+				if (lyric?.title?.substr(0, 1) !== chr) {
+					chr = lyric?.title?.substr(0, 1);
+					banner = <Tag>{chr}</Tag>;
+				}
 				return (<div key={key}>
-					Title: <Link href={`/lyrics/${lyric?.href}`}>{lyric?.title}</Link>
+					{banner}
+					<Link className="ml-10" href={`/lyrics/${lyric?.href}`}>{lyric?.title}</Link> <span className="smalltext">{lyric?.tablature ? '(Tablature)' : ''} {lyric?.annotated ? '(Annotated)' : ''}</span>
 				</div>)
 			})}
 		</>)}

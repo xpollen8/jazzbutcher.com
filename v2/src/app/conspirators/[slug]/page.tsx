2919939de2b80e436538f7858c01e70fc91eac84
xpollen8 <github@xpollen8.com>
2023-10-11 20:01:34 -0700

    dw: roughing it out

--- v2/src/app/conspirators/[slug]/page.tsx
+++ v2/src/app/conspirators/[slug]/page.tsx
@@ -1,29 +1,37 @@
 "use client"
 
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Tag from '@/components/Tag';
-import { expand } from '@/lib/defines';
+import { people, expand, mapPerformers, AutoLinkPlayer } from '@/lib/defines';
+import { imageThumb, imageFull, parseCaptionsSourcesEtc} from '@/lib/macros';
+import { Credit } from '@/components/GenericWeb';
 
 //import useConspirator from '@/lib/useConspirator';
 
+const PersonGallery = (props: { str?: string }) => {
+	const images = parseCaptionsSourcesEtc(props?.str);
+	return images?.map((img: str[], key: number) => {
+		const [ src, credit, crediturl, creditdate, caption ] = img;
+		const useSrc = imageThumb(src);
+		return <div key={key} className="listItem w-64">
+			<Link href={imageFull(src)}><Image src={imageThumb(src)} width={250} height={250} alt='image' /></Link>
+			{(caption) && <span>{caption}</span>}
+			<br />
+			{(credit) && <Credit g={credit} u={crediturl} d={creditdate} />}
+		</div>
+	})
+}
+
 const Conspirator = ({ params }: { params?: any }) => {
+	const person = people.find(({ href, name }) => href === `${params?.slug}.html` || name === unescape(params?.slug));
 	/*
-	const { data, isLoading, error } = useConspirator(params?.slug);
+	const { data, isLoading, error } = useConspirator(name);
 
 	const conspirator = data?.results[0];
-
-	const tabs = [
-			{ label: 'Lyrics', lookup: (song: any) => { return song?.lyrics }, func: Lyrics },
-			{ label: 'Found On', lookup: (song: any) => (song?.found_on), func: (song: any, key: number) => FoundOn(song, key, releases) },
-			{ label: 'Pat Says', lookup: (song: any) => (song?.pat_says), func: PatSays },
-			{ label: 'Others Say', lookup: (song: any) => (song?.others_say), func: OthersSay },
-			{ label: 'Tablature', lookup: (song: any) => (song?.tablature), func: Tablature },
-			{ label: 'Live Stats', lookup: (song: any) => ({}), func: LiveStats },
-			{ label: 'Media', lookup: (song: any) => (song?.media), func: Media },
-	];
-	*/
 
 	/*
 	return (<><Suspense fallback=<>Loading...</> >
@@ -38,10 +46,123 @@ const Conspirator = ({ params }: { params?: any }) => {
 	</Suspense></>)
 	*/
 	return <>
-				<Header section="conspirators" title={expand(params?.slug)} />
-		Work in progress
+		<Header section="conspirators" title={person?.name} />
+		<main>
+			<Tag>{person?.name}</Tag>
+			<PersonGallery str={person?.images} />
+			work in progress..
+		</main>
 		<Footer />
-		</>;
+	</>;
 }
 
 export default Conspirator;
+
+/*
+Agent Cooper
+Agent Wilson
+Aggi Demetri
+Alan Moore
+Alastair Indge
+Alex Green
+Alex Lee
+Alice Thompson
+Ani Cordero
+Anita Allbright
+B-Man
+Blair MacDonald
+Bolly
+Brent Bambury
+Buffalo Shame
+Charlie Baldonado
+Curtis E. Johnson
+Dave Henderson
+Dave Morgan
+David E. Barker
+David J.
+David Whittemore
+Deirdre O'Donoghue
+Derek Tomkins
+Dooj Wilkinson
+E-Man
+Emil Von D&auml;mmerung
+Erich Hubner
+Erol Suleyman
+Felix Ray
+Francisco Cabeza
+G-Man
+Gabriel Turner
+Garrick Simmons
+Greenwood Goulding
+Greg Gilmore
+Howard Turner
+Iain O'Higgins
+Ian Botterill
+Ian Sturgess
+Joby Palmer
+Joe Allen
+Joe Skyward
+Joe Woolley
+Joel Harries
+John A. Rivers
+Jonny Mattock
+Julian Poole
+Karel Von D&auml;mmerung
+Kathie McGinty
+Kathy Schaer
+Kevin Haskins
+Kevin Komoda
+Kizzy O'Callaghan
+Laurence O'Keefe
+Lee Brooks
+Les Sanders
+Lionel Brando
+MC Bott
+Malcolm Rivett-Carnac
+Marc Hadley
+Mark Brown
+Mark Refoy
+Martin. K. Daley
+Martin K. Daley
+Martin Stebbing
+Max Eider
+Max Read
+Max Reed
+Mick Mercer
+Misery Wilson
+Mitch Jenkins
+Mr. Crush
+Nick Bandy
+Nick Burson
+Otto Von D&auml;mmerung
+Owen Jones
+Pascal Legras
+Pat Beirne
+Pat Fish
+Pat Kenneally
+Paul Hookham
+Paul Mulreany
+Paul Taylor
+Peter Astor
+Peter Crouch
+Richard Formby
+Richard Lohan
+Rolo McGinty
+Russell Cooper
+Simon Taylor
+Sonic Boom
+Steve Beswick
+Steve Garofalo
+Steve Musgrove
+Steve New
+Steve Valentine
+Sumishta Brahm
+The Antichrist
+The Conspiracy
+The Undead
+Tim Burrell
+Tim Harries
+Tony Harris
+Tractor
+Wilson Headstone
+*/

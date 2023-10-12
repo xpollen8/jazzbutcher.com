41f4f3280acdd8c0e096ff30ebbba7ed721181c6
xpollen8 <github@xpollen8.com>
2023-10-11 19:57:54 -0700

    dw: sort-ish by last name

--- v2/src/app/conspirators/page.tsx
+++ v2/src/app/conspirators/page.tsx
@@ -1,11 +1,32 @@
+import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Tag from '@/components/Tag';
+import { people } from '@/lib/defines';
+
+const sortByLastName = (a: any, b: any) => {
+	const n1 = a.name.split(' ').pop();
+	const n2 = b.name.split(' ').pop();
+	return n1.localeCompare(n2);
+}
 
 const Conspirators = () => {
+	let lastLetter;
 	return (
 		<>
 		<Header section='conspirators' />
-		<h1> W.I.P. </h1>
+		{people.filter((p: any) => !p.act ).sort(sortByLastName).map((props: { href: string, name: string }, key: number) => {
+			const letter = props.name.split(' ').pop().substr(0, 1);
+			let banner;
+			if (letter !== lastLetter) {
+				banner = <Tag>{letter}</Tag>;
+				lastLetter = letter;
+			}
+			return <div key={key}>
+				{banner}
+				<Link className="ml-5" href={`/conspirators/${props?.href || props.name}`}>{props.name}</Link>
+			</div>
+		})}
 		<Footer />
 		</>
 	)

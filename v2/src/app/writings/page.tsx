f0c5a806d7d28edbf06836a9806ad08047306db8
xpollen8 <github@xpollen8.com>
2023-10-13 17:16:12 -0700

    dw: use SectionOptions only

--- v2/src/app/writings/page.tsx
+++ v2/src/app/writings/page.tsx
@@ -1,25 +1,14 @@
-import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import LetterHeader from '@/components/LetterHeader';
-import MakeSimpleURI from '@/components/MakeSimpleURI';
+import Tag from '@/components/Tag';
 import SectionOptions from '@/components/SectionOptions';
-
-const writings = [
-	{ uri: '/writings/albums', text: "Pat Reviews His Albums" },
-	{ uri: '/writings/gigs', text: "Pat Reviews His Gigs" },
-	{ uri: "/press/20020709_starindia.html", text: "The Star Of India - Pat reviews his favorite Restaurant", aux: "(2002-07)", children: "Northampton's Star of India restaurant is dearly loved by the artist" },
-	{	uri: '/press/20040512_wilson_about.html', text: "Wilson Explainer", aux: '(2002)', children: "I started the Wilson project fondly imagining that it would be a solo thing." },
-];
 
 const Writings = () => 
 <>
 	<Header section='writings' />
 	<main>
-		<LetterHeader title="Pat had a few things to say" />
+		<Tag>Written words that don't fit neatly elsewhere</Tag>
 		<SectionOptions section='writings' />
-		<hr />
-		{writings.map(MakeSimpleURI)}
 	</main>
 	<Footer />
 </>

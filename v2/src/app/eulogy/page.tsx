b5ef2d365acbbd7b18b4e4e0f0bdb7c8f56a004e
xpollen8 <github@xpollen8.com>
2023-10-13 18:07:16 -0700

    dw: cleanup

--- v2/src/app/eulogy/page.tsx
+++ v2/src/app/eulogy/page.tsx
@@ -6,26 +6,27 @@ import { Attribution } from '@/components/GenericWeb';
 const Eulogy = () =>
 <>
 	<Header section='memoriam' title="Eulogy" />
-	<Tag>
-		Eulogy
-	</Tag>
-	<div className="p-4">
-	<h2>The End of the Jazz Age</h2>
-		As Chairman Mao once said, “the true revolutionary moves through the people like a fish through water”. While that’s almost certainly not how Pat chose his name, it does come close to summing up his style: he was subversive, glided effortlessly through a dozen scenes or circles and, here at this furthest inland point, he was an ocean breeze along our peeling terraces.
-	<p />
-		Wherever he was physically, you knew that somewhere in his heart he was in stripy shirt and jaunty cap aboard a yacht, and we’re so lucky that Northampton was the hostile landbound island where he drunkenly saw fit to beach himself. When he first put to shore here in the early eighties at the shallow end of Margaret Thatcher, sizzling with talent, fresh tunes and ridiculous ideas, he had a sense beyond his years that understood the seething musical diversity and energy behind the urban blight. Pat saw a fertile undergrowth where he could blossom into something tropical, unhindered by the greenhouse practices of the big metropolitan pop Meccas. He loved this unravelled, aggravating, glorious town – once called it England’s Barcelona – and the town adored him back; was proud of him; one of the very brightest badges on its frayed lapel.
-	<p />
-		Pat was a hipster in the old, pre-beard-oil sense of someone who knew what was hip. He knew his Baudelaire from his Syd Barrett, and his Brecht from his Beat Generation. He had every classic shot from every great French film on file in his ingenious brain, and you could see it the way he dressed and walked and smiled, a perfect Gallic leading man looking poetic through the Gauloise haze. When he built the Jazz Butcher as an alter-ego, he bolted together a wonderful Frankenstein’s monster of Cool – his favourite unearthings from the cultural boneyard, stitched together and then animated by the lightning of a thousand magic voices on the radio. 
-	<p />
-		And he knew all those voices, all those bands, knew everything about them. He breathed music and he had a nervous-system made of limited-edition coloured vinyl. Jukebox histories poured through him into every album; every number; every moving, funny, smart-arsed line; seventy years of white-hot culture made as new as wet paint by his blinding skill and unique personality. Pat was a world-class singer-songwriter, one of the best that even this prolific land has ever seen. Had he based his career practically anywhere except the haunted black hole of Northampton, then his lyrics could have been on every lip, although he never seemed the kind who’d want that easy, meaningless acclaim. Instead, he watched with his perpetual air of louche amusement as less capable performers borrowed his song-structures, his breath-taking range of reference, even took the look and lilt of him, then tailored his exotic handmade jungle into lucrative, conventional careers. He bore this with a tight smile and sometimes a wry aside, because Pat wasn’t one for starting a commotion, although it might well be argued that he started The Commotions.
-	<p />
-		He was an astonishing musician with a perfect sense of where to take a song, so that it ended up where nobody had been before. The drive and the invention pushing him through his Conspiracy and his Sikorskis, through the blazing Wilson-Sumosonic-Drones Club rush of these last twenty years, are irreplaceable. The map of modern music, without all the acts he’s championed from Spacemen 3 to Liam Dullaghan, would be a sparse cartography indeed. The hole he leaves in culture as a great performer is enormous, although not as gaping as the hole he leaves in us as inspiration, musical collaborator and, above all, friend. We’re going to miss the fireworks of his mind, his casually hilarious conversation, and the Mardi Gras that was his company. We’re going to miss the updates on his furry soulmate Raoul, and his endless enthusiasm, and that finger-flick he’d sometimes do that signified delight. The swan of Shakespeare Road has moved upriver, and how will we cut to the musical meat of the matter, now that the Butcher’s gone?
-	<p />
-		Pat, last of the gentleman adventurers, it was a privilege to know you and to be a part of your Shoe-town eternity. You’re always with us, and the Art School dance goes on forever. There’s a heaven in the lead-out groove. Rest easy, Fishy. 
-	<p />
-	<Attribution g='Alan Moore' d='2021-11-04' />
-	<p />
-	</div>
+	<main>
+		<Tag> Eulogy </Tag>
+		<div className="p-4">
+		<b>The End of the Jazz Age</b>
+		<p />
+			As Chairman Mao once said, “the true revolutionary moves through the people like a fish through water”. While that’s almost certainly not how Pat chose his name, it does come close to summing up his style: he was subversive, glided effortlessly through a dozen scenes or circles and, here at this furthest inland point, he was an ocean breeze along our peeling terraces.
+		<p />
+			Wherever he was physically, you knew that somewhere in his heart he was in stripy shirt and jaunty cap aboard a yacht, and we’re so lucky that Northampton was the hostile landbound island where he drunkenly saw fit to beach himself. When he first put to shore here in the early eighties at the shallow end of Margaret Thatcher, sizzling with talent, fresh tunes and ridiculous ideas, he had a sense beyond his years that understood the seething musical diversity and energy behind the urban blight. Pat saw a fertile undergrowth where he could blossom into something tropical, unhindered by the greenhouse practices of the big metropolitan pop Meccas. He loved this unravelled, aggravating, glorious town – once called it England’s Barcelona – and the town adored him back; was proud of him; one of the very brightest badges on its frayed lapel.
+		<p />
+			Pat was a hipster in the old, pre-beard-oil sense of someone who knew what was hip. He knew his Baudelaire from his Syd Barrett, and his Brecht from his Beat Generation. He had every classic shot from every great French film on file in his ingenious brain, and you could see it the way he dressed and walked and smiled, a perfect Gallic leading man looking poetic through the Gauloise haze. When he built the Jazz Butcher as an alter-ego, he bolted together a wonderful Frankenstein’s monster of Cool – his favourite unearthings from the cultural boneyard, stitched together and then animated by the lightning of a thousand magic voices on the radio. 
+		<p />
+			And he knew all those voices, all those bands, knew everything about them. He breathed music and he had a nervous-system made of limited-edition coloured vinyl. Jukebox histories poured through him into every album; every number; every moving, funny, smart-arsed line; seventy years of white-hot culture made as new as wet paint by his blinding skill and unique personality. Pat was a world-class singer-songwriter, one of the best that even this prolific land has ever seen. Had he based his career practically anywhere except the haunted black hole of Northampton, then his lyrics could have been on every lip, although he never seemed the kind who’d want that easy, meaningless acclaim. Instead, he watched with his perpetual air of louche amusement as less capable performers borrowed his song-structures, his breath-taking range of reference, even took the look and lilt of him, then tailored his exotic handmade jungle into lucrative, conventional careers. He bore this with a tight smile and sometimes a wry aside, because Pat wasn’t one for starting a commotion, although it might well be argued that he started The Commotions.
+		<p />
+			He was an astonishing musician with a perfect sense of where to take a song, so that it ended up where nobody had been before. The drive and the invention pushing him through his Conspiracy and his Sikorskis, through the blazing Wilson-Sumosonic-Drones Club rush of these last twenty years, are irreplaceable. The map of modern music, without all the acts he’s championed from Spacemen 3 to Liam Dullaghan, would be a sparse cartography indeed. The hole he leaves in culture as a great performer is enormous, although not as gaping as the hole he leaves in us as inspiration, musical collaborator and, above all, friend. We’re going to miss the fireworks of his mind, his casually hilarious conversation, and the Mardi Gras that was his company. We’re going to miss the updates on his furry soulmate Raoul, and his endless enthusiasm, and that finger-flick he’d sometimes do that signified delight. The swan of Shakespeare Road has moved upriver, and how will we cut to the musical meat of the matter, now that the Butcher’s gone?
+		<p />
+			Pat, last of the gentleman adventurers, it was a privilege to know you and to be a part of your Shoe-town eternity. You’re always with us, and the Art School dance goes on forever. There’s a heaven in the lead-out groove. Rest easy, Fishy. 
+		<p />
+		<Attribution g='Alan Moore' d='2021-11-04' />
+		<p />
+		</div>
+	</main>
 	<Footer />
 </>
 

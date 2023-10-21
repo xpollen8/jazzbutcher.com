7f27e0cb930b663fbf0573ad45479be5b2acccbc
xpollen8 <github@xpollen8.com>
2023-10-21 14:20:43 -0700

    dw: cleanup

--- v2/src/app/etc/page.tsx
+++ v2/src/app/etc/page.tsx
@@ -5,15 +5,15 @@ import SectionOptions from '@/components/SectionOptions';
 const ETC = () => <>
 	<Header section="etc" />
 		<main className="etc">
-			<LetterHeader title="A repository of older Jazz Butcher website features" subhead="(c. 2003)" />
+			<LetterHeader title="A repository of older Jazz Butcher website features" aux=<>
 			The Jazz Butcher website first appeared online in February, 1994 - making it
 			among the first 1,000 websites <b>in the world</b>. Over the years,
-			an enormous amount of disparate information has accummulated here (some 4235 pages
-			and counting!)
+			an enormous amount of information has accummulated here.
 			<p />
 			But some material - no matter how lovely - has a shelf-life.
 			Links to this historical and ephemeral information will be preserved here.
-			<p />
+			</>
+			/>
 			<SectionOptions section="etc" />
 	</main>
 </>

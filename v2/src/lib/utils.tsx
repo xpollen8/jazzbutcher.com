0d7865ebc06d456c8078a620c8b934419a484128
xpollen8 <github@xpollen8.com>
2023-10-28 18:48:39 -0700

    dw: liting

--- v2/src/lib/utils.tsx
+++ v2/src/lib/utils.tsx
@@ -297,7 +297,7 @@ export const parseProject = (extra: string) => ['wilson','sumo','eg','solo','duo
 export const pressFiltersInclude = (filters: string, f: string) => filters.split(',').find((s: string) => s === f);
 
 export const parseImage = (str?: string, width: number = 250) => {
-	const raw = truncAt(';;', str || '').trim();
+	const raw = str && truncAt(';;', str || '')?.trim() || '';
 	const base = raw.includes('http') ? raw : `https://v1.jazzbutcher.com${raw}`;
 	return {
 		image: `${base}.jpg`,

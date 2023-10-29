8ceb71e11b19eacd29e566d25c486591e68bfc05
xpollen8 <github@xpollen8.com>
2023-10-29 15:14:15 -0700

    dw: title cleanup

--- v2/src/app/press/[slug]/page.tsx
+++ v2/src/app/press/[slug]/page.tsx
@@ -12,11 +12,13 @@ const PressArticle = ({ params }: { params?: any }) => {
 	const { data, isLoading, error } = usePressArticle(params?.slug);
 
 	const article = data?.results[0];
+	const subTitle = article?.publication || (article?.type?.includes('pat') && 'The Butcher Writes') || article?.title;
+	const title = (subTitle) ? `${subTitle} - ` : '';
 
 	return (<>
 		<Suspense fallback=<>Loading...</> >
 			{(!isLoading && article) && (<>
-					<Header section="press" title={`${article?.publication || (article?.type?.includes('pat') && 'The Butcher Writes')} - ${article.dtpublished.substr(0, 10).replace(/-00/g, '-01')}`} />
+					<Header section="press" title={`${title} ${article.dtpublished.substr(0, 10).replace(/-00/g, '-01')}`} />
 					<main>
 						<PressItem item={article} />
 					</main>

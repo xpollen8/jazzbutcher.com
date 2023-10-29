fd5531dc7cc1e73ce35ea6c7842e273633310eba
xpollen8 <github@xpollen8.com>
2023-10-29 10:10:06 -0700

    dw: simplify

--- v2/src/components/Tag.tsx
+++ v2/src/components/Tag.tsx
@@ -1,7 +1,3 @@
-//import LetterHeader from '@/components/LetterHeader';
-
 const Tag = ({ style, className, children }: { style?: any, className?: any, children: React.ReactNode }) => <div style={style} className={(className) ? className : 'tag'}>{children}</div>
-
-//const Tag = ({ className, children }: { className?: any, children: React.ReactNode }) => <div className={className}><LetterHeader title={children} /></div>
 
 export default Tag;

0c04a949b0c6ee9cc4c2f20eb1e1824274633048
xpollen8 <github@xpollen8.com>
2023-10-29 15:23:44 -0700

    dw: more actions

--- v2/src/components/PageComments.tsx
+++ v2/src/components/PageComments.tsx
@@ -12,39 +12,66 @@ import IconReply from '@/svg/IconReply';
 import IconLike from '@/svg/IconLike';
 import IconAddComment from '@/svg/IconAddComment';
 
-import usePageComments, { usePageCommentLike, usePageCommentReply, submitPageCommentNew } from '@/lib/usePageComments';
+import usePageComments, { patchPageComment, deletePageComment, usePageCommentLike, usePageCommentReply, submitPageCommentReply, submitPageCommentNew } from '@/lib/usePageComments';
 import { type CommentType, dateDiff } from '@/lib/utils';
 
 const	CommentLike = (props: any) => {
 	const { onClick } = props;
 	const { data, isLoading, error } = usePageCommentLike(props);
-	return <IconLike style={{ width: '1.7em' }} onClick={onClick} />
+	return <IconLike className="pointable" style={{ width: '1.7em' }} onClick={onClick} />
 }
 
 const	CommentReply = (props: any) => {
 	const { replying, onClick } = props;
 	const { data, isLoading, error } = usePageCommentReply(props);
-	return (!replying) && <IconReply style={{ width: '1.7em' }} onClick={onClick} />
+	return (!replying) && <IconReply className="pointable" style={{ width: '1.7em' }} onClick={onClick} />
 }
 
-const	ShowNewCommentForm = (props: any) => {
+const	ToggleCommentForm = (props: any) => {
 	const { text, toggleCommentForm, showForm } = props;
 	return <Tag>
 		<div onClick={() => toggleCommentForm(true)}>
-			{text} {(!showForm) && <>
-				[<span className="text-blue-600">Add your own</span>]
+			<center>
+			{(!showForm) && <>
+				<span className="pointable text-blue-600 bg-white p-1">[ + ]</span>
 				{/*<IconAddComment style={{ width: '1.7em' }} />*/}
 			</>}
+			{' '}{text}
+			</center>
 		</div>
 	</Tag>
 }
 
+const DeleteComment = (props: any) => {
+	console.log("DeleteComment", props);
+	const [ deleting, setDeleting ] = useState(false);
+	const [ editing, setEditing ] = useState(false);
+	const [ confirm, setConfirm ] = useState(false);
+	return <>
+		{(!editing && deleting) && <span className="pointable text-blue-600 bg-white p-1" onClick={async () => {
+			setConfirm(!confirm);
+			await deletePageComment(props);
+			setDeleting(!deleting);
+			setEditing(false);
+		}}>[ Really Delete? ]</span>}
+		{(!editing && !deleting) && <span className="pointable text-red-600 bg-white p-1" onClick={() => {
+			setEditing(false);
+			setDeleting(!deleting)
+		}}>[ Delete ]</span>}
+		{(editing) && <><CommentForm {...props} editing={true} toggleCommentForm={setEditing} /></>}
+		{(!editing) && <span className="pointable text-green-600 bg-white p-1" onClick={() => {
+			setEditing(!editing)
+		}}>[ Edit ]</span>}
+	</>
+}
+
 const Comment = (props: CommentType & any, key: number) => {
-	const { feedback_id, subject, dtcreated, who, whence, comments, toggleCommentForm } = props;
+	const mySession = getSessionId();
+	const { editing=false, session, feedback_id, subject, dtcreated, who, whence, comments, toggleCommentForm } = props;
 	const [ replying, setReplying ] = useState(false);
 	return (
 		<div key={key} className="comment">
-			<div id="subject">{subject}</div>
+			<div id="subject">{(session === mySession) && <DeleteComment {...props} />}{subject}</div>
 			<div id="comments" className="annotation" dangerouslySetInnerHTML={{__html: comments }} />
 			<div id="who"><> <b>{whence}</b> {!!(who?.length) && <span className="smalltext">- {who} </span>}</></div>
 			<div id="date">{dateDiff(dtcreated, '')}</div>
@@ -56,37 +83,69 @@ const Comment = (props: CommentType & any, key: number) => {
 					toggleCommentForm(false);
 					setReplying(!replying);
 				}}/>
-				{(replying) && <div className="w-full"><NewCommentForm {...props} toggleCommentForm={setReplying} /></div>}
+				{(replying) && <div className="w-full"><CommentForm {...props} toggleCommentForm={setReplying} /></div>}
 			</div>
 		</div>
 	)
 }
 
-const NewCommentForm = (props: { feedback_id?: number, subject?: string, pathname: string, toggleCommentForm: any }) => {
-	const { feedback_id, subject: inSubject, pathname, toggleCommentForm } = props;
-	const [ subject, setSubject ] = useState((feedback_id) ? `RE: ${inSubject}` : '');
-	const [ comments, setComments ] = useState('');
-	const action = (feedback_id) ? `Replying to "${inSubject}"` : `Composing a message for "${pathname}"`;
-	return <>
+const CommentForm = (props: { editing?: boolean, session: string, feedback_id?: number, subject?: string, uri: string, toggleCommentForm: any }) => {
+	const { editing=false, session, feedback_id, subject: inSubject, uri, comments: inComments, who: inWho, whence: inWhence, toggleCommentForm } = props;
+	const [ subject, setSubject ] = useState((feedback_id) ? `RE: ${inSubject}` : inSubject);
+	const [ comments, setComments ] = useState(editing ? inComments : '');
+	const [ who, setWho ] = useState(editing ? inWho : '');
+	const [ whence, setWhence ] = useState(editing ? inWhence : '');
+	const action = (editing) ? `Editing your post` : ((feedback_id) ? `Replying to "${inSubject}"` : `Composing a message for "${uri}"`);
+	const formAction = (editing) ? patchPageComment : ((feedback_id) ? submitPageCommentReply : submitPageCommentNew);
+	return <div className="text-left">
 		<form
 			onSubmit={(ev: any) => {
 				ev.preventDefault();
-				submitPageCommentNew({
+				formAction({
+					session,
 					feedback_id,
-					pathname,
-					payload: {
-						subject,
-						comments
-					}
+					uri,
+					who,
+					whence,
+					subject,
+					comments,
 				})
 			}}
 			>
 			<Tag>
 				<span onClick={() => toggleCommentForm(false)}>
-					{action} [ DISMISS ]
+					<center>
+					<span className="pointable text-red-600 bg-white p-1">[ Dismiss ]</span>
+					{' '}{action}
+					</center>
 				</span>
 			</Tag>
-			<div className="comment">
+			<div className="listItem">
+				<label>
+				Name/Location (anonymous is fine)
+				</label>
+				<input type="text" value={whence}
+					placeholder="Name/Location"
+					style={{ outline: '1px solid green', width: '100%', height: '35px', background: '#eee', padding: '5px' }}
+					onChange={(ev: any) => {
+						ev.preventDefault();
+						setWhence(ev?.target?.value);
+					}}
+				/>
+				<label>
+				Email (will not be published in the clear)
+				</label>
+				<input type="text" value={who}
+					placeholder="Email"
+					style={{ outline: '1px solid green', width: '100%', height: '35px', background: '#eee', padding: '5px' }}
+					onChange={(ev: any) => {
+						ev.preventDefault();
+						setWho(ev?.target?.value);
+					}}
+				/>
+				<label>
+				Subject
+				</label>
 				<input type="text" value={subject}
 					placeholder="Subject"
 					style={{ outline: '1px solid green', width: '100%', height: '35px', background: '#eee', padding: '5px' }}
@@ -95,10 +154,12 @@ const NewCommentForm = (props: { feedback_id?: number, subject?: string, pathnam
 						setSubject(ev?.target?.value);
 					}}
 				/>
-				<p />
-				<input type="text" value={comments}
-					placeholder="New comment form will go here"
-					style={{ outline: '1px solid green', width: '100%', height: '250px', background: '#eee', padding: '5px' }}
+				<label>
+				Your Comments
+				</label>
+				<textarea value={comments}
+					placeholder="Your words here"
+					style={{ outline: '1px solid green', width: '100%', height: '200px', background: '#eee', padding: '5px' }}
 					onChange={(ev: any) => {
 						ev.preventDefault();
 						setComments(ev?.target?.value);
@@ -107,14 +168,14 @@ const NewCommentForm = (props: { feedback_id?: number, subject?: string, pathnam
 			</div>
 			<button type="submit">Submit</button>
 		</form>
-	</>
+	</div>
 }
 
-const Comments = (props: { pathname: string, comments: CommentType[], className?: string, toggleCommentForm: any, showForm: boolean }) => {
-	const { pathname, comments = [], className, toggleCommentForm, showForm } = props;
+const Comments = (props: { session: string, uri: string, comments: CommentType[], className?: string, toggleCommentForm: any, showForm: boolean }) => {
+	const { session, uri, comments = [], className, toggleCommentForm, showForm } = props;
 	return <div className={className}>
-		<ShowNewCommentForm text={`Visitor comments for "${pathname}"`}{...props} />
-		{comments?.map((c: CommentType, key: number) => <Comment key={key} {...c} toggleCommentForm={toggleCommentForm} />)}
+		<ToggleCommentForm text={`Visitor comments for "${uri}"`}{...props} />
+		{comments?.map((c: CommentType, key: number) => <Comment session={session} key={key} {...c} toggleCommentForm={toggleCommentForm} />)}
 	</div>
 }
 
@@ -155,11 +216,22 @@ export const CommentBubble = (props: any) => {
 	}
 }
 
+const uuidv4 = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c: string) => {
+	var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+	return v.toString(16);
+});
+
+const getSessionId = () => {
+	//return localStorage && localStorage.getItem('session') || localStorage.setItem('session', uuidv4());
+}
+
 const PageComments = ({ className }: { className?: string }) => {
-	const pathname = usePathname();
-	const { data, isLoading, error } = usePageComments(pathname);
+	const uri = usePathname()?.substr(1);
+	console.log("URI", uri);
+	const { data, isLoading, error } = usePageComments(uri);
 	const comments = data?.results || [];
 	const [ showForm, toggleCommentForm ] = useState(false);
+	const session = getSessionId();
 
 	return (<>
 		<Suspense fallback={<>Loading...</>}>
@@ -170,8 +242,8 @@ const PageComments = ({ className }: { className?: string }) => {
 					</CommentBubble>
 				</summary>
 				<div className="commentOverlay">
-				{(showForm) && <NewCommentForm pathname={pathname} toggleCommentForm={toggleCommentForm} />}
-				<Comments pathname={pathname} comments={comments} showForm={showForm} toggleCommentForm={toggleCommentForm} />
+				{(showForm) && <CommentForm session={session} uri={uri} toggleCommentForm={toggleCommentForm} />}
+				<Comments session={session} uri={uri} comments={comments} showForm={showForm} toggleCommentForm={toggleCommentForm} />
 				</div>
 			</details>
 		</Suspense>

b7a7e04137cc244ddd75507b48c895df8e88ec5d
xpollen8 <github@xpollen8.com>
2023-10-31 10:55:13 -0700

    dw: loading/error handling

--- v2/src/app/releases/[lookup]/page.tsx
+++ v2/src/app/releases/[lookup]/page.tsx
@@ -5,19 +5,28 @@ import { Suspense } from 'react';
 import Release from '@/components/Release';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Tag from '@/components/Tag';
 import useRelease from '@/lib/useRelease';
 
 const Page = ({ params }: { params?: any }) => {
 	const { data, isLoading, error } = useRelease(params?.lookup);
-	const release = data && data?.results[0];
+	const release = data?.results[0];
 	return (<Suspense fallback=<>Loading...</> >
-		{(!isLoading && release) && (<>
-			<Header project={release?.project} section="releases" title={release?.title} />
-			<main>
-				<Release release={release} />
-			</main>
-			<Footer />
-		</>)}
+		{(!isLoading) ? (<>
+			{(release) ? (<>
+				<Header project={release?.project} section="releases" title={release?.title} />
+				<main>
+					<Release release={release} />
+				</main>
+				<Footer />
+			</>)
+			:
+				<Tag>No results</Tag>
+			}
+		</>)
+		:
+			<Tag>Loading...</Tag>
+		}
 	</Suspense>
 	)
 }

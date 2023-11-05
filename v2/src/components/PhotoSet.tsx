f0b3876e9c57dd425eca5e3607f6ab168de0eef8
xpollen8 <github@xpollen8.com>
2023-11-04 23:17:29 -0700

    dw: more image CSS

--- v2/src/components/PhotoSet.tsx
+++ v2/src/components/PhotoSet.tsx
@@ -15,7 +15,7 @@ const PhotoSet = ({ title, photos, description, credit, credit_url, credit_date 
 			const { src, alt, credit, credit_url, credit_date, body } = w;
 			const [ part, ext ] = src.split('.');
 			return <div key={key} className="p-1 drop-shadow-sm border border-slate-500 text-center w-64">
-				<Link href={src}><Image key={key} unoptimized src={`${part}_250.${ext}`} width={250} height={250} alt={alt} /></Link>
+				<Link href={src}><Image key={key} unoptimized src={`${part}_250.${ext}`} width={350} height={350} alt={alt} /></Link>
 				{alt}
 				{(body) && <><p />&quot;<i>{body}</i>&quot;</>}
 				{(credit) && <><br /><Credit g={credit} u={credit_url} d={credit_date} /></>}

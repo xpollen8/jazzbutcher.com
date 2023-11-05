1b5e83bd2e519b6bb473263c665f3ffe63eb398c
xpollen8 <github@xpollen8.com>
2023-11-04 22:01:27 -0700

    dw: parseImage

--- v2/src/components/ImageStrip.tsx
+++ v2/src/components/ImageStrip.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Attribution } from '@/components/GenericWeb';
+import { parseImage } from '@/lib/utils';
 
 const ImageStrip = (props: any) => {
 	const images = props?.images;
@@ -8,18 +9,20 @@ const ImageStrip = (props: any) => {
 	const style = props?.style;
 	const className = props?.className;
 	return images && images?.length && <div style={style} className={className}>
-		{images?.map(([ image, caption, source, sourceurl, sourcedate ]: any, key: number) =>
-		<Link key={key} href={(image.startsWith('http')) ? `${image}.jpg` : `https://v1.jazzbutcher.com${image}.jpg`}>
-			<div >
-				<Image
-					alt={caption || 'album image'}
-					width={width * 3} height={width * 3}
-					src={(image.startsWith('http')) ? `${image}_250.jpg` : `https://v1.jazzbutcher.com${image}_250.jpg`}
-				/>
-				{(caption) && <><i>{caption}</i><br/></>}
-				{(source) && <Attribution g={source} u={sourceurl} d={sourcedate} />}
-			</div>
-		</Link>
+		{images?.map(([ inImage, caption, source, sourceurl, sourcedate ]: any, key: number) => {
+			const { image, thumb } = parseImage(inImage);
+			return <Link key={key} href={image}>
+				<div >
+					<Image
+						alt={caption || 'album image'}
+						width={width} height={width}
+						src={thumb}
+					/>
+					{(caption) && <><i>{caption}</i><br/></>}
+					{(source) && <Attribution g={source} u={sourceurl} d={sourcedate} />}
+				</div>
+			</Link>
+		}
 	)}
 	</div>
 }

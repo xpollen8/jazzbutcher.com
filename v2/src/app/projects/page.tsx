0cc8accf0b1af82003276965109232f097c32b47
xpollen8 <github@xpollen8.com>
2023-11-06 20:38:40 -0800

    dw: cleanup and clarify all project/collab/contrib classifications

--- v2/src/app/projects/page.tsx
+++ v2/src/app/projects/page.tsx
@@ -7,7 +7,7 @@ import LetterHeader from '@/components/LetterHeader';
 import Tag from '@/components/Tag';
 import { expand } from '@/lib/defines';
 
-const music_projects = [
+const projects = [
 	{ uri: '/projects/black_eg', text: 'The Black Eg', aux: "The brothers Von Dämmerung" },
 	{ uri: '/projects/sumosonic', text: 'Sumosonic', aux: "Aka Audio Aquatic" },
 	{ uri: '/projects/wilson', text: 'Wilson', aux: "Northampton 'super group'",
@@ -21,7 +21,7 @@ const music_projects = [
 	},
 ];
 
-const with_others = [
+const inbands = [
 	{ uri: '/projects/drones_club', text: 'Drones Club', aux: "Bass for Medieval Drum and Bass band",
 		children: <>
 			<b>{expand('dronesclub')}</b> started in 2014 in a medieval pub in Northampton.
@@ -33,10 +33,12 @@ const with_others = [
 ];
 
 const collaborations = [
-	{ uri: '/releases/va_canyoutalktothedude_2', text: "Four One And Only's", aux: "Vocals for 'The U.F.O. Man'", },
 	{ uri: '/releases/collaboration_cambodia', text: 'Cambodia', aux: "w/Cole Coonce" },
 	{ uri: '/releases/project_vaguely_familiar', text: 'Vaguely Familiar', aux: "w/Richard Formby + Sumishta Brahm"  },
-	{ uri: '/releases/collaboration_trampling_tokyo', text: "With Alan Moore", aux: "The 'Trampling Tokyo' single" },
+	{ uri: '/releases/collaboration_trampling_tokyo', text: "With Alan Moore", aux: "'Trampling Tokyo', 'London'" },
+];
+
+const live = [
 	{ uri: '/projects/the_undertakers', text: "The Undertakers", aux: "Unrehearsed covers, live",
 		children: <>
 		Formed as the house band of Slurp&apos;s Bar, Northampton,
@@ -46,9 +48,37 @@ const collaborations = [
 		and a total of about 43 people have ever seen them play live.
 		</>
 	},
-	{ uri: '/projects/prime_evils', text: "Prime Evils", aux: "Bass, Joe Woolley and Jonny Mattock",
-		children: <>(ref: <Link href='/gigs/2006/Jul27'>2006-07-27</Link>)</>
-	},
+	{ uri: '/gigs/2006/Jul27', text: "Prime Evils", aux: "Pat on Bass, with Joe Woolley and Jonny Mattock", },
+];
+
+const contributions = [
+	{ text: 'David J - Crocodile Tears & The Velvet Cosh (LP, 1985)', aux: 'Flute', },
+	{ text: 'Blue Moods turning Tail (EP 1986)', aux: 'glockenspiel', },
+	{ text: 'On Glass (LP 1986)', aux: 'Glockenspiel, vocals', },
+	{ text: 'Venus Fly Trap - Rocket USA (single, 1989)', aux: 'Tenor sax', },
+	{ text: 'M Walking on the Water - Pluto (LP, 1989)', aux: 'Vocals', },
+	{ text: 'Blue Aeroplanes - Beatsongs (LP 1991)', aux: 'Vocals', },
+	{ text: 'Up in a Down World (EP 1993)', aux: 'Vocals, co-writing credit on "Meantime"', },
+	{ text: 'Rough Music (LP 1994)', aux: 'Guitar, organ, co-writing credit on "Whatever Happened to our Golden Birds?"', },
+	{ text: 'Spacemen 3 - Recurring (LP 1990)', aux: 'Tenor sax, flute', },
+	{ text: 'Sonic Boom - Spectrum (LP 1989)', aux: 'Finger pops, vocals', },
+	{ text: 'Spectrum - Soul Kiss (Glide Divine) (LP, 1992)', aux: 'Tenor sax', },
+	{ text: 'Indian Summer (single, 1993)', aux: 'Electric guitar', },
+	{ text: 'Dave Kusworth & The Bounty Hunters - All the Heartbreak Stories (LP, 1991)', aux: 'Flute, tenor sax', },
+	{ text: 'Chelsea Paris - Tramway (LP 1992)', aux: 'Guitar, vocals', },
+	{ text: 'The Field Trip - Take to the Streets (EP, 1993)', aux: 'Tenor sax', },
+	{ text: 'The Black Watch - Jiggery-Pokery (LP, 2002)', aux: 'Vocals', },
+	{ uri: '/projects/stranger_tractors', text: 'Stranger Tractors - Vibration (CD single, 1995)', aux: 'Drums, vocals', },
+	{ uri: '/releases/va_canyoutalktothedude_2', text: "Four One And Only's - Holy (LP, 1996)", aux: "Vocals for 'The U.F.O. Man'", },
+	{ text: 'The Purelove - King Pop (Marsh Marigold Records, 1997)', aux: 'Guitar', },
+	{ text: 'The Purelove - Live (1997 tour of Germany)', aux: 'Guitar', },
+];
+
+const producer = [
+	{ text: "Love Ambassadeux - Black Mischief", aux: "(single, Rumpo Records, 1985)", },
+	{ text: "13 Frightened Girls - Lost at Sea", aux: '(3 track 12", Foundation, 1991)', },
+	{ text: "Venus Fly Trap - Pandora's Box", aux: "(LP, Danceteria 1992)", },
+	{ text: "Chelsea Paris - Tramway", aux: "(LP, Virgin France, 1992)", },
 ];
 
 const other = [
@@ -56,48 +86,29 @@ const other = [
 	{ uri: '/projects/bookstore', text: "Pat's Bookstore" },
 ];
 
+const meta = [
+			{ title: "Musical Side Projects", subhead: "Where Pat is the guiding force", data: projects },
+			{ title: "In Others' Bands", subhead: "Where Pat is a musician in another's group", data: inbands },
+			{ title: "Pub Bands", subhead: "Where Pat plays in live bands with friends", data: live },
+			{ title: "Collaborations", subhead: "Project-based, small group/collaborative efforts", data: collaborations },
+			{ title: "Contributions", subhead: "As a producer, studio musician/one-time efforts", data: contributions },
+			{ title: "Producer", data: producer },
+			{ title: "Other Substantial Efforts", data: other },
+];
+
 const Project = () => 
 <>
 	<Header section="projects" />
 		<main>
-		<LetterHeader title="Music Side Projects" subhead="Pat-driven music groups" />
-		{music_projects.map(MakeSimpleURI)}
-		<LetterHeader title="Music With Others" subhead="Pat in other groups" />
-		{with_others.map(MakeSimpleURI)}
-		<LetterHeader title="Short Collaborations" subhead="one-offs and projects-based efforts" />
-		{collaborations.map(MakeSimpleURI)}
-		<LetterHeader title="Not necessarily music" />
-		{other.map(MakeSimpleURI)}
-<Tag>Productions</Tag>
-<ol style={{ marginLeft: '10px' }} className="listItem">
-	<li>Love Ambassadeux - Black Mischief (single, Rumpo Records, 1985)</li>
-	<li>13 Frightened Girls - Lost at Sea (3 track 12&quot;, Foundation, 1991)</li>
-	<li>Venus Fly Trap - Pandora&apos;s Box (LP, Danceteria 1992)</li>
-	<li>Chelsea Paris - Tramway (LP, Virgin France, 1992)</li>
-	</ol>
-	<Tag>Contributions</Tag>
-	<ol style={{ marginLeft: '10px' }} className="listItem ml-5">
-	<li>David J - Crocodile Tears & The Velvet Cosh (LP, 1985) - Flute</li>
-	<li>Blue Moods turning Tail (EP 1986) - glockenspiel</li>
-	<li>On Glass (LP 1986) - Glockenspiel, vocals</li>
-	<li>Venus Fly Trap - Rocket USA (single, 1989) - Tenor sax</li>
-	<li>M Walking on the Water - Pluto (LP, 1989) - Vocals</li>
-	<li>Blue Aeroplanes - Beatsongs (LP 1991) - Vocals</li>
-	<li>Up in a Down World (EP 1993) - Vocals, co-writing credit on &quot;Meantime&quot;</li>
-	<li>Rough Music (LP 1994) - Guitar, organ, co-writing credit on &quot;Whatever Happened to our Golden Birds?&quot;</li>
-	<li>Spacemen 3 - Recurring (LP 1990) - Tenor sax, flute</li>
-	<li>Sonic Boom - Spectrum (LP 1989) - Finger pops, vocals</li>
-	<li>Spectrum - Soul Kiss (Glide Divine) (LP, 1992) - Tenor sax</li>
-	<li>Indian Summer (single, 1993) - Electric guitar</li>
-	<li>Dave Kusworth & The Bounty Hunters - All the Heartbreak Stories (LP, 1991) - Flute, tenor sax</li>
-	<li>Chelsea Paris - Tramway (LP 1992) - Guitar, vocals</li>
-	<li>The Field Trip - Take to the Streets (EP, 1993) - Tenor sax</li>
-	<li>The Black Watch - Jiggery-Pokery (LP, 2002) - Vocals (listen@youtube)</li>
-	<li>The Four One And Only&apos;s - Holy (LP, 1996) - Vocals</li>
-	<li>Stranger Tractors - Vibration (CD single, 1995) - Drums, vocals</li>
-	<li>The Purelove - King Pop (Marsh Marigold Records, 1997) - Guitar</li>
-	<li>The Purelove - Live (1997 tour of Germany): Guitar</li>
-</ol>
+			{meta.map((m: any, key: number) => {
+				return <div key={key} className="mt-5">
+					<Tag>{m.title} {(m.subhead) && <span className="smalltext">- {m.subhead}</span>}</Tag>
+						<p />
+					<div className="listItem">
+						{m.data.map(MakeSimpleURI)}
+					</div>
+				</div>
+			})}
 		</main>
 	<Footer />
 </>

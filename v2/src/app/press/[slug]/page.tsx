f855c9f8545b9005b908614cd8058a2b364b4890
David Whittemore <github@xpollen8.com>
2023-11-09 19:07:52 -0800

    dw: use api error

--- v2/src/app/press/[slug]/page.tsx
+++ v2/src/app/press/[slug]/page.tsx
@@ -9,13 +9,14 @@ import PressItem from '@/components/PressItem';
 import usePressArticle from '@/lib/usePressArticle';
 
 const PressArticle = ({ params }: { params?: any }) => {
-	const { data, isLoading, error } = usePressArticle(params?.slug);
+	const { data, isLoading } = usePressArticle(params?.slug);
 
 	const article = data?.results[0];
 	const subTitle = article?.publication || (article?.type?.includes('pat') && 'The Butcher Writes') || article?.title;
 	const title = (subTitle) ? `${subTitle} - ` : '';
 
 	return (<>
+		{(data?.error) && <h1>{data?.error}</h1>}
 		<Suspense fallback=<>Loading...</> >
 			{(!isLoading && article) && (<>
 					<Header section="press" title={`${title} ${article.dtpublished.substr(0, 10).replace(/-00/g, '-01')}`} />

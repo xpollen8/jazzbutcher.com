0fa0ff37ac16b41d84653ed553e7ce0b66dac70d
xpollen8 <github@xpollen8.com>
2023-11-13 11:47:59 -0800

    dw: font tweaks

--- v2/src/components/PhotoSet.tsx
+++ v2/src/components/PhotoSet.tsx
@@ -20,7 +20,7 @@ const PhotoSet = ({ title, photos, pdf, description, credit, credit_url, credit_
 			const { image, thumb } = parseImage(src, 250);
 			return <div key={key} className="text-center drop-shadow-sm border border-slate-400 rounded-sm">
 				{(image) && <Link href={image}>{(thumb) && <Image className="rounded-sm" key={key} unoptimized src={thumb} width={350} height={350} alt={alt} />}</Link>}
-				<div className="text-base font-light -mt-6">
+				<div className="text-sm font-light -mt-6">
 				{(alt) && <span className="px-1">{alt}</span>}
 				{(body) && <span className="px-1">&quot;<i>{body}</i>&quot;</span>}
 				{(credit) && <span className="px-1"><br /><Credit g={credit} u={credit_url} d={credit_date} /></span>}

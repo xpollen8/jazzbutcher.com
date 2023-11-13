60df362055f7031ab038b37b8bc68e1f82d942a3
xpollen8 <github@xpollen8.com>
2023-11-12 19:08:02 -0800

    dw: cleanup

--- v2/src/components/EmbedMedia.tsx
+++ v2/src/components/EmbedMedia.tsx
@@ -151,7 +151,6 @@ const EmbedMedia = ({ data = {}, className, children, disableVideo=false } : { d
 	// const mediaDomain = parseDomain(mediaurl);
 	// const creditDomain = parseDomain(mediacrediturl);
 	const useMediaurl = mediaurl && imageBase(mediaurl);
-	//const useMediaurl = (mediaurl && mediaurl.startsWith('/')) ? `https://v1.jazzbutcher.com${mediaurl}` : mediaurl;
 	return (<>
 		{(() => {
 			if (useMediaurl && !disableVideo) {

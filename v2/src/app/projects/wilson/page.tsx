74dd99793a5824e9779347447b6271f00e46b308
xpollen8 <github@xpollen8.com>
2023-11-18 21:38:14 -0800

    dw: added Coverly cartoon

--- v2/src/app/projects/wilson/page.tsx
+++ v2/src/app/projects/wilson/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import MakeSimpleURI from '@/components/MakeSimpleURI';
@@ -6,6 +7,7 @@ import FilterReleases from '@/components/FilterReleases';
 import FilterPresses from '@/components/FilterPresses';
 import AudioReleases from '@/components/AudioReleases';
 import VideoReleases from '@/components/VideoReleases';
+import { Credit } from '@/components/GenericWeb';
 
 const Wilson = (props: any) => 
 <>
@@ -13,6 +15,10 @@ const Wilson = (props: any) =>
 	<main>
 		<MakeSimpleURI uri='/press/20040512_wilson_about.html?section=etc' text="Wilson Explainer" aux='(2002)'>
 			I started the Wilson project fondly imagining that it would be a solo thing
+		<div className="listItem">
+		<Link href="/images/20030927_DaveCoverly_Wilson.jpg"><Image unoptimized src="/images/20030927_DaveCoverly_Wilson_250.jpg" width={250} height={250} alt="Dave Coverly cartoon" /></Link>
+		<Credit g="Dave Coverly" u="https://speedbump.com" d="2003-09-27" />
+		</div>
 		</MakeSimpleURI>
 		<FilterReleases project='wilson' filters={props.searchParams?.filters} />
 		<AudioReleases project='wilson' />

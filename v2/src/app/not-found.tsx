8213789bd36f3ab5f9c9ee56996bbf47a1130771
xpollen8 <github@xpollen8.com>
2023-11-29 11:56:19 -0800

    dw: initial checkin

---
+++ v2/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+export default () =>
+<>
+	<Header section='jbc' />
+	<main>
+		<h1>The words you were looking for.. went!</h1>
+	</main>
+	<Footer />
+</>

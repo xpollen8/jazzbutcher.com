67659c0a52ef92e6630ef40a14a58dd547ad19d6
xpollen8 <github@xpollen8.com>
2024-02-16 19:00:31 -0800

    dw: 30 years

--- v2/src/app/layout.tsx
+++ v2/src/app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
 			<body>
+				<div style={{ border: 'solid', backgroundColor: '#ffffff', padding: '3px', textAlign: 'center' }}>
+					<h2>
+						Celebrating 30 goddamned WWW years: <a href="https://v1.jazzbutcher.com/articles/1994/0020.html"><span style={{ color: '#00f' }}>1994-02-17</span></a>
+							<br/>
+								One of the 1st 1,000 websites IN THE WORLD.
+									</h2>
+										</div>
 				{children}
 				<Analytics />
 				<SpeedInsights />

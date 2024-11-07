2843fa5ebd50ead7cf656f909c42f4e761bb7195
xpollen8 <github@xpollen8.com>
2024-11-07 10:35:00 -0800

    dw: layout change

--- v2/src/app/memoriam/page.tsx
+++ v2/src/app/memoriam/page.tsx
@@ -12,14 +12,12 @@ const Memoriam = () =>
 	<Header section='memoriam' />
 	<main>
 		<div className="flex flex-wrap justify-center gap-1">
-			<div className="border p-3 bg-slate-100 drop-shadow-sm max-w-xl grow mb-3">
+			<div className="border p-3 bg-slate-100 drop-shadow-sm max-w-xl grow mb-2">
 				<Memorial />
-			</div>
-			<div className="w-1/4 max-w-xl grow">
-				<TributeAudio />
-			</div>
-			<div className="w-1/4 max-w-xl grow">
 				<TributeText />
+			</div>
+			<div className="max-w-xl grow">
+				<TributeAudio />
 			</div>
 		</div>
 	</main>

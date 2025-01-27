9333ea5a8c75ffdfda86fec95cf553d45f282d9d
xpollen8 <github@xpollen8.com>
2025-01-27 12:55:57 -0800

    dw: demo of gigplayer

--- v2/src/app/trivia/page.tsx
+++ v2/src/app/trivia/page.tsx
@@ -1,11 +1,69 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import GigPlayer, { GigPlayerHeader, GigPlayerFooter } from '@/components/GigPlayer';
 
 const Trivia = () => 
 <>
 	<Header section="trivia" />
 	<main className="etc">
 		<h1>This section is not yet ported from old JBC site</h1>
+    <div>
+      <GigPlayer
+				src="https://s3.amazonaws.com/assets.jazzbutcher.com/audio/gigs/20051027/20051027_MastersOfBudvar_PatFish_Hamburg.mp3"
+				tracks={[
+					{ title: 'Introduction', start: 0},
+					{ title: 'Quality People', start: 78},
+					{ title: 'Shakey', start: 314},
+					{ title: 'Buffalo Sniper', start: 656, artist: 'Wilson'},
+					// Add more songs as needed
+				]}
+				header=<GigPlayerHeader
+					artist="Pat Fish's set"
+					venue='KIR'
+					city='Hamburg'
+					country='Germany'
+					datetime='2005-10-27'
+				/>
+				footer=<GigPlayerFooter
+					comment="Masters of Budvar present The Jazz Butcher Conspiracy Soul Revue featuring Pat Fish , Max Eider & Owen Jones plus Curtis E. Johnson and Joe Woolley"
+					credit='Owen Jones;;'
+				/>
+			/>
+      <GigPlayer
+				src="https://s3.amazonaws.com/assets.jazzbutcher.com/audio/gigs/20051027/20051027_MastersOfBudvar_JazzButcher_Hamburg.mp3"
+				tracks={[
+					{ title: `Introduction`, start: 0 },
+					{ title: `Partytime`, start: 49, comment: `Pat + Max` },
+					{ title: `It Has To Be You`, author: `Max Eider`, start: 386  },
+					{ title: `La Mer`, start: 707 },
+					{ title: `Who Loves You Now?`, start: 935 },
+					{ title: `Whaddya`, start: 1209, version: `backing track` },
+					{ title: `Southern Mark Smith`, start: 1488.5 },
+					{ title: `Girls Who Keep Goldfish`, start: 1891, version: `drum machine` },
+					{ title: `Shame About You`, start: 2111, version: `backing track` },
+					{ title: `My Desert`, start: 2372 },
+					{ title: `Caroline Wheeler's Birthday Present`, start: 2592, version: `spooky synth intro` },
+					{ title: `Bigfoot Motel`, start: 2909 },
+					{ title: `Zombie Love`, start: 3192.5 },
+					//encore 1
+					{ title: `I Wish I Could Sprechen Sie Deutsch`, start: 3635 },
+					{ title: `Soul Happy Hour`, start: 3663 },
+					{ title: `Take The Skinheads Bowling`, author: `Camper Van Beethoven`, start: 3822 },
+					{ title: `Sweet Jane`, author: `Lou Reed`, start: 4213.5 },
+				]}
+				header=<GigPlayerHeader
+					artist="Full band set"
+					venue='KIR'
+					city='Hamburg'
+					country='Germany'
+					datetime='2005-10-27'
+				/>
+				footer=<GigPlayerFooter
+					comment="Masters of Budvar present The Jazz Butcher Conspiracy Soul Revue featuring Pat Fish , Max Eider & Owen Jones plus Curtis E. Johnson and Joe Woolley"
+					credit='Owen Jones;;'
+				/>
+			/>
+    </div>
 	</main>
 	<Footer />
 </>

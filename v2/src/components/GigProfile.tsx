9ecafb1f9802d5379bc6fb41e6518c412c12997d
xpollen8 <github@xpollen8.com>
2025-07-09 15:30:24 -0700

    dw: simpler inset logic

--- v2/src/components/GigProfile.tsx
+++ v2/src/components/GigProfile.tsx
@@ -157,9 +157,10 @@ const GigPlayed = (data: any) => {
 	const playlists: any[] = [];
 	// "Entire Shows"
 	data?.filter((d:any) => d?.mediaurl?.includes('.mp3') && d.ordinal === 0)?.forEach((d:any) => {
-		const { setnum, ordinal, song, comment, mediaurl, mediaurlcredit } = d;
+		const { type, setnum, ordinal, song, comment, mediaurl, mediaurlcredit } = d;
 		// add if in playlist format (start is set)
-		const inSet = data?.filter((d:any) => d.setnum === setnum && d.ordinal !== 0 && d.start);
+		const inSet = data?.filter((d:any) => d.start !== null);
+		console.log({ data, inSet });
 		if (inSet?.length) {
 			playlists.push({
 				src: mediaurl,

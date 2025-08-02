fcca0c01ac2f5231eb4dc0fe94bbe6443f15abd3
xpollen8 <github@xpollen8.com>
2025-08-01 18:09:02 -0700

    dw: simpler homepage

--- v2/src/components/Memorial.tsx
+++ v2/src/components/Memorial.tsx
@@ -8,7 +8,7 @@ const Memorial = (props: { home?: boolean }) => {
 		<div>AKA: Pat Fish - The Jazz Butcher</div>
 		<b> 1957-12-20 .. 2021-10-05</b>
 		<p />
-			{(props?.home) ? <Image src="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker_500.jpg" width={450} height={450} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} /> : (<Link href="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg">
+			{(!props?.home) && (<Link href="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg">
 				<Image src="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg" width={450} height={450} alt="Grave marker" style={{ border: "1px", borderRadius: "10px" }} />
 			</Link>)}
 				{(!props?.home) &&
@@ -18,8 +18,8 @@ const Memorial = (props: { home?: boolean }) => {
 				Gentleman Adventurers, and hope it will bring peace to all the JBC fans, who still miss him, and any those who may wish to visit Pat in the future. Love to all.
 				<br />
 				Sarah x
+				<Credit g="onetoysarah" u="https://www.instagram.com/r_v_ra/p/DFurSIqqJjN/?img_index=1" d="2025-02-06" />
 				</div>}
-		<Credit g="onetoysarah" u="https://www.instagram.com/r_v_ra/p/DFurSIqqJjN/?img_index=1" d="2025-02-06" />
 		<p />
 			{(props?.home) ? <Image src="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute_500.jpg" width={450} height={450} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} /> : (<Link href="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute.jpg">
 				<Image src="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute_500.jpg" width={450} height={450} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} />

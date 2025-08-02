1c1119e77d234b3fde8e4a9dbf707a9912fba0dc
xpollen8 <github@xpollen8.com>
2025-08-01 18:14:44 -0700

    dw: simpler homepage

--- v2/src/components/Memorial.tsx
+++ v2/src/components/Memorial.tsx
@@ -4,9 +4,6 @@ import { Credit } from '@/components/GenericWeb';
 
 const Memorial = (props: { home?: boolean }) => {
 	return (<center>
-		<div className="text-2xl">Patrick Guy Sibley Huntrods</div>
-		<div>AKA: Pat Fish - The Jazz Butcher</div>
-		<b> 1957-12-20 .. 2021-10-05</b>
 		<p />
 			{(!props?.home) && (<Link href="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg">
 				<Image src="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg" width={450} height={450} alt="Grave marker" style={{ border: "1px", borderRadius: "10px" }} />
@@ -25,6 +22,10 @@ const Memorial = (props: { home?: boolean }) => {
 				<Image src="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute_500.jpg" width={450} height={450} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} />
 			</Link>)}
 		<Credit g="Jooles Joyce" />
+		<p />
+		<div>Pat Fish / The Jazz Butcher</div>
+		<b>Patrick Guy Sibley Huntrods</b>
+		<div> 1957-12-20 / 2021-10-05</div>
 	</center>)
 }
 

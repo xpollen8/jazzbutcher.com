f14646a11da31417aa0b47fbc45084572b1c442a
xpollen8 <github@xpollen8.com>
2025-08-03 15:37:35 -0700

    dw: cleanup

--- v2/src/app/layout.tsx
+++ v2/src/app/layout.tsx
@@ -10,15 +10,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-					/*
-				<div style={{ border: 'solid', backgroundColor: '#ffffff', padding: '3px', textAlign: 'center' }}>
-					<h2>
-						Celebrating 30 goddamned WWW years: <a href="https://v1.jazzbutcher.com/articles/1994/0020.html"><span style={{ color: '#00f' }}>1994-02-17</span></a>
-							<br/>
-								One of the 1st 1,000 websites IN THE WORLD.
-									</h2>
-										</div>
-										*/
   return (
     <html lang="en" className={GeistSans.className}>
 			<body>

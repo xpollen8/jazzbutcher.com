cbe85a97b76b61516632c308e8645c413885d42d
xpollen8 <github@xpollen8.com>
2025-08-03 19:28:09 -0700

    dw: memoriam CSS

--- v2/src/components/Memorial.tsx
+++ v2/src/components/Memorial.tsx
@@ -2,34 +2,50 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Credit } from '@/components/GenericWeb';
 
-const Memorial = (props: { home?: boolean }) => {
-	return (<center>
-		<p />
-		{(props?.home) ? <Image src="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute_500.jpg" width={500} height={500} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} /> : (<Link href="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute.jpg">
-			<Image src="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute_500.jpg" width={500} height={500} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} />
-		</Link>)}
-		<Credit g="Jooles Joyce" />
-		<p />
-		<div>Pat Fish / The Jazz Butcher</div>
-		<b>Patrick Guy Sibley Huntrods</b>
-		<div> 1957-12-20 / 2021-10-05</div>
-		<p />
-		{(!props?.home) && <>
-			<Link href="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg">
-			<Image src="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg" width={400} height={400} alt="Grave marker" style={{ border: "1px", borderRadius: "10px" }} />
-		</Link>
-		<br />
-		<div className="smalltext" style={{ width: '450px' }}>
-		I loved Pat very dearly. He was the coolest man I ever met. When I first visited Pat&apos;s plot, after the pandemic, there was no marker - but I came back and a kind
-		lady at the Olney burial ground showed me to his resting place. Here is the memorial plaque I arranged for Pat. I know Pat was proud of this song, Last of the
-		Gentleman Adventurers, and hope it will bring peace to all the JBC fans, who still miss him, and any those who may wish to visit Pat in the future. Love to all.
-		<br />
-		Sarah x
-		<Credit g="onetoysarah" u="https://www.instagram.com/r_v_ra/p/DFurSIqqJjN/?img_index=1" d="2025-02-06" />
+const MemorialGuitar = ({ link }: { link: string }) => <center>
+	<div className="flex flex-col min-[500px]:flex-row gap-5">
+		<div className="w-full min-[500px]:w-1/2">
+			<Link href={link}><Image src="https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute_500.jpg" width={300} height={300} alt="Guitar on tree" style={{ border: "1px", borderRadius: "10px" }} /></Link>
+			<Credit g="Jooles Joyce" />
 		</div>
-		</>}
+		<div className="w-full min-[500px]:w-1/2" style={{ padding: '10px', textAlign: 'left' }}>
+			<p />
+			Pat Fish
+			<p />
+			The Jazz Butcher
+			<p />
+			<b>Patrick Guy Sibley Huntrods</b>
+			<p />
+			<div className="smalltext"> 1957-12-20 / 2021-10-05</div>
+		</div>
+	</div>
+</center>
+
+const MemorialSarah = () => <center>
+	<div className="flex flex-col min-[500px]:flex-row gap-5">
+		<div className="w-full min-[500px]:w-1/2">
+			<Link href="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg">
+				<Image src="https://v1.jazzbutcher.com/images/20250206_onetoysarah_gravemarker.jpg" width={300} height={300} alt="Grave marker" style={{ border: "1px", borderRadius: "10px" }} />
+			</Link>
+			<Credit g="onetoysarah" u="https://www.instagram.com/r_v_ra/p/DFurSIqqJjN/?img_index=1" d="2025-02-06" />
+		</div>
+		<div className="w-full min-[500px]:w-1/2">
+			<div className="smalltext" style={{ padding: '10px', textAlign: 'left' }}>
+				I loved Pat very dearly. He was the coolest man I ever met. When I first visited Pat&apos;s plot, after the pandemic, there was no marker - but I came back and a kind
+				lady at the Olney burial ground showed me to his resting place. Here is the memorial plaque I arranged for Pat. I know Pat was proud of this song, Last of the
+				Gentleman Adventurers, and hope it will bring peace to all the JBC fans, who still miss him, and any those who may wish to visit Pat in the future. Love to all.
+			</div>
+		</div>
+	</div>
+</center>
+
+const Memorial = (props: { home?: boolean }) => {
+	return (<>
+		<MemorialGuitar link={(props?.home) ? '/memoriam' : 'https://v1.jazzbutcher.com/images/20211008_joolesjoyce_tribute.jpg'} />
 		<p />
-	</center>)
+		{(!props?.home) && <MemorialSarah />}
+		<p />
+	</>)
 }
 
 export default Memorial;

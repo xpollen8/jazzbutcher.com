8585c72992c484ebd31f9393008b48acda850555
xpollen8 <github@xpollen8.com>
2025-08-10 15:36:01 -0700

    dw: better quoting

--- v2/src/components/GigSearch.tsx
+++ v2/src/components/GigSearch.tsx
@@ -130,7 +130,7 @@ export const BannerGigs = (results: HashedType, searchYear?: number) => {
 	const searchType = gigSearchOptionsByType(results?.type).text;
 	const numMatched = results?.numResults ?? 0;
 	const bannerYear = (searchYear) ? `In ${searchYear}, ` : '';
-	const bannerTerms = (results?.searchTerms || results?.query) ? <>{searchType}: <i>{results?.searchTerms || results?.query}</i></> : '';
+	const bannerTerms = (results?.searchTerms || results?.query) ? <>{searchType}: <i>"{results?.searchTerms || results?.query}"</i></> : '';
 	const bannerClass = (numMatched) ? 'search found' : 'search notfound';
 	const bannerText = `${bannerYear}${numMatched} gig${(numMatched === 1) ? '' : 's'} matched`;
 

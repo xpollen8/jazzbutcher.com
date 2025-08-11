6d26af07e81603838bb2a3c7889ff77ed4ec4414
xpollen8 <github@xpollen8.com>
2025-08-11 15:08:56 -0700

    dw: static lookup and utils for return

--- v2/src/lib/useRelease.ts
+++ v2/src/lib/useRelease.ts
@@ -1,28 +1,10 @@
-/*
-import useSWR from 'swr';
+import releasesStatic from '@/../public/data/releases.json';
+import { returnSWR } from '@/lib/utils';
 
 const useRelease = (lookup?: string) => {
-	const fetcher = (url: string) => fetch(url).then((res) => res.json());
-	const { data, isLoading, error } = useSWR(`/api/release_by_lookup/${lookup}`, (lookup) ? fetcher : () => {});
-	return {
-		data,
-		isLoading,
-		error,
-	}
-}
-*/
-
-
-import MakeAlbumBlurb from '@/components/MakeAlbumBlurb';
-import releasesStatic from '@/../public/data/releases.json';
-import { returnResults } from '@/lib/utils';
-
-const useRelease = (lookup: string) => {
 	const href = `/albums/${lookup}.html`;
 	const	results = releasesStatic?.results?.filter((f: any) => f.href === href) || [];
-	return {
-		data: returnResults(results),
-	}
+	return returnSWR(results);
 }
 
 export default useRelease;

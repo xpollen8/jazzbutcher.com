c25d9b58ce915e00d3399163394c053f4f1c969b
xpollen8 <github@xpollen8.com>
2025-08-15 16:13:35 -0700

    dw: pass options into api call

--- v2/src/lib/useRecentUpdates.ts
+++ v2/src/lib/useRecentUpdates.ts
@@ -1,8 +1,9 @@
 import useSWR from 'swr';
+import { type HashedType } from '@/lib/utils';
 
-const useRecentUpdates = () => {
+const useRecentUpdates = (options: HashedType) => {
 	const fetcher = (url: string) => fetch(url).then((res) => res.json());
-	const { data, error, isLoading } = useSWR(`/api/recent_updates`, fetcher);
+	const { data, error, isLoading } = useSWR(`/api/recent_updates/${JSON.stringify(options)}`, fetcher);
 	return {
 		data,
 		isLoading,

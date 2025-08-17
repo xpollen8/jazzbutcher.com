7f0a2fb2e5805fe27df42146d7ee418d1a06b405
xpollen8 <github@xpollen8.com>
2025-08-17 16:52:10 -0700

    dw: Contributors->Contributions

---
+++ v2/src/lib/useContributions.ts
@@ -0,0 +1,14 @@
+import useSWR from 'swr';
+import { type HashedType } from '@/lib/utils';
+
+const useContributions = (options: HashedType) => {
+	const fetcher = (url: string) => fetch(url).then((res) => res.json());
+	const { data, error, isLoading } = useSWR(`/api/contributions/${JSON.stringify(options)}`, fetcher);
+	return {
+		data,
+		isLoading,
+		error,
+	}
+}
+
+export default useContributions;

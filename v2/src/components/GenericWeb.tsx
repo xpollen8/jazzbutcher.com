eef3df463366561d25fd625a6413c53dccc09dc1
xpollen8 <github@xpollen8.com>
2025-08-27 12:12:15 -0700

    dw: remove complications

--- v2/src/components/GenericWeb.tsx
+++ v2/src/components/GenericWeb.tsx
@@ -132,7 +132,7 @@ export const ParsedCaption = (props: { credit?: string, url?: string, credit_url
 	const useCaption = removeHTML(props?.caption || props?.image_caption);
 	const useURL = (useCredit) ? `/contributions/${useCredit}` : props?.credit_url;
 	return (<>
-		{expandAll(useCaption, false)}
+		{expandAll(useCaption)}
 		{(needBR) && <br />}
 		<Credit g={useCredit} u={useURL} d={props?.credit_date} />
 	</>)

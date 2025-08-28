f9f08a20360c268b3831e5c89843aa2e9d1e0fcf
xpollen8 <github@xpollen8.com>
2025-08-27 21:07:47 -0700

    dw: use children decomposition

--- v2/src/components/InfoTag.tsx
+++ v2/src/components/InfoTag.tsx
@@ -1,5 +1,5 @@
-const InfoTag = ({ text }: { text: string }) => {
-	return <span style={{ zIndex: '1', position: 'relative', top: 12, left: -7, border: '1px solid white', padding: '7px', borderTopRightRadius: '20px', borderBottomRightRadius: '20px', color: 'white' }} className="bg-slate-700 drop-shadow-lg">{text}</span>
+const InfoTag = ({ children }: { children?: string | React.ReactElement }) => {
+	return <span style={{ zIndex: '1', position: 'relative', top: 12, left: -7, border: '1px solid white', padding: '7px', borderTopRightRadius: '20px', borderBottomRightRadius: '20px', color: 'white' }} className="bg-slate-700 drop-shadow-lg">{children}</span>
 }
 
 export default InfoTag;

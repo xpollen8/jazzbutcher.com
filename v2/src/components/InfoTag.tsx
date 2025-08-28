38db4a278b61ea0d595ee171d334aa666f37e7eb
xpollen8 <github@xpollen8.com>
2025-08-27 21:13:48 -0700

    dw: formatting

--- v2/src/components/InfoTag.tsx
+++ v2/src/components/InfoTag.tsx
@@ -1,5 +1,5 @@
 const InfoTag = ({ children }: { children?: string | React.ReactElement }) => {
-	return <span style={{ zIndex: '1', position: 'relative', top: 12, left: -7, border: '1px solid white', padding: '7px', borderTopRightRadius: '20px', borderBottomRightRadius: '20px', color: 'white' }} className="bg-slate-700 drop-shadow-lg">{children}</span>
+	return <span style={{ zIndex: '1', position: 'relative', top: 12, left: -5, border: '1px solid white', padding: '5px', borderTopRightRadius: '20px', borderBottomRightRadius: '20px', color: 'white' }} className="bg-slate-700 drop-shadow-lg">{children}</span>
 }
 
 export default InfoTag;

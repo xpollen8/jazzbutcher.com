05ec32632da9d51a85c6bc411780ce906512ebb8
xpollen8 <github@xpollen8.com>
2025-09-02 23:34:22 -0700

    dw: ensure a summary

--- v2/src/components/Contributions.tsx
+++ v2/src/components/Contributions.tsx
@@ -196,7 +196,7 @@ const Contributions = ({ options, label='Community contribution' }: HashedType) 
 				type: prettyType(r?.type, 'Press article'),
 				added: r?.dtpublished,
 				summary: r?.title || r?.publication,
-				caption: r?.headline?.substring(0, 50) + '...',
+				caption: (r?.headline || r?.publication)?.substring(0, 50) + '...',
 				href: r?.url,
 			}
 		);

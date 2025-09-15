4160b52895446bb01f91bd76700cc10e4051770d
xpollen8 <github@xpollen8.com>
2025-09-15 12:18:26 -0700

    dw: accept date

--- v2/src/components/FeaturedItem.tsx
+++ v2/src/components/FeaturedItem.tsx
@@ -4,7 +4,7 @@ import { parseImage, parseDomain, linkInternal, linkExternal } from '@/lib/utils
 import { expand } from '@/lib/defines';
 import { removeHTML } from '@/components/GenericWeb';
 
-const FeaturedItem = ({ link, image, alt='thumbnail', title, buy, buy_title, label, media, className, children }: {
+const FeaturedItem = ({ date, link, image, alt='thumbnail', title, buy, buy_title, label, media, className, children }: {
 	link?: string
 	image?: string
 	label?: string
@@ -55,6 +55,7 @@ const FeaturedItem = ({ link, image, alt='thumbnail', title, buy, buy_title, lab
 				<div className="album_blurb">
 					{children}
 				</div>
+				{date}
 			</div>
 		<p className="clear_float" />
 		</div>

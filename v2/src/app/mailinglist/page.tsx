add75fcf96d7dde20bb531b2330127cb5ad992ee
xpollen8 <github@xpollen8.com>
2025-09-24 18:25:58 -0700

    dw: CSS

--- v2/src/app/mailinglist/page.tsx
+++ v2/src/app/mailinglist/page.tsx
@@ -5,23 +5,20 @@ import MakeSimpleURI from '@/components/MakeSimpleURI';
 const MailingList = () => 
 <>
 	<Header section="mailinglist" />
-		<main className="listItem">
+		<main>
 			<LetterHeader title="The Jazz Butcher mailing list" />
-			<div className="flex flex-wrap justify-around">
-				<div>
+			<p />
+			<div className="flex justify-around">
+				<div style={{ maxWidth: '90%' }}>
+					The jbc-list existed from October 1989 until early 2004
+					at which point manual spam filtering became too burdensome.
+					Several of the hundreds of subscribers were in contact with the band.
+					<p />
 					{[...Array(16)].map((x: any, idx: number) => { 
 						const year = 1989 + idx;
 						return <MakeSimpleURI key={idx} uri={`/mailinglist/${year}`} text={year + ''} />
 					})}
 				</div>
-				<div style={{ maxWidth: '50%' }}>
-					<div>
-					The jbc-list existed from October 1989 until early 2004
-					at which point manual spam filtering became too burdensome.
-					Several of the hundreds of subscribers were in contact with the band.
-					<p />
-				</div>
-			</div>
 		</div>
 	</main>
 </>

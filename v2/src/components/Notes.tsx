a35535885d8cf2c9ba5b59afdfe605535777836a
xpollen8 <github@xpollen8.com>
2025-09-26 18:42:49 -0700

    dw: notes surround

--- v2/src/components/Notes.tsx
+++ v2/src/components/Notes.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import Tag from '@/components/Tag';
 
-const Notes = ({ width=50, height=50 }: any) => <div className="flex flex-wrap mr-1">
+const Notes = ({ width=50, height=50, children }: any) => <div className="flex flex-wrap mr-1">
 	<Image src='/images/gents_note1.png' width={width} height={height} alt="note" />
+	{children}
 	<Image src='/images/gents_note2.png' width={width} height={height} alt="note" />
 </div>
 
 export const NotesTag = ({ children }: { children: React.ReactNode }) => <Tag><div className="flex flex-wrap">
-	<Notes width={20} height={20} />
+	<Notes width={20} height={20} >
 	{children}
+	</Notes>
 	</div>
 </Tag>
 

7afb6ca898c928c42e040fa334b418eb07be98d6
xpollen8 <github@xpollen8.com>
2025-09-28 12:59:16 -0700

    dw: group 'Re:' under subject

--- v2/src/components/JBCList.tsx
+++ v2/src/components/JBCList.tsx
@@ -3,108 +3,123 @@ import { type HashedType, prettyDate, dateAgo } from '@/lib/utils';
 import { notFound } from 'next/navigation';
 import messages from '@/../public/data/jbc-list.json';
 import LetterHeader from '@/components/LetterHeader';
+import { AutoLink } from '@/lib/defines';
 
 const pad = (n: number) => String(n + 1).padStart(4, '0');
 
 const JBCListNav = ({ year = '1989', id = 'date', next, prev }: any) => {
 	const yi = parseInt(year);
-	const py = (yi > 1989) ? yi - 1 : null;
-	const ny = (yi < 2004) ? yi + 1 : null;
-	if (!(yi >= 1989 && yi <= 2004)) return notFound();
-	const i = parseInt(id);
-	const mode = (i) ? <>Message: {i}</> : <>By: {id}</>;
-	const subject_selected = (id === 'subject') ? 'email_nav_selected' : '';
-	const date_selected = (id === 'date') ? 'email_nav_selected' : '';
-	const sender_selected = (id === 'sender') ? 'email_nav_selected' : '';
-	const yl = (i > 1) ? '' : `/${id}`;
-	return <LetterHeader title=<>{`jbc-list: ${year}`} - {mode}</> subhead=<>
-		{(py) && <>{' '}[<Link href={`/mailinglist/${py}${yl}`}>{py}</Link>]</>}
-		{(ny) && <>{' '}[<Link href={`/mailinglist/${ny}${yl}`}>{ny}</Link>]</>}
-		{' '}[<Link className={subject_selected} href={`/mailinglist/${year}/subject`}>By Subject</Link>]
-		{' '}[<Link className={date_selected} href={`/mailinglist/${year}`}>By Date</Link>]
-		{' '}[<Link className={sender_selected} href={`/mailinglist/${year}/sender`}>By Sender</Link>]
-		{(prev) && <>{' '}[<Link href={`/mailinglist/${prev}`}>Prev</Link>]</>}
-		{(next) && <>{' '}[<Link href={`/mailinglist/${next}`}>Next</Link>]</>}
-	</>
-	/>
+	if (yi) {
+		const py = (yi > 1989) ? yi - 1 : null;
+		const ny = (yi < 2004) ? yi + 1 : null;
+		if (!(yi >= 1989 && yi <= 2004)) return notFound();
+		const i = parseInt(id);
+		const mode = (i) ? <>Message: {i}</> : <>By: {id}</>;
+		const subject_selected = (id === 'subject') ? 'email_nav_selected' : '';
+		const date_selected = (id === 'date') ? 'email_nav_selected' : '';
+		const sender_selected = (id === 'sender') ? 'email_nav_selected' : '';
+		const yl = (i > 1) ? '' : `/${id}`;
+		return <LetterHeader title=<>{`jbc-list: ${year}`} - {mode}</> subhead=<>
+			{(py) && <>{' '}[<Link href={`/mailinglist/${py}${yl}`}>{py}</Link>]</>}
+			{(ny) && <>{' '}[<Link href={`/mailinglist/${ny}${yl}`}>{ny}</Link>]</>}
+			{' '}[<Link className={subject_selected} href={`/mailinglist/${year}/subject`}>By Subject</Link>]
+			{' '}[<Link className={date_selected} href={`/mailinglist/${year}`}>By Date</Link>]
+			{' '}[<Link className={sender_selected} href={`/mailinglist/${year}/sender`}>By Sender</Link>]
+			{(prev) && <>{' '}[<Link href={`/mailinglist/${prev}`}>Prev</Link>]</>}
+			{(next) && <>{' '}[<Link href={`/mailinglist/${next}`}>Next</Link>]</>}
+		</>
+		/>
+	} else {
+		return <>NAV</>
+	}
 }
 
 export const JBCListing = ({ year, id = 'date' }: any) => {
-	// @ts-ignore
-	const yearMessages: HashedType = messages[year];
-
-	switch (id) {
-		case 'subject': {
-			const subjects: HashedType = {};
-			yearMessages?.forEach((m: any) => {
-				const subject = m.subject;
-				if (!subjects[subject]) subjects[subject] = [];
-				subjects[subject].push(m);
-			})
-			return <>
-				<JBCListNav year={year} id={id} />
-				{Object.keys(subjects).map((subject: string, key: number) => {
-					return <ol className="email_listing" key={key}>
-						<b>{subject}</b>
-						{subjects[subject]?.map((m: any, key: number) => {
-							return <li className="odd:bg-gray-100 border-b" key={key}>
-								{m.date.substring(0, 10)}: <Link href={`/mailinglist/${m.msgId}`}>{m.from}</Link>
-							</li>
-						})}
-					</ol>
-				})}
-			</>
-		}
-		break;
-		case 'sender': {
-			const senders: HashedType = {};
-			yearMessages?.forEach((m: any) => {
-				const sender = m.from;
-				if (!senders[sender]) senders[sender] = [];
-				senders[sender].push(m);
-			})
-			return <>
-				<JBCListNav year={year} id={id} />
-				{Object.keys(senders).map((sender: string, key: number) => {
-					return <ol className="email_listing" key={key}>
-						<b><Link href={`/contributions/${sender}`}>{sender}</Link></b>
-						{senders[sender]?.map((m: any, key: number) => {
-							return <li className="odd:bg-gray-100 border-b" key={key}>
-								{m.date.substring(0, 10)}: <Link href={`/mailinglist/${m.msgId}`}>{m.subject}</Link>
-							</li>
-						})}
-					</ol>
-				})}
-			</>
-		}
-		break;
-		default: {
-			const days: HashedType = {};
-			yearMessages?.forEach((m: any) => {
-				const day = m.date.substring(5, 10);
-				if (!days[day]) days[day] = [];
-				days[day].push(m);
-			})
-			return <>
-				<JBCListNav year={year} id={id} />
-				{Object.keys(days).map((day: string, key: number) => {
-					const [ mm, dd ] = day.split('-');
-					return <ol className="email_listing" key={key}>
-						<b>{year}-{day}</b>
-						{days[day]?.map((m: any, key: number) => {
-							return <li className="odd:bg-gray-100 border-b" key={key}>
-								<Link href={`/mailinglist/${m.msgId}`}>{m.subject}</Link>
-							</li>
-						})}
-					</ol>
-				})}
-			</>
-		}
+	const doSubjects = (m: any, y: any, i: any) => {
+		const subjects: HashedType = {};
+		m?.forEach((m: any) => {
+			const subject = m.subject;
+			const [ re, sub ] = (subject || '').split('e:');
+			if (re?.length && sub?.length) {
+				const s = sub.trim();
+				if (!subjects[s]) subjects[s] = [];
+				subjects[s].push({ re: re + 'e:', ...m });
+			} else {
+				const s = subject.trim();
+				if (!subjects[s]) subjects[s] = [];
+				subjects[s].push(m);
+			}
+		})
+		return <>
+			<JBCListNav year={y} id={i} />
+			{Object.keys(subjects).map((subject: string, key: number) => {
+				return <ol className="email_listing" key={key}>
+					<b>{subject}</b>
+					{subjects[subject]?.map((m: any, key: number) => {
+						return <li className="odd:bg-gray-100 border-b" key={key}>
+							{m.date.substring(0, 10)}: <Link href={`/mailinglist/${m.msgId}`}>{m.re} {m.from}</Link>
+						</li>
+					})}
+				</ol>
+			})}
+		</>
 	}
-	return <>
-		YEAR: {year}
-		MODE: {id}
-	</>
+	const doSenders = (m: any, y: any, i: any) => {
+		const senders: HashedType = {};
+		m?.forEach((m: any) => {
+			const sender = m.from;
+			if (!senders[sender]) senders[sender] = [];
+			senders[sender].push(m);
+		})
+		return <>
+			<JBCListNav year={y} id={i} />
+			{Object.keys(senders).map((sender: string, key: number) => {
+				return <ol className="email_listing" key={key}>
+					<b><Link href={`/contributions/${sender}`}>{sender}</Link></b>
+					{senders[sender]?.map((m: any, key: number) => {
+						return <li className="odd:bg-gray-100 border-b" key={key}>
+							{m.date.substring(0, 10)}: <Link href={`/mailinglist/${m.msgId}`}>{m.subject}</Link>
+						</li>
+					})}
+				</ol>
+			})}
+		</>
+	}
+	const doDates = (m: any, y: any, i: any) => {
+		const days: HashedType = {};
+		m?.forEach((m: any) => {
+			const day = m.date.substring(0, 10);
+			if (!days[day]) days[day] = [];
+			days[day].push(m);
+		})
+		return <>
+			<JBCListNav year={y} id={i} />
+			{Object.keys(days).map((day: string, key: number) => {
+				const [ mm, dd ] = day.split('-');
+				return <ol className="email_listing" key={key}>
+					<b>{day}</b>
+					{days[day]?.map((m: any, key: number) => {
+						return <li className="odd:bg-gray-100 border-b" key={key}>
+							<Link href={`/mailinglist/${m.msgId}`}>{m.subject}</Link>
+						</li>
+					})}
+				</ol>
+			})}
+		</>
+	}
+	if (parseInt(year, 10) > 1988) {
+		// @ts-ignore
+		const yearMessages: HashedType = messages[year];
+		if (id === 'subject') return doSubjects(yearMessages, year, id);
+		if (id === 'sender') return doSenders(yearMessages, year, id);
+		return doDates(yearMessages, year, id);
+	}
+
+	// @ts-ignore
+	const m = Object.keys(messages)?.map((year: any) => [...messages[year]])?.flat();
+	if (year === 'subject') return doSubjects(m, year, id);
+	if (year === 'sender') return doSenders(m, year, id);
+	return doDates(m, year, id);
 }
 
 const findPrevious = (year: number, yearIdx: number) => {

be34cd3ec2eddf000d11b6efcb0d1cb7a7982d9f
xpollen8 <github@xpollen8.com>
2025-10-04 22:01:26 -0700

    dw: margin

--- v2/src/components/NotebookHero.tsx
+++ v2/src/components/NotebookHero.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 
 const NotebookHero = ({ arg }: any) => {
-	const [ src, setSrc ] = useState('');
+	const [ src, setSrc ] = useState();
 	const images = [
 		'lotga/LoTGA_44_Strata_1500.png',
 		'lotga/LoTGA_60_Pile_1500.png',
@@ -15,7 +16,7 @@ const NotebookHero = ({ arg }: any) => {
 	useEffect(() => {
 		setSrc('https://v1.jazzbutcher.com/images/notebooks/' + images[Math.floor(Math.random() * (images.length))]);
 	});
-	return <Image src={src} width={1500} height={500} alt='from LoTGA notebook' />
+	return src && <Link href={src}><Image className="mt-1" src={src} width={1500} height={500} alt='from LoTGA notebook' /></Link>
 }
 
 export default NotebookHero;

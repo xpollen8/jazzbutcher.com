c563c88973a2adc5d1f771b0ef533b2cb0dc1a2f
David Whittemore <github@xpollen8.com>
2026-08-25 12:26:08 -0700

    dw: add wallis pic of the guys

--- v2/src/app/page.tsx
+++ v2/src/app/page.tsx
@@ -6,12 +6,19 @@ import Footer from '@/components/Footer';
 import OnThisDay from '@/components/OnThisDay';
 import RecentUpdates from '@/components/RecentUpdates';
 import { MostRecentNews } from '@/components/News';
+import { Credit } from '@/components/GenericWeb';
 
 const Home = (): React.ReactNode =>
 <>
 	<Header section='jbc' />
 	<main>
-		<NotebookHero />
+		<center>
+			<div style={{ maxWidth: '100%', height: 'auto' }}>
+				<Link href="/gigs/2014-08-20"><Image src={'https://v1.jazzbutcher.com/images/20140820/20140820_2_1500.jpg'} width='1500' height='500' alt={'Pat and Max'} objectFit='contain' /></Link>
+				<Credit g="Mat Wallis" d="2014-08-20" />
+			</div>
+			<NotebookHero />
+		</center>
 		<div className="flex flex-col min-[750px]:flex-row gap-1">
 			<div className="w-full min-[750px]:w-1/2">
 				<OnThisDay />

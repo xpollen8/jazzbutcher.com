a52374b510889fcba6458872ca71098a5598985d
David Whittemore <github@xpollen8.com>
2026-09-06 13:41:22 -0700

    dw: don't need arg params

--- v2/src/app/api/[...slug]/apiData.tsx
+++ v2/src/app/api/[...slug]/apiData.tsx
@@ -309,7 +309,7 @@ const apiData = async (path: string, args?: any, formData?: any): Promise<Hashed
 			case 'feedback_delete': { return await apiDataFromDataServer(path, args); }
 			case 'feedback_by_page': { return await apiDataFromDataServer('feedback', args); }
 			case 'recent_feedback': {
-					const feedbacks = await apiDataFromDataServer('feedbacks', JSON.stringify(args));
+					const feedbacks = await apiDataFromDataServer('feedbacks');
 					return findRecent(feedbacks, ['dtcreated'], {});
 			}
 
